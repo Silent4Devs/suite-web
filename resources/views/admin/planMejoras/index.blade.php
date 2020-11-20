01e2a549fe16215d03ad56ac01a70853450c8aa9
Omar <omar.leocadio@silent4business.com>
2020-11-20 12:02:58 -0600

    carga de guia
    carga de guia

--- resources/views/admin/planMejoras/index.blade.php
+++ resources/views/admin/planMejoras/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.admin')
 @section('content')
 @can('plan_mejora_create')
-    <div style="margin-bottom: 10px;" class="row">
+<div class="card mt-5">
+    <div class="col-md-10 col-sm-9 py-3 card card-body bg-primary align-self-center " style="margin-top:-40px; ">
+        <h3 class="mb-2  text-center text-white"><strong>Plan implementación</strong></h3>
+    </div>
+    <div style="margin-bottom: 10px; margin-left:12px;" class="row">
         <div class="col-lg-12">
             <a class="btn btn-success" href="{{ route('admin.plan-mejoras.create') }}">
                 {{ trans('global.add') }} {{ trans('cruds.planMejora.title_singular') }}
--- resources/views/admin/planaccionCorrectivas/index.blade.php
+++ resources/views/admin/planaccionCorrectivas/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.admin')
 @section('content')
 @can('planaccion_correctiva_create')
-    <div style="margin-bottom: 10px;" class="row">
+<div class="card mt-5">
+    <div class="col-md-10 col-sm-9 py-3 card card-body bg-primary align-self-center " style="margin-top:-40px; ">
+        <h3 class="mb-2  text-center text-white"><strong>Plan acción</strong></h3>
+    </div>
+    <div style="margin-bottom: 10px; margin-left:12px;" class="row">
         <div class="col-lg-12">
             <a class="btn btn-success" href="{{ route('admin.planaccion-correctivas.create') }}">
                 {{ trans('global.add') }} {{ trans('cruds.planaccionCorrectiva.title_singular') }}
