4dd25b4dba15009a3537746df4c6b464c13ebc27
KaMarai <71285125+KaMarai@users.noreply.github.com>
2020-12-04 14:02:57 -0600

    GAP2
    Agregue tabla  gap2

--- resources/views/admin/analisisbrechas/gapdos.blade.php
+++ resources/views/admin/analisisbrechas/gapdos.blade.php
@@ -4,6 +4,8 @@
               GAP 02: IMPLEMENTACIÓN DEL PLAN DE SEGURIDAD Y PRIVACIDAD DE LA INFORMACIÓN (40%)
               </div>
 
+
+
               <!-- Card -->
               <div class="card" style="margin-left:10px; margin-right:10px;">
                   <!-- Card content -->
@@ -11,11 +13,138 @@
                       <!-- Text -->
                       <p class="card-text" align="justify">Implementacion del Plan de Seguridad y Privacidad. Tiene un peso del 40% del total del componente: 20% - Identificacion y analisis de riesgos, 20% - Plan de tratamiento de riesgos, clasificacion y gestion de controles.
                       </p>
+                      <p> <strong>INSTRUCCIONES:</strong> Por favor, conteste el siguiente cuestionario de acuerdo con los siguientes parámetros:</p>
 
                   </div>
 
               </div>
               <!-- Card -->
+
+              <table
+                  class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-PartesInteresada">
+                  <thead>
+
+                  <tr>
+                      <th style="background-color: #321fdb;" class="text-white text-center" >Cumple satisfactoriamente</th>
+                      <th>Existe, es gestionado, se está cumpliendo con lo que la norma ISO 27001  solicita, está documentado,  es conocido y aplicado por todos los involucrados en el SGSI.  cumple 100%.
+                  </th>
+                  <tr>
+                      <th style="background-color: #321fdb;" class="text-white text-center" >Cumple parcialmente</th>
+                      <th>Lo que la norma requiere  (ISO27001 versión 2013)  se está haciendo de manera parcial, se está haciendo diferente, no está documentado, se definió y aprobó pero no se gestiona.
+                  </th>
+                  <tr>
+                      <th style="background-color: #321fdb;" class="text-white text-center" >No cumple</th>
+                      <th>Existe, es gestionado, se está cumpliendo con lo que la norma ISO 27001  solicita, está documentado,  es conocido y aplicado por todos los involucrados en el SGSI.  cumple 100%.
+                  </th>
+                  <tr>
+                      <th style="background-color: #321fdb;" class="text-white text-center" >No aplica</th>
+                      <th>El control no es aplicable para la entidad. En el campo evidencia por favor indicar la justificación respectiva de su no aplicabilidad.
+                  </th>
+                  </tr>
+                  </thead>
+
+              </table>
+
+
+              <table
+                  class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-PartesInteresada" style="margin-top:15px;">
+                  <thead>
+
+                  <tr>
+                      <th style="background-color: #321fdb;" class="text-white text-center" >Hacer</th>
+
+
+                  </tr>
+
+                  </thead>
+
+              </table>
+
+
+
+              <table
+                  class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-PartesInteresada"style="margin-top:-15px;"  >
+                  <thead>
+
+                  <tr style="background-color: #321fdb;" class="text-white text-center">
+                      <th>Control</th>
+                      <th>Valoración</th>
+                      <th >Evidencia de cumplimiento</th>
+                      <th>Recomendación</th>
+                  </tr>
+                  </thead>
+
+              </table>
+
+
+
+              <table
+                  class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-PartesInteresada" style="margin-top:-20px;">
+                  <thead>
+
+                  <tr>
+                      <th style="background-color: #321fdb;" class="text-white text-center" >A5</th>
+                      <th>Políticas de la Seguridad de la Información</th></th>
+                  <tr>
+                      <th style="background-color: #321fdb;" class="text-white text-center" >A5.1</th>
+                      <th>Orientación de la dirección para la gestión de la seguridad de la información</th>
+
+
+                  </tr>
+
+                  </thead>
+
+              </table>
+
+
+              <table
+                  class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-PartesInteresada" style="margin-top:-20px;">
+                  <thead>
+
+                  <tr>
+                      <th style="background-color: #321fdb;" class="text-white text-center" >OBJETIVO: Brindar orientación y soporte, por parte de la dirección, para la seguridad de la información de acuerdo con los requisitos del negocio y con las leyes y reglamentos pertinentes</th>
+
+
+                  </tr>
+
+                  </thead>
+
+              </table>
+
+
+
+
+              <table
+                  class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-PartesInteresada" style="margin-top:-15px;">
+                  <thead>
+
+                  <tr>
+
+                      <th>A5.1.1</th>
+                      <th>Políticas para la seguridad de la información</th>
+                      <th>Se debe definir un conjunto de políticas para la seguridad de la información, aprobada por la dirección, publicada y comunicada a los empleados y a las partes externas pertinentes.</th>
+                      <th>Cumple parcialmente</th>
+                      <th>Describir la evidencia que soporta el cumplimiento del requisito. Indicar sitio fisico, o sistema donde se pueda verificar el mismo. </th>
+                      <th>Pautas que se deben llevar a cabo para dar cumplimiento al requisito.</th>
+
+
+                  </tr>
+
+                  <tr>
+
+                      <th>A5.1.1</th>
+                      <th>Políticas para la seguridad de la información</th>
+                      <th>Se debe definir un conjunto de políticas para la seguridad de la información, aprobada por la dirección, publicada y comunicada a los empleados y a las partes externas pertinentes.</th>
+                      <th>Cumple parcialmente</th>
+                      <th>Describir la evidencia que soporta el cumplimiento del requisito. Indicar sitio fisico, o sistema donde se pueda verificar el mismo. </th>
+                      <th>Pautas que se deben llevar a cabo para dar cumplimiento al requisito.</th>
+
+
+                  </tr>
+
+                  </thead>
+
+              </table>
           </div>
 
       </div>
