ad160f3817113ad9f482f08b4fe2197f88617e5e
john <vojohn95@gmail.com>
2020-12-17 22:31:11 -0600

    Terminado dashboard

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         App\Providers\RouteServiceProvider::class,
         Emotality\TawkTo\TawkToServiceProvider::class,
         Alexusmai\LaravelFileManager\FileManagerServiceProvider::class,
-        Fx3costa\LaravelChartJs\Providers\ChartjsServiceProvider::class
 
     ],
 
