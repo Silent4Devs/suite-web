b6853ae1a5caff9243153620d563045caff8cf61
johnvo <vojohn95@gmail.com>
2020-12-24 11:45:59 -0600

    Falta edicion de table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.admin')
 @section('content')
+
     <div class="content">
         <div class="row">
             <div class="col-lg-12">
@@ -86,22 +87,6 @@
                                     </div>
                                 </div>
                             </div>
-
-                        <!--<div class="row">
-                        <div class="{{ $chart1->options['column_class'] }}">
-                            <h3>{!! $chart1->options['chart_title'] !!}</h3>
-                            {!! $chart1->renderHtml() !!}
-                            </div>
-                            <div class="{{ $chart2->options['column_class'] }}">
-                            <h3>{!! $chart2->options['chart_title'] !!}</h3>
-                            {!! $chart2->renderHtml() !!}
-                            </div>
-                            <div class="{{ $chart3->options['column_class'] }}">
-                            <h3>{!! $chart3->options['chart_title'] !!}</h3>
-                            {!! $chart3->renderHtml() !!}
-                            </div>
-                        </div>-->
-
                         </div>
                     </div>
                 </div>
@@ -110,9 +95,7 @@
     </div>
 @endsection
 @section('scripts')
-    @parent
-    <!--<script
-        src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>{!! $chart1->renderJs() !!}{!! $chart2->renderJs() !!}{!! $chart3->renderJs() !!}-->
+
 @endsection
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
