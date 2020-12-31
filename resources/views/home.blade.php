53d26740f722720438decf15812de68ca391c9d2
Omar <omar.leocadio@silent4business.com>
2020-12-30 20:55:02 -0600

    Fix/AddDash
    Fix/AddDash

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,93 @@
 @extends('layouts.admin')
 @section('content')
-    <div class="content">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card mt-5">
-                    <div class="col-md-10 col-sm-9 py-3 card card-body bg-primary align-self-center"
-                         style="margin-top: -40px;">
-                        <h3 class="mb-2 text-center text-white"><strong>Tablero de Control</strong></h3>
+<div class="content">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card mt-5">
+                <div class="col-md-12 col-sm-9 py-3 card card-body bg-primary align-self-center " style="margin-top:-40px; ">
+                    <h3 class="mb-2  text-center text-white"><strong>Tablero de Control</strong></h3>
+                </div>
+                <div class="col-12">
+                    <div class="card-body">
+                        @if(session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                        @endif
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="col-md-12 col-sm-6 py-1 card card-body bg-info align-self-center">
+                                    <h3 class="mb-2  text-center text-white"><strong>Plan</strong></h3>
+                                </div>
+                                <h3>Progreso general del plan <strong>20%</strong></h3>
+                                <canvas id="progresochart1"></canvas>
+                            </div>
+                            <div class="{{ $chart3->options['column_class'] }}">
+                                <div class="col-md-12 col-sm-6 py-1 card card-body bg-info align-self-center ">
+                                    <h3 class="mb-2  text-center text-white"><strong>Do</strong></h3>
+                                </div>
+                                <h3>{!! $chart3->options['chart_title'] !!}</h3>
+                                {!! $chart3->renderHtml() !!}
+                            </div>
+                        </div>
                     </div>
-
-                    <div class="row">
-                        <div class="{{$chart1->options['column_class']}}">
-                            <h3>{!! $chart1->options['chart_title'] !!}</h3>
-                            {!! $chart1->renderHtml() !!}
-                        </div>
-                        <div class="{{$chart2->options['column_class']}}">
-                            <h3>{!! $chart2->options['chart_title'] !!}</h3>
-                            {!! $chart2->renderHtml() !!}
-                        </div>
-                        <div class="{{$chart2->options['column_class']}}">
-                            <h3>{!! $chart2->options['chart_title'] !!}</h3>
-                            {!! $chart1->renderHtml() !!}
+                </div>
+                <div class="col-12" style="margin-top:20px; ">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="{{ $chart1->options['column_class'] }}">
+                                <div class="col-md-12 col-sm-6 py-1 card card-body bg-info align-self-center " style="margin-top:-40px; ">
+                                    <h3 class="mb-2  text-center text-white"><strong>Check</strong></h3>
+                                </div>
+                                <h3>{!! $chart1->options['chart_title'] !!}</h3>
+                                {!! $chart1->renderHtml() !!}
+                            </div>
+                            <div class="{{ $chart2->options['column_class'] }}">
+                                <div class="col-md-12 col-sm-6 py-1 card card-body bg-info align-self-center"  style="margin-top:-40px;">
+                                    <h3 class="mb-2  text-center text-white"><strong>Act</strong></h3>
+                                </div>
+                                <h3>{!! $chart2->options['chart_title'] !!}</h3>
+                                {!! $chart2->renderHtml() !!}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
 @endsection
 @section('scripts')
-    @parent
-    <script src="https://cdnjs.cloudflare.com/ajax/lib/Charts.js/2.5.0/Chart.min.js"></script>
-{!! $chart1->renderJs() !!}
-    {!! $chart2->renderJs() !!}
-    {!! $chart3->renderJs() !!}
+@parent
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<script src="https://unpkg.com/gauge-chart@latest/dist/bundle.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/emn178/chartjs-plugin-labels/src/chartjs-plugin-labels.js"></script>{!! $chart1->renderJs() !!}{!! $chart2->renderJs() !!}{!! $chart3->renderJs() !!}
+<script>
+    var popCanvas1 = document.getElementById("progresochart1");
+    var barChart1 = new Chart(popCanvas1, {
+        type: 'doughnut',
+        labels: {
+            render: 'value'
+        },
+        data: {
+            labels: [
+                "Sin iniciar",
+                "En proceso",
+                "Completadas",
+                "Retrasadas"
+            ],
+            datasets: [{
+                label: '% Implementación por fase',
+                data: [30, 20, 20, 10],
+                backgroundColor: [
+                    'rgba(22, 160, 133, 0.6)',
+                    'rgba(244, 208, 63, 0.6)',
+                    'rgba(231, 76, 60, 0.6)',
+                    'rgba(133, 193, 233 , 0.6)',
+                ]
+            }]
+        }
+    });
+</script>
+@endsection
