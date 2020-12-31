08a13444ec5daf72c0a2275240929c9cd3b07e2a
Benito Lopez <benito.lopez.tecno@gmail.com>
2020-12-30 21:34:06 -0600

    detalles

--- resources/views/admin/implementacions/introduccion.blade.php
+++ resources/views/admin/implementacions/introduccion.blade.php
@@ -1,43 +1,25 @@
-      <div class="row">
-            <div class="col-6 col-md-6 col-sm-12">
 
-              <h3 class="text-center indigo-text" class="mb-0">Introducción</h3>
-
-              <!-- Card -->
-              <div class="card" style="margin-left:10px; margin-right:10px;"  >
-                  <!-- Card content -->
-                  <div class="card-body">
-
-                      <!-- Text -->
-                      <p class="card-text" align="justify">Bienvenido al módulo de implementación de la norma
-                          internacional
-                          ISO/IEC
-                          27001:2013. En este apartado te llevaremos paso a paso para lograr la implementación
-                          adecuada de la norma ISO 27001 en tu organización. Te proporcionaremos las herramientas
-                          necesarias para conseguirlo. Contarás con una guía de implementación con los conceptos y
-                          ejemplos prácticos que te permitirán tener una mejor comprensión de la norma, un plan de
-                          trabajo base con las actividades generales de implementación de la norma y la asesoría
-                          en línea de nuestros consultores expertos.</p>
-                      <!-- Button -->
-                      <a id="profile-tab-classic" data-toggle="tab" a
-                         href="#test2"
-                         role="tab" aria-controls="profile-classic" aria-selected="true" class="btn btn-primary">Siguiente</a>
-
-                  </div>
-
-              </div>
-              <!-- Card -->
-          </div>
-          <div class="col-12 col-md-6 col-sm-12">
-              <!-- Card -->
+<div class="row" style="padding: 0;">
+    <h3 class="text-center indigo-text col-12 mb-4">Introducción</h3>
 
 
+    <p class="card-text col-md-6" align="justify">Bienvenido al módulo de implementación de la norma
+        internacional
+        ISO/IEC
+        27001:2013. En este apartado te llevaremos paso a paso para lograr la implementación
+        adecuada de la norma ISO 27001 en tu organización. Te proporcionaremos las herramientas
+        necesarias para conseguirlo. Contarás con una guía de implementación con los conceptos y
+        ejemplos prácticos que te permitirán tener una mejor comprensión de la norma, un plan de
+        trabajo base con las actividades generales de implementación de la norma y la asesoría
+        en línea de nuestros consultores expertos.
+        <br><br>
+        <a id="profile-tab-classic" data-toggle="tab" href="#test2" role="tab" aria-controls="profile-classic" aria-selected="true" class="btn btn-primary mt-4">Siguiente</a>
 
-              <img class="card-img-top" src="../img/seguridad.jpg" alt="Card image">
+    </p>
 
-                  <!-- Card image -->
+    <div class="col-md-6">
+        <img src="../img/seguridad.jpg" width="100%" alt="Card image">
+    </div>
+</div>
 
 
-
-          </div>
-      </div>
