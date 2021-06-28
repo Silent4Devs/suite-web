75ee12d44688875da89fa9438eea2880b0fd1f40
John <vojohn95@gmail.com>
2021-06-28 15:01:18 -0500

    Relations finished

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -1,11 +1,53 @@
 <?php
 
+/**
+ * Created by Reliese Model.
+ */
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class Grupo
+ *
+ * @property int $id
+ * @property string|null $nombre
+ * @property string|null $descripcion
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $deleted_at
+ *
+ * @property Collection|Area[] $areas
+ * @property Collection|Macroproceso[] $macroprocesos
+ *
+ * @package App\Models
+ */
 class Grupo extends Model
 {
-    use HasFactory;
+	use SoftDeletes;
+	protected $table = 'grupos';
+
+    protected $dates = ['deleted_at'];
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+	protected $fillable = [
+		'nombre',
+		'descripcion'
+	];
+
+	public function areas()
+	{
+		return $this->hasMany(Area::class, 'id_grupo');
+	}
+
+	public function macroprocesos()
+	{
+		return $this->hasMany(Macroproceso::class, 'id_grupo');
+	}
 }
