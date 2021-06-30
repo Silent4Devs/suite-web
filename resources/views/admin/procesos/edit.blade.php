14b8c7e053ddcbcc50e0ea8fd9be5d46d43895c4
John <vojohn95@gmail.com>
2021-06-30 13:47:37 -0500

    Procesos terminados

--- resources/views/admin/procesos/edit.blade.php
+++ resources/views/admin/procesos/edit.blade.php
@@ -5,12 +5,12 @@
 
     <div class="mt-4 card">
         <div class="py-3 col-md-10 col-sm-9 card-body verde_silent align-self-center" style="margin-top: -40px;">
-            <h3 class="mb-1 text-center text-white align-items-centera"><strong> Editar: </strong>Macroproceso
-                {{ $macroproceso->nombre }}</h3>
+            <h3 class="mb-1 text-center text-white align-items-centera"><strong> Editar: </strong>Proceso
+                {{ $proceso->nombre }}</h3>
         </div>
 
         <div class="card-body">
-            <form method="POST" action="{{ route('admin.macroprocesos.update', $macroproceso) }}" class="row">
+            <form method="POST" action="{{ route('admin.procesos.update', $proceso) }}" class="row">
                 @csrf
                 @method('PATCH')
                 <div class="col-md-12 col-sm-12">
@@ -22,7 +22,7 @@
                     <label class="required" for="codigo"><i class="fas  fa-barcode iconos-crear"></i>
                         Código</label>
                     <input class="form-control {{ $errors->has('codigo') ? 'is-invalid' : '' }}" type="text" name="codigo"
-                        id="codigo" value="{{ old('codigo', $macroproceso->codigo) }}" required>
+                        id="codigo" value="{{ old('codigo', $proceso->codigo) }}" required>
                     @if ($errors->has('codigo'))
                         <div class="invalid-feedback">
                             {{ $errors->first('codigo') }}
@@ -33,7 +33,7 @@
                     <label class="required" for="nombre"><i class="fas fa-file-signature iconos-crear"></i>
                         Nombre</label>
                     <input class="form-control {{ $errors->has('nombre') ? 'is-invalid' : '' }}" type="text" name="nombre"
-                        id="nombre" value="{{ old('nombre', $macroproceso->nombre) }}" required>
+                        id="nombre" value="{{ old('nombre', $proceso->nombre) }}" required>
                     @if ($errors->has('nombre'))
                         <div class="invalid-feedback">
                             {{ $errors->first('nombre') }}
@@ -42,25 +42,26 @@
                 </div>
 
                 <div class="form-group col-md-4 col-sm-4">
-                    <label for="id_grupo"><i class="fas fa-users iconos-crear"></i>Grupo </label>
-                    <select class="form-control select2 {{ $errors->has('id_grupo') ? 'is-invalid' : '' }}"
-                        name="id_grupo" id="id_grupo" required>
+                    <label for="id_macroproceso"><i class="fas fa-users iconos-crear"></i>Macroproceso </label>
+                    <select class="form-control select2 {{ $errors->has('id_macroproceso') ? 'is-invalid' : '' }}"
+                        name="id_macroproceso" id="id_macroproceso" required>
                         <option value="">
-                            Escoja un grupo
+                            Escoja un macroproceso
                         </option>
-                        @if ($grupos)
-                            @foreach ($grupos as $grupo)
-                                <option value="{{ $grupo->id }}"
-                                    {{ $grupo->id == $macroproceso->grupo->id ? 'selected' : '' }}>
-                                    {{ $grupo->nombre }}</option>
+                        @if ($macroprocesos)
+                            @foreach ($macroprocesos as $macroproceso)
+                                <option value="{{ $macroproceso->id }}"
+                                    {{ $macroproceso->id == $proceso->macroproceso->id ? 'selected' : '' }}>
+                                    {{ $macroproceso->codigo }} / {{ $macroproceso->nombre }}
+                                </option>
                             @endforeach
                         @else
                             <option value="">No hay proveedores registrados</option>
                         @endif
                     </select>
-                    @if ($errors->has('id_grupo'))
+                    @if ($errors->has('id_macroproceso'))
                         <div class="invalid-feedback">
-                            {{ $errors->first('id_grupo') }}
+                            {{ $errors->first('id_macroproceso') }}
                         </div>
                     @endif
                     <span class="help-block"></span>
@@ -69,7 +70,9 @@
                     <label class="required" for="descripcion"><i class="fas fa-sticky-note iconos-crear"></i>
                         Descripción</label>
                     <textarea rows="3" class="form-control {{ $errors->has('descripcion') ? 'is-invalid' : '' }}" type="text" name="descripcion"
-                        id="descripcion" value="{{ old('descripcion', $macroproceso->descripcion) }}" required></textarea>
+                        id="descripcion" value="{{ old('descripcion', $proceso->descripcion) }}" required>
+                        {{ $proceso->descripcion }}
+                    </textarea>
                     @if ($errors->has('descripcion'))
                         <div class="invalid-feedback">
                             {{ $errors->first('descripcion') }}
@@ -85,7 +88,5 @@
             </form>
         </div>
     </div>
-
-
 
 @endsection
