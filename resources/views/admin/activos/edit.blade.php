0e0539c22b0ddf10903e053468e561c7e84dce05
Karen Marai <karen_03_93@hotmail.com>
2021-08-06 11:26:43 -0500

    añadi ventanas para añadir modelos y marcas

--- resources/views/admin/activos/edit.blade.php
+++ resources/views/admin/activos/edit.blade.php
@@ -10,16 +10,20 @@
         <form method="POST" class="row" action="{{ route("admin.activos.update", [$activo->id]) }}" enctype="multipart/form-data">
             @method('PUT')
             @csrf
+
             <div class="form-group col-md-12">
                 <label class="required" for="nombreactivo_id"><i class="fas fa-chart-line iconos-crear"></i>Nombre del Activo</label>
-                <input class="form-control select2 {{ $errors->has('nombre_activo') ? 'is-invalid' : '' }}" name="nombre_activo" id="nombre_activo" value={{ old('nombre_activo', $activo->nombre_activo) }} >
-                @if($errors->has('nombre_activo'))
+                <input class="form-control {{ $errors->has('nombreactivo') ? 'is-invalid' : '' }}" type="text" name="nombreactivo"
+                id="n_serie" value="{{ old('nombreactivo', $activo->nombreactivo) }}" required>
+                @if($errors->has('nombreactivo'))
                     <div class="invalid-feedback">
-                        {{ $errors->first('nombre_activo') }}
+                        {{ $errors->first('nombreactivo') }}
                     </div>
                 @endif
                   <span class="help-block"></span>
             </div>
+
+
 
 
             <div class="form-group col-md-6">
@@ -60,20 +64,74 @@
                 @endif
                 <span class="help-block">{{ trans('cruds.activo.fields.descripcion_helper') }}</span>
             </div>
-            <div class="form-group col-md-6">
-                <label for="dueno_id"><i class="fas fa-user-tie iconos-crear"></i>{{ trans('cruds.activo.fields.dueno') }}</label>
-                <select class="form-control select2 {{ $errors->has('dueno') ? 'is-invalid' : '' }}" name="dueno_id" id="dueno_id">
-                    @foreach($duenos as $id => $dueno)
-                        <option value="{{ $id }}" {{ (old('dueno_id') ? old('dueno_id') : $activo->dueno->id ?? '') == $id ? 'selected' : '' }}>{{ $dueno }}</option>
+
+
+            <div class="form-group col-md-4">
+                <label for="dueno_id"><i class="fas fa-user-tie iconos-crear"></i>Dueño</label>
+                <select class="form-control select2 {{ $errors->has('dueno') ? 'is-invalid' : '' }}"
+                    name="dueno_id" id="dueno_id">
+                    @foreach ($empleados as $id => $empleado)
+                    <option data-puesto="{{ $empleado->puesto}}" value="{{ $empleado->id }}" data-area="{{ $empleado->area->area}}" {{old('dueno_id',$activo->dueno_id)==$empleado->id ?"selected" :""}}>
+
+                        {{ $empleado->name }}
+                    </option>
                     @endforeach
                 </select>
-                @if($errors->has('dueno'))
+                @if ($errors->has('empleados'))
                     <div class="invalid-feedback">
-                        {{ $errors->first('dueno') }}
+                        {{ $errors->first('empleados') }}
                     </div>
                 @endif
-                <span class="help-block">{{ trans('cruds.activo.fields.dueno_helper') }}</span>
+                <span class="help-block">{{ trans('cruds.sede.fields.organizacion_helper') }}</span>
             </div>
+
+            <div class="form-group col-md-4">
+                <label for="id_puesto_dueno"><i class="fas fa-briefcase iconos-crear"></i>Puesto</label>
+                <div class="form-control" id="puesto_dueno"></div>
+            </div>
+
+            <div class="form-group col-md-4">
+                <label for="id_area_dueno"><i class="fas fa-street-view iconos-crear"></i>Área</label>
+                <div class="form-control" id="area_dueno"></div>
+
+            </div>
+
+            <div class="form-group col-md-4">
+                <label for="id_responsable"><i class="fas fa-user-tie iconos-crear"></i>Responsable</label>
+                <select class="form-control select2 {{ $errors->has('puesto') ? 'is-invalid' : '' }}"
+                    name="id_responsable" id="id_responsable">
+                    @foreach ($empleados as $id => $empleado)
+                    <option data-puesto="{{ $empleado->puesto}}" value="{{ $empleado->id }}" data-area="{{ $empleado->area->area}}" {{old('id_responsable',$activo->id_responsable)==$empleado->id ?"selected" :""}}>
+
+                        {{ $empleado->name }}
+                    </option>
+                    @endforeach
+                </select>
+                @if ($errors->has('empleados'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('empleados') }}
+                    </div>
+                @endif
+                <span class="help-block">{{ trans('cruds.sede.fields.organizacion_helper') }}</span>
+            </div>
+
+
+            <div class="form-group col-md-4">
+                <label for="id_responsable"><i class="fas fa-briefcase iconos-crear"></i>Puesto</label>
+                <div class="form-control" id="puesto_responsable"></div>
+
+            </div>
+
+
+
+            <div class="form-group col-md-4">
+                <label for="id_area_responsable"><i class="fas fa-street-view iconos-crear"></i>Área</label>
+                <div class="form-control" id="area_responsable"></div>
+
+            </div>
+
+
+
             <div class="form-group col-md-6">
                 <label for="ubicacion_id"><i class="fas fa-map-marker-alt iconos-crear"></i>{{ trans('cruds.activo.fields.ubicacion') }}</label>
                 <select class="form-control select2 {{ $errors->has('ubicacion') ? 'is-invalid' : '' }}" name="ubicacion_id" id="ubicacion_id">
@@ -88,6 +146,81 @@
                 @endif
                 <span class="help-block">{{ trans('cruds.activo.fields.ubicacion_helper') }}</span>
             </div>
+
+
+            <div class="form-group col-sm-6">
+                <label class="required" for="marca">
+                    Marca</label>
+                <input class="form-control {{ $errors->has('nombre') ? 'is-invalid' : '' }}" type="text" name="marca"
+                    id="marca" value="{{ old('marca', $activo->marca) }}" required>
+                @if ($errors->has('marca'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('marca') }}
+                    </div>
+                @endif
+            </div>
+
+            <div class="form-group col-sm-6">
+                <label class="required" for="marca">
+                    Modelo</label>
+                <input class="form-control {{ $errors->has('modelo') ? 'is-invalid' : '' }}" type="text" name="modelo"
+                    id="modelo" value="{{ old('modelo', $activo->modelo) }}" required>
+                @if ($errors->has('modelo'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('modelo') }}
+                    </div>
+                @endif
+            </div>
+
+            <div class="form-group col-sm-6">
+                <label class="required" for="n_serie"><i class="fas fa-barcode iconos-crear"></i>No de serie</label>
+                <input class="form-control {{ $errors->has('n_serie') ? 'is-invalid' : '' }}" type="text" name="n_serie"
+                    id="n_serie" value="{{ old('n_serie', $activo->n_serie) }}" required>
+                @if ($errors->has('n_serie'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('n_serie') }}
+                    </div>
+                @endif
+            </div>
+
+
+            <div class="form-group col-sm-6">
+                <label class="required" for="n_serie"><i class="fas fa-barcode iconos-crear"></i>No de producto</label>
+                <input class="form-control {{ $errors->has('n_serie') ? 'is-invalid' : '' }}" type="text" name="n_producto"
+                    id="n_producto" value="{{ old('n_producto', $activo->n_producto) }}" required>
+                @if ($errors->has('n_producto'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('n_producto') }}
+                    </div>
+                @endif
+            </div>
+
+
+            <div class="form-group col-sm-6">
+                <label class="required" for="n_serie"><i class="fas fa-calendar-alt iconos-crear"></i> Fecha de compra</label>
+                <input class="form-control {{ $errors->has('fecha_compra') ? 'is-invalid' : '' }}" type="date" name="fecha_compra"
+                    id="fecha_compra" value="{{ old('fecha_compra',$activo->fecha_compra) }}" required>
+                @if ($errors->has('fecha_compra'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('fecha_compra') }}
+                    </div>
+                @endif
+            </div>
+
+
+            <div class="form-group col-sm-6">
+                <label class="required" for="n_serie"><i class="fas fa-calendar-alt iconos-crear"></i>Fecha fin de compra</label>
+                <input class="form-control {{ $errors->has('fecha_fin') ? 'is-invalid' : '' }}" type="date" name="fecha_fin"
+                    id="n_producto" value="{{ old('fecha_fin', $activo->fecha_fin) }}" required>
+                @if ($errors->has('fecha_fin'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('fecha_fin') }}
+                    </div>
+                @endif
+            </div>
+
+
+
             <div class="text-right form-group col-12">
                 <button class="btn btn-danger" type="submit">
                     {{ trans('global.save') }}
@@ -99,4 +232,40 @@
 
 
 
+@endsection
+
+
+@section('scripts')
+
+<script>
+    document.addEventListener('DOMContentLoaded', function(e) {
+
+        let responsable=document.querySelector('#id_responsable');
+        let area_init=responsable.options[responsable.selectedIndex].getAttribute('data-area');
+            let puesto_init=responsable.options[responsable.selectedIndex].getAttribute('data-puesto');
+            document.getElementById('puesto_responsable').innerHTML=puesto_init
+            document.getElementById('area_responsable').innerHTML=area_init
+
+            let dueno=document.querySelector('#dueno_id');
+            let area=dueno.options[dueno.selectedIndex].getAttribute('data-area');
+            let puesto=dueno.options[dueno.selectedIndex].getAttribute('data-puesto');
+            document.getElementById('puesto_dueno').innerHTML=puesto
+            document.getElementById('area_dueno').innerHTML=area
+
+        responsable.addEventListener('change',function (e) {
+            e.preventDefault();
+            let area=this.options[this.selectedIndex].getAttribute('data-area');
+            let puesto=this.options[this.selectedIndex].getAttribute('data-puesto');
+            document.getElementById('puesto_responsable').innerHTML=puesto
+            document.getElementById('area_responsable').innerHTML=area
+        })
+        dueno.addEventListener('change',function (e) {
+            e.preventDefault();
+            let area=this.options[this.selectedIndex].getAttribute('data-area');
+            let puesto=this.options[this.selectedIndex].getAttribute('data-puesto');
+            document.getElementById('puesto_dueno').innerHTML=puesto
+            document.getElementById('area_dueno').innerHTML=area
+        })
+    })
+</script>
 @endsection
--- resources/views/admin/activos/index.blade.php
+++ resources/views/admin/activos/index.blade.php
@@ -11,7 +11,7 @@
 
         @include('partials.flashMessages')
         <div class="card-body datatable-fix">
-            <table class="table table-bordered w-100 datatable-Activo">
+            <table class="table table-bordered w-100 datatable-Activo" id="columnaft">
                 <thead class="thead-dark">
                     <tr>
                         <th>
@@ -33,7 +33,28 @@
                             {{ trans('cruds.activo.fields.dueno') }}
                         </th>
                         <th>
+                            Responsable
+                         </th>
+                        <th>
                             {{ trans('cruds.activo.fields.ubicacion') }}
+                        </th>
+                        <th>
+                           Marca
+                        </th>
+                        <th>
+                            Modelo
+                        </th>
+                        <th>
+                            N° serie
+                        </th>
+                        <th>
+                            N° producto
+                        </th>
+                        <th>
+                            Fecha fin
+                        </th>
+                        <th>
+                            Fecha compra
                         </th>
                         <th>
                             Opciones
@@ -212,6 +233,7 @@
                 retrieve: true,
                 aaSorting: [],
                 ajax: "{{ route('admin.activos.index') }}",
+                columnDefs:[{targets:[6,7,8,9,10,11,12,13],visible:false}],
                 columns: [{
                         data: 'id',
                         name: 'id'
@@ -219,6 +241,8 @@
                     {
                         data: 'nombre_activo',
                         name: 'nombre_activo'
+
+
                     },
                     {
                         data: 'tipoactivo_tipo',
@@ -237,8 +261,36 @@
                         name: 'dueno.name'
                     },
                     {
+                        data: 'responsable',
+                        name: 'responsable'
+                    },
+                    {
                         data: 'ubicacion_sede',
                         name: 'ubicacion.sede'
+                    },
+                    {
+                        data: 'marca',
+                        name: 'marca'
+                    },
+                    {
+                        data: 'modelo',
+                        name: 'modelo'
+                    },
+                    {
+                        data: 'n_serie',
+                        name: 'n_serie'
+                    },
+                    {
+                        data: 'n_producto',
+                        name: 'n_producto'
+                    },
+                    {
+                        data: 'fecha_fin',
+                        name: 'fecha_fin'
+                    },
+                    {
+                        data: 'fecha_compra',
+                        name: 'fecha_compra'
                     },
                     {
                         data: 'actions',
@@ -266,4 +318,7 @@
         });
 
     </script>
+
+
+
 @endsection
