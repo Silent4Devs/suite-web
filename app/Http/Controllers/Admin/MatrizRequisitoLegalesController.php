f359d4097871e40bf355abb621becc35cb080b10
Karen Marai <karen_03_93@hotmail.com>
2021-08-13 13:34:11 -0500

    Añadi nuevos campos en matriz de requisitos legales

--- app/Http/Controllers/Admin/MatrizRequisitoLegalesController.php
+++ app/Http/Controllers/Admin/MatrizRequisitoLegalesController.php
@@ -2,25 +2,29 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Gate;
+use App\Models\Team;
+use App\Models\Empleado;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\MassDestroyMatrizRequisitoLegaleRequest;
+use App\Models\MatrizRequisitoLegale;
+use App\Models\EvidenciaMatrizRequisitoLegale;
+use Yajra\DataTables\Facades\DataTables;
+use Symfony\Component\HttpFoundation\Response;
 use App\Http\Requests\StoreMatrizRequisitoLegaleRequest;
 use App\Http\Requests\UpdateMatrizRequisitoLegaleRequest;
-use App\Models\MatrizRequisitoLegale;
-use App\Models\Team;
-use Gate;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Yajra\DataTables\Facades\DataTables;
+use App\Http\Requests\MassDestroyMatrizRequisitoLegaleRequest;
 
 class MatrizRequisitoLegalesController extends Controller
 {
     public function index(Request $request)
     {
         abort_if(Gate::denies('matriz_requisito_legale_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
+        // dd($query = MatrizRequisitoLegale::with(['team','evidencias_matriz','empleado'])->get());
         if ($request->ajax()) {
-            $query = MatrizRequisitoLegale::with(['team'])->select(sprintf('%s.*', (new MatrizRequisitoLegale)->table));
+            $query = MatrizRequisitoLegale::with(['team','evidencias_matriz','empleado'])->select(sprintf('%s.*', (new MatrizRequisitoLegale)->table));
             $table = Datatables::of($query);
 
             $table->addColumn('placeholder', '&nbsp;');
@@ -44,22 +48,62 @@ class MatrizRequisitoLegalesController extends Controller
             $table->editColumn('id', function ($row) {
                 return $row->id ? $row->id : "";
             });
+            $table->editColumn('tipo', function ($row) {
+                return $row->tipo ? $row->tipo : "";
+            });
             $table->editColumn('nombrerequisito', function ($row) {
                 return $row->nombrerequisito ? $row->nombrerequisito : "";
-            });
-
-            $table->editColumn('requisitoacumplir', function ($row) {
-                return $row->requisitoacumplir ? $row->requisitoacumplir : "";
-            });
-            $table->editColumn('cumplerequisito', function ($row) {
-                return $row->cumplerequisito ? MatrizRequisitoLegale::CUMPLEREQUISITO_SELECT[$row->cumplerequisito] : '';
             });
             $table->editColumn('formacumple', function ($row) {
                 return $row->formacumple ? $row->formacumple : "";
             });
+            $table->editColumn('requisitoacumplir', function ($row) {
+                return $row->requisitoacumplir ? $row->requisitoacumplir : "";
+            });
+            $table->editColumn('alcance', function ($row) {
+                return $row->alcance ? $row->alcance : "";
+            });
+            $table->editColumn('medio', function ($row) {
+                return $row->medio ? $row->medio : "";
+            });
+            $table->editColumn('fechaexpedicion', function ($row) {
+                return $row->fechaexpedicion ? $row->fechaexpedicion : "";
+            });
+            $table->editColumn('fechavigor', function ($row) {
+                return $row->fechavigor ? $row->fechavigor : "";
+            });
             $table->editColumn('periodicidad_cumplimiento', function ($row) {
                 return $row->periodicidad_cumplimiento ? $row->periodicidad_cumplimiento : "";
             });
+            $table->editColumn('cumplerequisito', function ($row) {
+                return $row->cumplerequisito ? MatrizRequisitoLegale::CUMPLEREQUISITO_SELECT[$row->cumplerequisito] : '';
+            });
+            $table->editColumn('metodo', function ($row) {
+                return $row->metodo ? $row->metodo : "";
+            });
+            $table->editColumn('descripcion_cumplimiento', function ($row) {
+                return $row->descripcion_cumplimiento ? $row->descripcion_cumplimiento : "";
+            });
+            $table->editColumn('evidencia', function ($row) {
+                return $row->evidencias_matriz ? $row->evidencias_matriz : "";
+            });
+
+            $table->editColumn('reviso', function ($row) {
+                return $row->empleado ? $row->empleado->name : "";
+            });
+
+            $table->editColumn('puesto', function ($row) {
+                return $row->empleado ? $row->empleado->puesto : "";
+            });
+
+            $table->editColumn('area', function ($row) {
+                return $row->empleado ? $row->empleado->area->area : "";
+            });
+
+            $table->editColumn('comentarios', function ($row) {
+                return $row->comentarios ? $row->comentarios : "";
+            });
+
 
             $table->rawColumns(['actions', 'placeholder']);
 
@@ -68,37 +112,165 @@ class MatrizRequisitoLegalesController extends Controller
 
         $teams = Team::get();
 
-        return view('admin.matrizRequisitoLegales.index', compact('teams'));
+        $empleados = Empleado::get();
+
+        return view('admin.matrizRequisitoLegales.index', compact('teams','empleados'));
     }
 
     public function create()
     {
         abort_if(Gate::denies('matriz_requisito_legale_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return view('admin.matrizRequisitoLegales.create');
+        $empleados = Empleado::get();
+
+        return view('admin.matrizRequisitoLegales.create',compact('empleados'));
     }
 
     public function store(StoreMatrizRequisitoLegaleRequest $request)
     {
         $matrizRequisitoLegale = MatrizRequisitoLegale::create($request->all());
 
-        return redirect()->route('admin.matriz-requisito-legales.index');
+        
+       
+        // $image=null;
+
+        // if($request->file('evidencia') !=null or !empty($request->file('evidencia'))){ 
+
+        //     $extension = pathinfo($request->file('evidencia')->getClientOriginalName(), PATHINFO_EXTENSION);
+        //     $name_image = basename(pathinfo($request->file('evidencia')->getClientOriginalName(), PATHINFO_BASENAME), "." . $extension);
+        //     $new_name_image = 'Evidencia_' . $matrizRequisitoLegale->id . '_' . $name_image . '.' . $extension;
+        //     $route = storage_path() . 'public/matriz_evidencias' . $new_name_image;
+        //     $image = $new_name_image;
+        //     if($extension == 'JPG' || $extension == 'PNG' || $extension == 'GIF'){
+        //     //Usamos image_intervention para disminuir el peso de la imagen
+        //     $img_intervention = Image::make($request->file('evidencia'));
+        //     $img_intervention->resize(256, null, function ($constraint) {
+        //         $constraint->aspectRatio();
+        //     })->save($route);
+        // }
+
+        // // $file = $request->file('evidencia');   
+        // // // Save the file
+        // // $path = $file->storeAs('public/matriz_requisitos', $new_name_image);
+        // // dd($path);
+        
+        // $paths = [];
+        //     $files = $request->file('evidencia');
+        //     // dd($files);
+        //     foreach ($files as $file)
+        //     {
+        //         // Generate a file name with extension
+        //         // Save the file
+        //         $paths[] = $file->storeAs('public/matriz_evidencias', $new_name_image);
+        //     }
+          
+
+        // }
+
+
+
+        $files = $request->file('files');
+        
+        // $new_files = 'Evidencia_' . $matrizRequisitoLegale->id . '_' . ;
+
+
+        foreach($files as $file){
+            if (Storage::putFileAs('public/matriz_evidencias',$file, $file->getClientOriginalName())){
+      
+             EvidenciaMatrizRequisitoLegale::create([
+            'evidencia'=> $file->getClientOriginalName(),
+            'id_matriz_requisito' =>$matrizRequisitoLegale->id,
+            ]); 
+            }
+        }
+        //  dd($files);
+      
+
+        // EvidenciaMatrizRequisitoLegale::create([
+        //     'nombrerequisito' => $request->nombrerequisito,
+        //     'fechaexpedicion'=> $request->fechaexpedicion,
+        //     'fechavigor'=> $request->fechavigor,
+        //     'requisitoacumplir'=> $request->requisitoacumplir,
+        //     'cumplerequisito'=> $request->cumplerequisito,
+        //     'formacumple'=> $request->formacumple,
+        //     'periodicidad_cumplimiento'=> $request->periodicidad_cumplimiento,
+        //     'fechaverificacion'=> $request->fechaverificacion,
+        //     'medio'=> $request->medio,
+        //     'tipo'=> $request->tipo,
+        //     'descripcion_cumplimiento'=> $request->descripcion_cumplimiento,
+        //     'id_reviso'=> $request->id_reviso,
+        //     'evidencia'=> $name_image,
+        //     'id_matriz_requisito' =>$matrizRequisitoLegale->id,
+            
+            
+        // ]); 
+
+
+        return redirect()->route('admin.matriz-requisito-legales.index')->with("success", 'Guardado con éxito');
     }
 
     public function edit(MatrizRequisitoLegale $matrizRequisitoLegale)
     {
         abort_if(Gate::denies('matriz_requisito_legale_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
+        $empleados = Empleado::get();
+
         $matrizRequisitoLegale->load('team');
 
-        return view('admin.matrizRequisitoLegales.edit', compact('matrizRequisitoLegale'));
+
+        return view('admin.matrizRequisitoLegales.edit', compact('matrizRequisitoLegale','empleados'));
     }
 
     public function update(UpdateMatrizRequisitoLegaleRequest $request, MatrizRequisitoLegale $matrizRequisitoLegale)
     {
-        $matrizRequisitoLegale->update($request->all());
+        
 
-        return redirect()->route('admin.matriz-requisito-legales.index');
+        $matrizRequisitoLegale->update([
+
+            'nombrerequisito' => $request->nombrerequisito,
+            'fechaexpedicion'=> $request->fechaexpedicion,
+            'fechavigor'=> $request->fechavigor,
+            'requisitoacumplir'=> $request->requisitoacumplir,
+            'cumplerequisito'=> $request->cumplerequisito,
+            'formacumple'=> $request->formacumple,
+            'periodicidad_cumplimiento'=> $request->periodicidad_cumplimiento,
+            'fechaverificacion'=> $request->fechaverificacion,
+            'medio'=> $request->medio,
+            'tipo'=> $request->tipo,
+            'descripcion_cumplimiento'=> $request->descripcion_cumplimiento,
+            'evidencia'=> $request->evidencia,
+            'id_reviso'=> $request->id_reviso,
+        ]);
+
+        dd($matrizRequisitoLegale, $request);
+
+        $image=null;
+
+        if($request->file('evidencia') !=null or !empty($request->file('evidencia'))){ 
+
+            $extension = pathinfo($request->file('evidencia')->getClientOriginalName(), PATHINFO_EXTENSION);
+            $name_image = basename(pathinfo($request->file('evidencia')->getClientOriginalName(), PATHINFO_BASENAME), "." . $extension);
+            $new_name_image = 'Evidencia_' . $matriz_requisito->id . '_' . $name_image . '.' . $extension;
+            $route = storage_path() . '/app/public/matriz_evidencias/' . $new_name_image;
+            $image = $new_name_image;
+            //Usamos image_intervention para disminuir el peso de la imagen
+            $img_intervention = Image::make($request->file('evidencia'));
+            $img_intervention->resize(256, null, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save($route);
+        }
+
+
+        EvidenciaMatrizRequisitoLegale::update([
+            'evidencia'=> $name_image,
+            'id_matriz_requisito' =>$matrizRequisitoLegale->id,
+            
+            
+        ]); 
+
+        
+
+        return redirect()->route('admin.matriz-requisito-legales.index')->with("success", 'Editado con éxito');
     }
 
     public function show(MatrizRequisitoLegale $matrizRequisitoLegale)
--- app/Models/MatrizRequisitoLegale.php
+++ app/Models/MatrizRequisitoLegale.php
@@ -19,15 +19,39 @@ class MatrizRequisitoLegale extends Model
         'nombrerequisito',
     ];
 
+    const PERIODICIDAD_SELECT = [
+        'Diaria' => 'Diaria',
+        'Semanal' => 'Semanal',
+        'Catorcenal' => 'Catorcenal',
+        'Quincenal' => 'Quincenal',
+        'Mensual' => 'Mensual',
+        'Bimestral' => 'Bimestral',
+        'Trimestral' => 'Trimestral',
+        'Semestral' => 'Semestral',
+        'Anual' => 'Anual',
+        'Multianual' => 'Multianual',
+        'Permanente' => 'Permanente',
+        'Única vez' => 'Única vez',
+    ];
+
+    const TIPO_SELECT = [
+        'Legal' => 'Legal',
+        'Normativo' => 'Normativo',
+        'Regulatorio' => 'Regulatorio',
+        'Contractual' => 'Contractual',
+        'Otro' => 'Otro',
+        
+    ];
+
     const CUMPLEREQUISITO_SELECT = [
         'Si' => 'Si',
         'No' => 'No',
     ];
 
     protected $dates = [
-        'fechaexpedicion',
-        'fechavigor',
-        'fechaverificacion',
+        // 'fechaexpedicion',
+        // 'fechavigor',
+        // 'fechaverificacion',
         'created_at',
         'updated_at',
         'deleted_at',
@@ -42,6 +66,14 @@ class MatrizRequisitoLegale extends Model
         'formacumple',
         'periodicidad_cumplimiento',
         'fechaverificacion',
+        'medio',
+        'tipo',
+        'descripcion_cumplimiento',
+        'evidencia',
+        'id_reviso',
+        'alcance',
+        'metodo',
+        'comentarios',
         'created_at',
         'updated_at',
         'deleted_at',
@@ -53,38 +85,48 @@ class MatrizRequisitoLegale extends Model
         return $date->format('Y-m-d H:i:s');
     }
 
-    public function getFechaexpedicionAttribute($value)
-    {
-        return $value ? Carbon::parse($value)->format(config('panel.date_format')) : null;
-    }
+    // public function getFechaexpedicionAttribute($value)
+    // {
+    //     return $value ? Carbon::parse($value)->format(config('panel.date_format')) : null;
+    // }
 
-    public function setFechaexpedicionAttribute($value)
-    {
-        $this->attributes['fechaexpedicion'] = $value ? Carbon::createFromFormat(config('panel.date_format'), $value)->format('Y-m-d') : null;
-    }
+    // public function setFechaexpedicionAttribute($value)
+    // {
+    //     $this->attributes['fechaexpedicion'] = $value ? Carbon::createFromFormat(config('panel.date_format'), $value)->format('Y-m-d') : null;
+    // }
 
-    public function getFechavigorAttribute($value)
-    {
-        return $value ? Carbon::parse($value)->format(config('panel.date_format')) : null;
-    }
+    // public function getFechavigorAttribute($value)
+    // {
+    //     return $value ? Carbon::parse($value)->format(config('panel.date_format')) : null;
+    // }
 
-    public function setFechavigorAttribute($value)
-    {
-        $this->attributes['fechavigor'] = $value ? Carbon::createFromFormat(config('panel.date_format'), $value)->format('Y-m-d') : null;
-    }
+    // public function setFechavigorAttribute($value)
+    // {
+    //     $this->attributes['fechavigor'] = $value ? Carbon::createFromFormat(config('panel.date_format'), $value)->format('Y-m-d') : null;
+    // }
 
     public function getFechaverificacionAttribute($value)
     {
         return $value ? Carbon::parse($value)->format(config('panel.date_format')) : null;
     }
 
-    public function setFechaverificacionAttribute($value)
-    {
-        $this->attributes['fechaverificacion'] = $value ? Carbon::createFromFormat(config('panel.date_format'), $value)->format('Y-m-d') : null;
-    }
+    // public function setFechaverificacionAttribute($value)
+    // {
+    //     $this->attributes['fechaverificacion'] = $value ? Carbon::createFromFormat(config('panel.date_format'), $value)->format('Y-m-d') : null;
+    // }
 
     public function team()
     {
         return $this->belongsTo(Team::class, 'team_id');
+    }
+
+    public function empleado()
+	{
+        return $this->belongsTo(Empleado::class, 'id_reviso', 'id')->with('area');
+
+	}
+
+    public function evidencias_matriz(){
+        return $this->hasMany(EvidenciaMatrizRequisitoLegale::class, 'id_matriz_requisito');
     }
 }
