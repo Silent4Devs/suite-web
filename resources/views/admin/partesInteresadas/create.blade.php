ef7340184cb396c46989910df2f87c5ae63de02e
Karen Marai <karen_03_93@hotmail.com>
2021-09-10 19:46:21 -0500

    Añadí estilo de los select2

--- resources/views/admin/partesInteresadas/create.blade.php
+++ resources/views/admin/partesInteresadas/create.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.admin')
 @section('content')
 
+
     {{ Breadcrumbs::render('admin.partes-interesadas.create') }}
+
+   
 
     <div class="mt-4 card">
         <div class="py-3 col-md-10 col-sm-9 card-body verde_silent align-self-center" style="margin-top: -40px;">
