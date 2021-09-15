8d7a4d32c598e9644ca3ce44f54106f5f846e703
UrielReyes <uriel.santiago@silent4business.com>
2021-09-15 14:58:17 -0500

    Creacion de modelos para evaluacion 360

---
+++ app/Models/RH/EvaluacionCompetencia.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\RH;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EvaluacionCompetencia extends Model
+{
+    use HasFactory;
+    protected $table = 'ev360_competencia_evaluacion';
+    protected $guarded = ["id"];
+
+    public function competencia()
+    {
+        return $this->belongsTo('App\Models\RH\Competencia', 'competencia_id', 'id');
+    }
+}
---
+++ app/Models/RH/PeriodoEvaluacion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\RH;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PeriodoEvaluacion extends Model
+{
+    use HasFactory;
+}
