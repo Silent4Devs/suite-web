4c9917ca1a258af7d66bf7d2e7a1645e23df5fb8
BenitoLopez12 <benito.lopez.tecno@gmail.com>
2021-09-19 16:27:06 -0500

    cambios-domingo

--- resources/views/admin/objetivosseguridads/create.blade.php
+++ resources/views/admin/objetivosseguridads/create.blade.php
@@ -332,7 +332,7 @@
 
                             <div class="form-group col-sm-6">
 
-                                <div class="form-inline">
+                                <div class="form-inline mb-2">
                                     <div class="form-group mb-2">
                                         <input type="text" readonly class="form-control-plaintext" id="staticEmail2"
                                             value="Añadir una variable:" disabled>
@@ -360,7 +360,7 @@
                         <div class="text-center form-group col-12" style="margin-left:15px;">
                             <a href="{{ redirect()->getUrlGenerator()->previous() }}"
                                 class="btn_cancelar">Cancelar</a>
-                            <button class="btn btn-info" type="submit">
+                            <button class="btn btn-danger" type="submit">
                                 {{ trans('global.save') }}
                             </button>
                         </div>
@@ -421,7 +421,7 @@
                     '<td><button type="button" name="usar" id="' + i +
                     '" class="btn btn-info btnAñadir" value="' + variable + '">Usar</button></td>' +
                     '<td><button type="button" name="remove" id="' + i +
-                    '" class="btn btn-danger btn_remove">Quitar</button></td>' +
+                    '" class="btn btn-info btn_remove">Quitar</button></td>' +
                     '</tr>'; //esto seria lo que contendria la fila
 
                 $('#mytable tr:first').after(fila);
