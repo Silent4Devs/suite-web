624aba449e2bf202ecabb06ea3f2575932581de4
Pedro Andres Garcia Diaz <pedro.garcia@s4b.com>
2021-09-21 09:38:45 -0500

    se homologaron formatos y botones asi como correciones de ortografia

--- resources/views/admin/amenazas/index.blade.php
+++ resources/views/admin/amenazas/index.blade.php
@@ -14,20 +14,7 @@
                 @include('csvImport.modal', ['model' => 'Amenaza', 'route' => 'admin.amenazas.parseCsvImport'])
             </div>
         </div>
-        <div class="px-1 py-2 mx-3 rounded shadow" style="background-color: #DBEAFE; border-top:solid 3px #3B82F6;">
-            <div class="row w-100">
-                <div class="text-center col-1 align-items-center d-flex justify-content-center">
-                    <div class="w-100">
-                        <i class="fas fa-info-circle" style="color: #3B82F6; font-size: 22px"></i>
-                    </div>
-                </div>
-                <div class="col-11">
-                    <p class="m-0" style="font-size: 16px; font-weight: bold; color: #1E3A8A">Instrucciones</p>
-                    <p class="m-0" style="font-size: 14px; color:#1E3A8A ">Por favor registre cada una de las sedes
-                        con las que cuenta su organización</p>
-                </div>
-            </div>
-        </div>
+
         @include('flash::message')
         @include('partials.flashMessages')
         <div class="card-body datatable-fix">
