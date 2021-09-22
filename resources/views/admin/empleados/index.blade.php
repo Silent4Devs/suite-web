fd646abab63a5997ca312701fd1d536550d50b05
Karen Marai <karen_03_93@hotmail.com>
2021-09-21 22:22:48 -0500

    Termine modulo de empleados

--- resources/views/admin/empleados/index.blade.php
+++ resources/views/admin/empleados/index.blade.php
@@ -173,13 +173,13 @@
                 var ids = $.map(dt.rows({ selected: true }).data(), function (entry) {
                 return entry.id
                 });
-            
+
                 if (ids.length === 0) {
                 alert('{{ trans('global.datatables.zero_selected') }}')
-            
+
                 return
                 }
-            
+
                 if (confirm('{{ trans('global.areYouSure') }}')) {
                 $.ajax({
                 headers: {'x-csrf-token': _token},
@@ -208,7 +208,8 @@
                         data: 'foto',
                         name: 'foto',
                         render: function(data, type, row, meta) {
-                            return `<div class="text-center w-100"><img style="width:${data!=""?"50px":"35px"}" src="{{ asset('storage/empleados/imagenes/') }}/${data !=""?data:"user.png"}"></div>`;
+                            return `<div class="text-center"><img style="width: 50px;height: 50px;border-radius: 50%;" src="{{ asset('storage/empleados/imagenes/') }}/${data !=""?data:"user.png"}"></div>`;
+
                         }
                     },
                     {
