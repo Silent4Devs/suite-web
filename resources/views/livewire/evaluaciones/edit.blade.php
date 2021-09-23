b5284fcd60c81350a17997ab00deaf43f4e04ce2
john <vojohn95@gmail.com>
2021-09-23 18:44:28 -0500

    Se añadio acceso a formularios

--- resources/views/livewire/evaluaciones/edit.blade.php
+++ resources/views/livewire/evaluaciones/edit.blade.php
@@ -4,19 +4,17 @@
     @include('livewire.evaluaciones.form')
     <div class="row">
         <div class="col-sm-12 col-md-12 col-lg-12">
-            <div class="form-group col-4"> 
+            <div class="form-group col-4">
                 <button type="submit" class="btn-success btn green" wire:loading.attr="disabled" wire:target="update"
                     wire:click.prevent="update()">
                     <i class="fas fa-spinner fa-pulse" wire:loading wire:target="update"></i>
                     <span wire:loading.remove wire:target="update">Actualizar</span>
                     {{-- <span wire:loading wire:target="update">Actualizando</span> --}}
                 </button>
-            </div>
-            <div class="form-group col-4">
                 <button  wire:click="default" class="btn btn-danger rounded">
                     Cancelar
                 </button>
             </div>
-        </div> 
+        </div>
     </div>
 </div>
