059926736a789ec54c67d008ecb8b7e3a4a4efef
john <vojohn95@gmail.com>
2021-09-22 19:16:28 -0500

    configuracion

--- app/Providers/TenancyServiceProvider.php
+++ app/Providers/TenancyServiceProvider.php
@@ -103,6 +103,10 @@ class TenancyServiceProvider extends ServiceProvider
         $this->mapRoutes();
 
         $this->makeTenancyMiddlewareHighestPriority();
+
+        \Stancl\Tenancy\Middleware\InitializeTenancyByDomain::$onFail = function () {
+            return redirect('localhost');
+        };
     }
 
     protected function bootEvents()
