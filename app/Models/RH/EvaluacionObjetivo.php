b36eccb39843f375b2525c0a29c1017ac3e5c55c
Uriel Reyes <urielreyes514@gmail.com>
2021-09-30 10:03:53 -0500

    Creacion de logica para objetivos y competencias en modelos

---
+++ app/Models/RH/EvaluacionObjetivo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\RH;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EvaluacionObjetivo extends Model
+{
+    use HasFactory;
+    protected $table = 'ev360_evaluacion_objetivos';
+    protected $guarded = ["id"];
+
+    public function objetivo()
+    {
+        return $this->belongsTo('App\Models\RH\Objetivo', 'objetivo_id', 'id');
+    }
+}
