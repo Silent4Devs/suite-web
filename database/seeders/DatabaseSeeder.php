4b6bd6b05986c1117e3a0f9ae6cda2f3952dbc95
Uriel Reyes <urielreyes514@gmail.com>
2021-10-07 10:09:24 -0500

    Creacion de migracion para perfil por empleado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PerfilEmpleado;
 use App\Models\PlanImplementacion;
 use App\Models\Puesto;
 use Illuminate\Database\Seeder;
@@ -31,6 +32,7 @@ class DatabaseSeeder extends Seeder
             GrupoSeeder::class,
             AreaSeeder::class,
             PuestoSeeder::class,
+            PerfilEmpleadosSeeder::class,
             EmpleadosSeeder::class,
             MacroprocesoSeeder::class,
             DocumentoSeeder::class,
