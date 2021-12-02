76e2063e11292ef72c95fa49f891b20db5cd8757
Uriel Santiago Reyes Paredes <uriel.santiago@silent4business.com>
2021-12-02 10:42:37 -0600

    Funcionalidad Bloqueo de usuario

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,25 +29,27 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
+            // \App\Http\Middleware\RedirectIfAuthenticated::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\AuthGates::class,
             \App\Http\Middleware\SetLocale::class,
+
         ],
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\AuthGates::class,
         ],
-        'universal' => [
-
-        ],
+        'universal' => [],
     ];
 
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'active' => \App\Http\Middleware\ActiveUser::class,
+        'isActive' => \App\Http\Middleware\IsActiveUser::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
