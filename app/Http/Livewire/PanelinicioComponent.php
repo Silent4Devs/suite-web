d604ab7f8cbe35b245db50a61368bb0ddb57c67e
john <vojohn95@gmail.com>
2021-12-03 18:19:36 -0600

    falta

--- app/Http/Livewire/PanelinicioComponent.php
+++ app/Http/Livewire/PanelinicioComponent.php
@@ -1,13 +1,50 @@
 <?php
 
 namespace App\Http\Livewire;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+
 
 use Livewire\Component;
 
 class PanelinicioComponent extends Component
 {
+    use LivewireAlert;
+
+    public $nombre_id, $nempleado_id, $area_id, $jefe_id, $puesto_id, $perfil_id;
+
     public function render()
     {
         return view('livewire.panelinicio-component');
     }
+
+    public function updatedNombreId($value)
+    {
+        $this->nombre_id = $value;
+    }
+
+    public function updatedNempleadoId($value)
+    {
+        $this->nempleado_id = $value;
+    }
+
+    public function updatedAreaId($value)
+    {
+        $this->area_id = $value;
+    }
+
+    public function updatedJefeId($value)
+    {
+        $this->jefe_id = $value;
+    }
+
+    public function updatedPuestoId($value)
+    {
+        $this->puesto_id = $value;
+    }
+
+    public function updatedPerfilId($value)
+    {
+        $this->perfil_id = $value;
+    }
+
 }
--- resources/views/livewire/panelinicio-component.blade.php
+++ resources/views/livewire/panelinicio-component.blade.php
@@ -1,3 +1,129 @@
-<div>
-    adsad
+<div class="container">
+    <div class="row">
+        <div class="form-group col-sm-3">
+            <label class="required" for="nombre_id"><i class="fas fa-street-view iconos-crear"></i>Nombre</label>
+            <br>
+            <label class="c-switch c-switch-lg c-switch-pill c-switch-label c-switch-info" style="margin-left: 30px;">
+                <input type="checkbox" wire:model.debounce.800ms="nombre_id" class="c-switch-input" checked>
+                <span class="c-switch-slider" data-checked="On" data-unchecked="Off"></span>
+            </label>
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="nempleado_id"><i class="fas fa-street-view iconos-crear"></i>N°
+                de
+                empleado</label>
+            <br>
+            <label class="c-switch c-switch-lg c-switch-pill c-switch-label c-switch-info" style="margin-left: 30px;">
+                <input type="checkbox" wire:model.debounce.800ms="nempleado_id" class="c-switch-input" checked>
+                <span class="c-switch-slider" data-checked="On" data-unchecked="Off"></span>
+            </label>
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="jefe_id"><i class="fas fa-user iconos-crear"></i>Jefe
+                Inmediato</label>
+            <br>
+            <label class="c-switch c-switch-lg c-switch-pill c-switch-label c-switch-info" style="margin-left: 30px;">
+                <input type="checkbox" wire:model.debounce.800ms="jefe_id" class="c-switch-input" checked>
+                <span class="c-switch-slider" data-checked="On" data-unchecked="Off"></span>
+            </label>
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="n_empleado"><i class="fas fa-street-view iconos-crear"></i>Área</label>
+            <br>
+            <label class="c-switch c-switch-lg c-switch-pill c-switch-label c-switch-info" style="margin-left: 30px;">
+                <input type="checkbox" wire:model.debounce.800ms="area_id" class="c-switch-input" checked>
+                <span class="c-switch-slider" data-checked="On" data-unchecked="Off"></span>
+            </label>
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="puesto_id"><i class="fas fa-briefcase iconos-crear"></i>Puesto</label>
+            <br>
+            <label class="c-switch c-switch-lg c-switch-pill c-switch-label c-switch-info" style="margin-left: 30px;">
+                <input type="checkbox" wire:model.debounce.800ms="puesto_id" class="c-switch-input" checked>
+                <span class="c-switch-slider" data-checked="On" data-unchecked="Off"></span>
+            </label>
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="n_empleado"><i class="fas fa-street-view iconos-crear"></i>N°
+                de
+                empleado</label>
+            <br>
+            <label class="c-switch c-switch-lg c-switch-pill c-switch-label c-switch-info" style="margin-left: 30px;">
+                <input type="checkbox" wire:model.debounce.800ms="puesto_id" class="c-switch-input" checked>
+                <span class="c-switch-slider" data-checked="On" data-unchecked="Off"></span>
+            </label>
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="n_empleado"><i class="fas fa-street-view iconos-crear"></i>N°
+                de
+                empleado</label>
+            <input class="form-control {{ $errors->has('n_empleado') ? 'is-invalid' : '' }}" type="text"
+                name="n_empleado" id="n_empleado" value="{{ old('n_empleado', '') }}" required>
+            @error('n_empleado')
+                <span class="invalid-feedback d-block" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="n_empleado"><i class="fas fa-street-view iconos-crear"></i>N°
+                de
+                empleado</label>
+            <input class="form-control {{ $errors->has('n_empleado') ? 'is-invalid' : '' }}" type="text"
+                name="n_empleado" id="n_empleado" value="{{ old('n_empleado', '') }}" required>
+            @error('n_empleado')
+                <span class="invalid-feedback d-block" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="n_empleado"><i class="fas fa-street-view iconos-crear"></i>N°
+                de
+                empleado</label>
+            <input class="form-control {{ $errors->has('n_empleado') ? 'is-invalid' : '' }}" type="text"
+                name="n_empleado" id="n_empleado" value="{{ old('n_empleado', '') }}" required>
+            @error('n_empleado')
+                <span class="invalid-feedback d-block" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="n_empleado"><i class="fas fa-street-view iconos-crear"></i>N°
+                de
+                empleado</label>
+            <input class="form-control {{ $errors->has('n_empleado') ? 'is-invalid' : '' }}" type="text"
+                name="n_empleado" id="n_empleado" value="{{ old('n_empleado', '') }}" required>
+            @error('n_empleado')
+                <span class="invalid-feedback d-block" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="n_empleado"><i class="fas fa-street-view iconos-crear"></i>N°
+                de
+                empleado</label>
+            <input class="form-control {{ $errors->has('n_empleado') ? 'is-invalid' : '' }}" type="text"
+                name="n_empleado" id="n_empleado" value="{{ old('n_empleado', '') }}" required>
+            @error('n_empleado')
+                <span class="invalid-feedback d-block" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="form-group col-sm-3">
+            <label class="required" for="n_empleado"><i class="fas fa-street-view iconos-crear"></i>N°
+                de
+                empleado</label>
+            <input class="form-control {{ $errors->has('n_empleado') ? 'is-invalid' : '' }}" type="text"
+                name="n_empleado" id="n_empleado" value="{{ old('n_empleado', '') }}" required>
+            @error('n_empleado')
+                <span class="invalid-feedback d-block" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+    </div>
 </div>
