ea8c1d4b347d452a18d14a8522274cecfb446494
Karen Marai <karen_03_93@hotmail.com>
2021-12-16 11:11:43 -0600

    modelo de empleados

--- app/Models/Empleado.php
+++ app/Models/Empleado.php
@@ -72,7 +72,7 @@ class Empleado extends Model
 
     //public $preventsLazyLoading = true;
     //protected $with = ['children:id,name,foto,puesto as title,area,supervisor_id']; //Se desborda la memoria al entrar en un bucle infinito se opto por utilizar eager loading
-    protected $appends = ['avatar', 'resourceId', 'empleados_misma_area', 'genero_formateado', 'puesto', 'declaraciones_responsable', 'declaraciones_aprobador','fecha_ingreso'];
+    protected $appends = ['avatar', 'resourceId', 'empleados_misma_area', 'genero_formateado', 'puesto', 'declaraciones_responsable', 'declaraciones_aprobador', 'fecha_ingreso'];
     //, 'jefe_inmediato', 'empleados_misma_area'
     protected $fillable = [
         'name',
