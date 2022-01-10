2e0675b4ded59cc6078051fb10e3571230d58826
BenitoLopez12 <benito.lopez.tecno@gmail.com>
2022-01-10 11:42:56 -0600

    look-sedes

--- app/Models/PartesInteresadasClausula.php
+++ app/Models/PartesInteresadasClausula.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class PartesInteresadasClausula
+ * Class PartesInteresadasClausula.
  *
  * @property int $id
  * @property int $clausula_id
@@ -15,30 +15,28 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property Clausula $clausula
  * @property PartesInteresada $partes_interesada
- *
- * @package App\Models
  */
 class PartesInteresadasClausula extends Model
 {
-	protected $table = 'partes_interesadas_clausula';
+    protected $table = 'partes_interesadas_clausula';
 
-	protected $casts = [
-		'clausula_id' => 'int',
-		'partesint_id' => 'int',
-	];
+    protected $casts = [
+        'clausula_id' => 'int',
+        'partesint_id' => 'int',
+    ];
 
-	protected $fillable = [
-		'clausula_id',
-		'partesint_id'
-	];
+    protected $fillable = [
+        'clausula_id',
+        'partesint_id',
+    ];
 
-	public function clausula()
-	{
-		return $this->belongsTo(Clausula::class);
-	}
+    public function clausula()
+    {
+        return $this->belongsTo(Clausula::class);
+    }
 
-	public function partes_interesada()
-	{
-		return $this->belongsTo(PartesInteresada::class, 'partesint_id');
-	}
+    public function partes_interesada()
+    {
+        return $this->belongsTo(PartesInteresada::class, 'partesint_id');
+    }
 }
