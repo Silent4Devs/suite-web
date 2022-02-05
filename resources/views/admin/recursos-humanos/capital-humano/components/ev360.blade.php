a427a4db49efb0fcb61e3f95c705a07e570a74fa
Beni Mejia <benito.lopez.tecno@gmail.com>
2022-02-04 19:23:18 -0600

    entrga-v

--- resources/views/admin/recursos-humanos/capital-humano/components/ev360.blade.php
+++ resources/views/admin/recursos-humanos/capital-humano/components/ev360.blade.php
@@ -1,36 +1,5 @@
 <ul class="mt-4">
     <li>
-        <a href="{{ route('admin.ev360-competencias.index') }}">
-            <div style="text-transform: capitalize">
-                <i class="bi bi-star-half"></i><br>
-                <p class="m-0 mt-2">
-                    Definir Competencias
-                </p>
-            </div>
-        </a>
-    </li>
-    <li>
-        <a href="{{ route('admin.ev360-competencias-por-puesto.index') }}">
-            <div style="text-transform: capitalize">
-                <i class="bi bi-bookmark-star"></i><br>
-                <p class="m-0 mt-2">
-                    Competencias Por Puesto
-                </p>
-            </div>
-        </a>
-    </li>
-    <li>
-        <a href="{{ route('admin.ev360-objetivos.index') }}">
-            <div style="text-transform: capitalize">
-                <i class="bi bi-bullseye"></i><br>
-                <p class="m-0 mt-2">
-                    Asignar
-                    Objetivos Estratégicos
-                </p>
-            </div>
-        </a>
-    </li>
-    <li>
         <a href="{{ route('admin.ev360-evaluaciones.create') }}">
             <div style="text-transform: capitalize">
                 <i class="bi bi-clipboard-check"></i><br>
@@ -44,12 +13,12 @@
     </li>
     <li>
         <a href="{{ route('admin.ev360-evaluaciones.index') }}">
-            <div style="text-transform: capitalize">
+            <div>
                 <i class="bi bi-patch-check"></i><br>
                 <p class="m-0 mt-2">
+                    Seguimiento de 
                     Evaluaciones
                     <br>
-                    Creadas
                 </p>
             </div>
         </a>
