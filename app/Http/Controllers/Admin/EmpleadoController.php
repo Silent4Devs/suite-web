d3ddf9045c55dda1fd1c9433e40b81f3934ef583
Rosa Flores <rosa.flores@silent4business.com>
2022-02-08 18:55:29 -0600

    se mejoro campos de domicilio

--- app/Http/Controllers/Admin/EmpleadoController.php
+++ app/Http/Controllers/Admin/EmpleadoController.php
@@ -31,6 +31,8 @@ use Illuminate\Support\Str;
 use Intervention\Image\Facades\Image;
 use Symfony\Component\HttpFoundation\Response;
 use Yajra\DataTables\Facades\DataTables;
+use PDF;
+
 
 class EmpleadoController extends Controller
 {
@@ -345,6 +347,7 @@ class EmpleadoController extends Controller
 
     public function createEmpleado($request)
     {
+        // dd($request);
         $empleado = Empleado::create([
             'name' => $request->name,
             'area_id' =>  $request->area_id,
@@ -370,7 +373,16 @@ class EmpleadoController extends Controller
             'renovacion_contrato' => $request->renovacion_contrato,
             'esquema_contratacion' => $request->esquema_contratacion,
             'proyecto_asignado' => $request->proyecto_asignado,
+            // 'domicilio_personal' => $request->domicilio_personal,
+            'calle' => $request->calle,
+            'num_exterior' => $request->num_exterior,
             'domicilio_personal' => $request->domicilio_personal,
+            'num_interior' => $request->num_interior,
+            'colonia' => $request->colonia,
+            'delegacion' => $request->delegacion,
+            'estado' => $request->estado,
+            'pais' => $request->pais,
+            'cp' => $request->cp,
             'telefono_casa' => $request->telefono_casa,
             'correo_personal' => $request->correo_personal,
             'estado_civil' => $request->estado_civil,
@@ -1103,7 +1115,16 @@ class EmpleadoController extends Controller
             'renovacion_contrato' => $request->renovacion_contrato,
             'esquema_contratacion' => $request->esquema_contratacion,
             'proyecto_asignado' => $request->proyecto_asignado,
+            // 'domicilio_personal' => $request->domicilio_personal,
+            'calle' => $request->calle,
+            'num_exterior' => $request->num_exterior,
             'domicilio_personal' => $request->domicilio_personal,
+            'num_interior' => $request->num_interior,
+            'colonia' => $request->colonia,
+            'delegacion' => $request->delegacion,
+            'estado' => $request->estado,
+            'pais' => $request->pais,
+            'cp' => $request->cp,
             'telefono_casa' => $request->telefono_casa,
             'correo_personal' => $request->correo_personal,
             'estado_civil' => $request->estado_civil,
@@ -1443,4 +1464,31 @@ class EmpleadoController extends Controller
 
         return $empleados;
     }
+
+    // public function datosEmpleado($id){
+    //     // dd($request);
+    //     $visualizarEmpleados = Empleado::all();
+    //     // dd($visualizarEmpleados);
+
+    //     return view('admin.empleados.datosEmpleado', compact('visualizarEmpleados'));
+    // }
+
+
+    // public function createPDF(){
+    //     $visualizarEmpleados = Empleado::all();
+    //     $datos = Empleado::get();
+    //     $data = compact('datos', 'visualizarEmpleados');
+    //     // $imprimir = PDF::loadView('admin.empleados.datosEmpleado', $data);
+    //     // return $imprimir->stream();
+
+    //     $imprimir = PDF::loadView('admin.empleados.datosEmpleado', $data)->setOptions(['defaultFont' => 'sans-serif']);
+    //     return $imprimir->stream();
+
+    //     // return $imprimir->download('archivo.pdf');
+    //     // return $imprimir->download('archivo-pdf.pdf');
+
+    // }
+
+
+
 }
