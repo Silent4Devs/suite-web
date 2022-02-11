4d6de9a94f4f2bb170dbcb24f5c56869ab975122
Karen Marai <karen_03_93@hotmail.com>
2022-02-10 19:01:52 -0600

    Correcciones de analisis y husky

--- app/Http/Controllers/Admin/AnalisisdeRiesgosController.php
+++ app/Http/Controllers/Admin/AnalisisdeRiesgosController.php
@@ -73,12 +73,11 @@ class AnalisisdeRiesgosController extends Controller
             $table->editColumn('estatus', function ($row) {
                 if ($row->estatus == 1) {
                     return $row->estatus ? 'En proceso' : '';
-                } elseif($row->estatus ==2){
+                } elseif ($row->estatus == 2) {
                     return $row->estatus ? 'En revisión' : '';
-                }else{
+                } else {
                     return $row->estatus ? 'Aprobado' : '';
                 }
-
             });
             $table->editColumn('enlace', function ($row) {
                 return $row->id ? $row->id : '';
