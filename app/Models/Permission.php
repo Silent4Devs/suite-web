25d8f18273df5312cc2defa886c562ae3b24d9e8
Uriel Santiago Reyes Paredes <uriel.santiago@silent4business.com>
2022-02-22 10:04:58 -0600

    husky

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -7,7 +7,6 @@ use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Permission extends Model
 {
