ab58b07f8437554190a774c2fd9032aa08d56cc1
Uriel Santiago Reyes Paredes <uriel.santiago@silent4business.com>
2022-03-14 10:45:07 -0600

    correccion para mis datos

--- resources/views/admin/inicioUsuario/mis-datos.blade.php
+++ resources/views/admin/inicioUsuario/mis-datos.blade.php
@@ -917,10 +917,13 @@ if (!is_null($organizacion)) {
                                         style="padding-top:17px !important; padding-bottom:17px !important;">
                                         <h5 class="mb-0 d-inline-block"><i class="bi bi-person-badge mr-2"></i>Mi
                                             Autoevaluación
-                                            @if (!$mis_evaluaciones->evaluado)
-                                                <div class="circle-total-evaluaciones" style="top:-5px !important;">
-                                                    <span style="position: absolute;top: 3px;">1</span>
-                                                </div>
+                                            @if ($mis_evaluaciones)
+                                                @if (!$mis_evaluaciones->evaluado)
+                                                    <div class="circle-total-evaluaciones"
+                                                        style="top:-5px !important;">
+                                                        <span style="position: absolute;top: 3px;">1</span>
+                                                    </div>
+                                                @endif
                                             @endif
                                         </h5>
 
