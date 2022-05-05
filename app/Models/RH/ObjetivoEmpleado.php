695f0c9e9667d6036b4d36cd0ebe7807c0328605
Uriel Reyes <urielreyes514@gmail.com>
2022-05-05 10:55:51 -0500

    Se eliminó el query cachable de objetivos de empleados y se comentó temporalmente el botón de definir nuevos objetivos por confusiones

--- app/Models/RH/ObjetivoEmpleado.php
+++ app/Models/RH/ObjetivoEmpleado.php
@@ -4,13 +4,10 @@ namespace App\Models\RH;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class ObjetivoEmpleado extends Model
 {
-    use HasFactory, QueryCacheable;
-    public $cacheFor = 3600;
-    protected static $flushCacheOnUpdate = true;
+    use HasFactory;
     protected $table = 'ev360_objetivo_empleados';
     protected $fillable = ['empleado_id', 'objetivo_id', 'completado', 'en_curso'];
 
