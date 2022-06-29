d50aa199d0e1d66f5458921be27f010ae6ffd8dd
Uriel Reyes <urielreyes514@gmail.com>
2022-06-29 17:09:54 -0500

    husky

--- app/Models/DeclaracionAplicabilidadAprobadores.php
+++ app/Models/DeclaracionAplicabilidadAprobadores.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Rennokki\QueryCache\Traits\QueryCacheable;
 
 /**
  * Class DeclaracionAplicabilidadAprobadore.
