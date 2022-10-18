c883ff9a56837a73d9fea0480ed82196126567a6
Uriel Reyes <uriel.santiago@silent4business.com>
2022-10-18 16:38:30 -0500

    Correccion de vista

--- resources/views/admin/minutasaltadireccions/show.blade.php
+++ resources/views/admin/minutasaltadireccions/show.blade.php
@@ -1,76 +1,14 @@
 @extends('layouts.admin')
 @section('content')
-
     {{ Breadcrumbs::render('admin.minutasaltadireccions.create') }}
 
-<div class="card">
-    <div class="card-header">
-        {{ trans('global.show') }} {{ trans('cruds.minutasaltadireccion.title') }}
-    </div>
+    <div class="card">
 
-    <div class="card-body">
-        <div class="form-group">
-            <div class="form-group">
-                <a class="btn btn-default" href="{{ route('admin.minutasaltadireccions.index') }}">
-                    {{ trans('global.back_to_list') }}
-                </a>
-            </div>
-            <table class="table table-bordered table-striped">
-                <tbody>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.minutasaltadireccion.fields.id') }}
-                        </th>
-                        <td>
-                            {{ $minutasaltadireccion->id }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.minutasaltadireccion.fields.objetivoreunion') }}
-                        </th>
-                        <td>
-                            {{ $minutasaltadireccion->objetivoreunion }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.minutasaltadireccion.fields.responsablereunion') }}
-                        </th>
-                        <td>
-                            {{ $minutasaltadireccion->responsablereunion->name ?? '' }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.minutasaltadireccion.fields.arearesponsable') }}
-                        </th>
-                        <td>
-                            {{ $minutasaltadireccion->arearesponsable }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.minutasaltadireccion.fields.fechareunion') }}
-                        </th>
-                        <td>
-                            {{ $minutasaltadireccion->fechareunion }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.minutasaltadireccion.fields.archivo') }}
-                        </th>
-                        <td>
-                            @if($minutasaltadireccion->archivo)
-                                <a href="{{ $minutasaltadireccion->archivo->getUrl() }}" target="_blank">
-                                    {{ trans('global.view_file') }}
-                                </a>
-                            @endif
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="card-body">
+            Previsualizacion dla minuta: <strong>{{ $minutasaltadireccion->documento }}</strong>
+            <iframe src="{{ asset('storage/minutas/' . 'en aprobacion/' . $minutasaltadireccion->documento) }}" class="w-100"
+                style="height: 500px" frameborder="0"></iframe>
+
             <div class="form-group">
                 <a class="btn btn-default" href="{{ route('admin.minutasaltadireccions.index') }}">
                     {{ trans('global.back_to_list') }}
@@ -78,8 +16,4 @@
             </div>
         </div>
     </div>
-</div>
-
-
-
 @endsection
