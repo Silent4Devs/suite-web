022648106d6c2687ed464c26aa249d2c6aeb64e9
Víctor Hugo Rodríguez Albarrá <vhraindiana@hotmail.com>
2023-05-11 18:48:30 -0600

    Correcciones schedule

--- app/Jobs/EnviarMailFelicitacionJob.php
+++ app/Jobs/EnviarMailFelicitacionJob.php
@@ -57,9 +57,7 @@ class EnviarMailFelicitacionJob implements ShouldQueue
                 $correodestinatario = $cumpleañero->email;
 
                 $email = new FelicitacionesMail($nombre, $correodestinatario, $imgpastel, $imgtab);
-                // Mail::to($correodestinatario)->send($email);
-                Mail::to('karen.rodriguez@silent4business.com')->send($email);
-                Mail::to('luis.vargas@silent4business.com')->send($email);
+                Mail::to($correodestinatario)->send($email);
                 Log::info('Correo de cumpleaños enviado correctamente');
                 // echo $nombre.'<br>';
                 // echo $correodestinatario.'<br>';
