8dcad67960da0916e0016678ecc4bc1bb27ee713
BenitoLM <benito.lopez.tecno@gmail.com>
2023-05-19 16:18:51 -0600

    cambios revelar

--- public/sw.js
+++ public/sw.js
@@ -35,12 +35,8 @@ var checkResponse = function (request) {
 
 var addToCache = function (request) {
     return caches.open("offline").then(function (cache) {
-        // return fetch(request).then(function (response) {
-        //     return cache.put(request, response);
-        // });
-
-        return fetch(request.clone()).then(function (response) {
-            return cache.put(request, response.clone());
+        return fetch(request).then(function (response) {
+            return cache.put(request, response);
         });
     });
 };
