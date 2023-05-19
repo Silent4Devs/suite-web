cca1d0b553a51b8afdc1d5c976652b18ca50f931
BenitoLM <benito.lopez.tecno@gmail.com>
2023-05-18 18:09:34 -0600

    crg

--- app/Http/Controllers/Admin/IndicadoresSgsiController.php
+++ app/Http/Controllers/Admin/IndicadoresSgsiController.php
@@ -280,8 +280,6 @@ class IndicadoresSgsiController extends Controller
             );
         }
         
-        dd(VariablesIndicador::where('id_indicador', $indicadoresSgsis->id)->get());
-        
         return redirect()->action('Admin\IndicadoresSgsiController@evaluacionesUpdate', ['id' => $indicadoresSgsis->id]);
     }
 
