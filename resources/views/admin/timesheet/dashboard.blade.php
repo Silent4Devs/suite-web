4f6cb8b588d887e51ee230e578fa7b6d31f08f86
Víctor Hugo Rodríguez Albarrá <victor.rodriguez@s4b.com>
2023-07-24 13:23:59 -0600

    Permisos para visualizar dashboard del timesheet

--- resources/views/admin/timesheet/dashboard.blade.php
+++ resources/views/admin/timesheet/dashboard.blade.php
@@ -15,32 +15,44 @@
     <div class="mt-5">
         <nav class="mt-4 d-flex justify-content-between">
             <div class="nav nav-tabs" id="tabsIso27001" role="tablist">
+                @can('visualizar_registros_dashboard_timesheet')
                 <a class="nav-link active" id="nav-registros-tab" data-type="registros" data-toggle="tab"
                     href="#nav-registros" role="tab" aria-controls="nav-registros" aria-selected="true">
                     Registros Timesheet
                 </a>
+                @endcan
+                @can('visualizar_registros_dashboard_empleados_timesheet')
                 <a class="nav-link" id="nav-empleados-tab" data-type="empleados" data-toggle="tab" href="#nav-empleados"
                     role="tab" aria-controls="nav-empleados" aria-selected="false">
                     Empleados
                 </a>
-                <a class="nav-link" id="nav-proyectos-tab" data-type="proyectos" data-toggle="tab" href="#nav-proyectos"
+                @endcan
+                @can('visualizar_registros_dashboard_proyectos_timesheet')
+                    <a class="nav-link" id="nav-proyectos-tab" data-type="proyectos" data-toggle="tab" href="#nav-proyectos"
                     role="tab" aria-controls="nav-proyectos" aria-selected="false">
                     Proyectos
                 </a>
+                @endcan
             </div>
         </nav>
 
         <div class="tab-content" id="nav-tabContent">
+            @can('visualizar_registros_dashboard_timesheet')
             <div class="tab-pane mb-4 fade p-4 show active" id="nav-registros" role="tabpanel"
                 aria-labelledby="nav-registros-tab">
                 @include('admin.timesheet.dashboard.general')
             </div>
+            @endcan
+            @can('visualizar_registros_dashboard_empleados_timesheet')
             <div class="tab-pane mb-4 fade p-4" id="nav-empleados" role="tabpanel" aria-labelledby="nav-empleados-tab">
                 @include('admin.timesheet.dashboard.empleados')
             </div>
+            @endcan
+            @can('visualizar_registros_dashboard_proyectos_timesheet')
             <div class="tab-pane mb-4 fade p-4" id="nav-proyectos" role="tabpanel" aria-labelledby="nav-proyectos-tab">
                 @include('admin.timesheet.dashboard.proyectos')
             </div>
+            @endcan
         </div>
     </div>
 @endsection
