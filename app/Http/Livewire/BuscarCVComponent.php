a7e0c099a14befc200c4e9950a12cd881b2cf205
vojohn95 <vojohn95@gmail.com>
2023-07-24 20:03:34 -0600

    fx

--- app/Http/Livewire/BuscarCVComponent.php
+++ app/Http/Livewire/BuscarCVComponent.php
@@ -107,80 +107,47 @@ class BuscarCVComponent extends Component
 
     public function render()
     {
-        $cacheKey = 'empleadosCV_data_'. Auth::user()->id;
-        $empleadosCV = Cache::remember('empleadosCV_data_'.$cacheKey, 3600 * 60, function () {
-        return Empleado::alta()->with('empleado_certificaciones', 'empleado_cursos', 'empleado_experiencia')
-            ->when($this->empleado_id, function ($q3) {
-                $q3->where('id', $this->empleado_id);
-            })
-            ->when($this->area_id, function ($q4) {
-                $q4->where('area_id', $this->area_id);
-            })
-            ->when($this->certificacion, function ($q) {
-                $q->whereHas('empleado_certificaciones', function ($query) {
-                    $certificaciones = explode(',', $this->certificacion);
-                    $query->where(function ($queryArr) use ($certificaciones) {
-                        foreach ($certificaciones as $busqueda) {
-                            $queryArr->orWhere('nombre', 'ILIKE', "%{$busqueda}%");
-                        }
+        $cacheKey = 'empleadosCV_data_' . Auth::user()->id;
+
+        $empleadosCV = Cache::remember($cacheKey, 3600 * 60, function () {
+            return Empleado::alta()
+                ->with('empleado_certificaciones', 'empleado_cursos', 'empleado_experiencia')
+                ->when($this->empleado_id, function ($q3) {
+                    $q3->where('id', $this->empleado_id);
+                })
+                ->when($this->area_id, function ($q4) {
+                    $q4->where('area_id', $this->area_id);
+                })
+                ->when($this->certificacion, function ($q) {
+                    $q->whereHas('empleado_certificaciones', function ($query) {
+                        $certificaciones = explode(',', $this->certificacion);
+                        $query->where(function ($queryArr) use ($certificaciones) {
+                            foreach ($certificaciones as $busqueda) {
+                                $queryArr->orWhere('nombre', 'ILIKE', "%{$busqueda}%");
+                            }
+                        });
                     });
-                });
-            })
-            ->when($this->curso, function ($qCurso) {
-                $qCurso->whereHas('empleado_cursos', function ($queryCurso) {
-                    $queryCurso->where('curso_diploma', 'ILIKE', "%{$this->curso}%");
-                });
-            })
-            ->when($this->general, function ($qGeneral) {
-                $qGeneral->where('name', 'ILIKE', "%{$this->general}%");
-            })
+                })
+                ->when($this->curso, function ($qCurso) {
+                    $qCurso->whereHas('empleado_cursos', function ($queryCurso) {
+                        $queryCurso->where('curso_diploma', 'ILIKE', "%{$this->curso}%");
+                    });
+                })
+                ->when($this->general, function ($qGeneral) {
+                    $qGeneral->where('name', 'ILIKE', "%{$this->general}%");
+                })
+                ->paginate(21);
+        });
 
-            ->paginate(21);
-        $this->empleado = null;
-        /*   ->with([
-                'empleado_experiencia' => function ($q1) {
-                    $q1->where('empresa', 'ILIKE', $this->empresaExperiencia)
-                        ->orWhere('puesto', 'ILIKE', $this->puestoExperiencia)
-                        ->orWhere('descripcion', 'ILIKE', $this->descripcionExperiencia);
-                },
-                'empleado_certificaciones' => function ($q5) {
-                    $q5->where('nombre', 'LIKE', $this->certificacion);
-                },
-                'empleado_cursos' => function ($q2) {
-                    $q2->where('curso_diploma', 'ILIKE', $this->curso);
-                }
-            ])*/
-        /*$empleadoget = Empleado::select('*')->with('empleado_experiencia');
+        $this->empleado_id = null;
 
-        if (!$this->isPersonal) {
-            if ($this->area_id != '') {
-                if (Empleado::where('area_id', '=', $this->area_id)->count() > 0) {
-                    $this->empleados = Empleado::where('area_id', '=', $this->area_id)->get();
-                    $this->callAlert('success', 'La información se actualizo correctamente', true);
-                } else {
-                    $this->callAlert('warning', 'No se encontro registro con esta area', false, 'las opciones de busqueda se restablecieron');
-                    $this->area_id = '';
-                    $this->empleado_id = '';
-                    $this->empleados = Empleado::select('id', 'area_id', 'name')->get();
-                }
-            }
-        }
-        if ($this->empleado_id != '') {
-            if (Empleado::where('id', '=', $this->empleado_id)->count() > 0) {
-                $empleadoget->where('id', '=', $this->empleado_id);
-                $this->callAlert('success', 'La información se actualizo correctamente', true);
-            } else {
-                $this->callAlert('warning', 'No se encontro registro con este empleado', false, 'las opciones de busqueda se restablecieron');
-                $this->empleado_id = '';
-                $this->empleados = Empleado::select('id', 'area_id', 'name')->get();
-            }
-        }*/
         $this->lista_docs = ListaDocumentoEmpleado::getAll();
+
         return view('livewire.buscar-c-v-component', [
-            // 'empleadoget' => $empleadoget->get()->first(),
             'empleadosCV' => $empleadosCV,
         ]);
     }
+
 
     public function mostrarCurriculum($empleadoID)
     {
