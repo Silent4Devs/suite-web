4f72729bacc3fa9adf40d55e16d55e77280b8c3b
Víctor Hugo Rodríguez Albarrá <victor.rodriguez@s4b.com>
2023-07-31 12:53:20 -0600

    Tercera parte migraciones

---
+++ database/migrations/2023_07_31_105013_create_facturacions_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facturacion', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('contrato_id')->nullable();
+            // $table->foreign('contrato_id')->references('id')->on('contratos');
+            $table->string('no_factura', 50)->nullable();
+            $table->longText('concepto')->nullable();
+            $table->date('fecha_recepcion')->nullable();
+            $table->date('fecha_liberacion')->nullable();
+            $table->integer('no_revisiones')->nullable();
+            $table->boolean('cumple')->nullable();
+            $table->text('hallazgos_comentarios')->nullable();
+            $table->decimal('monto_factura', 13, 2)->nullable();
+            $table->longText('observaciones')->nullable();
+            $table->boolean('conformidad')->after('observaciones')->nullable();
+            $table->boolean('firma')->after('observaciones')->nullable();
+            $table->string('n_cxl')->after('observaciones')->nullable();
+            $table->string('estatus')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facturacion');
+    }
+};
---
+++ database/migrations/2023_07_31_110659_create_facturas_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facturas_files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('pdf')->nullable();
+            $table->string('xml')->nullable();
+            $table->unsignedBigInteger('factura_id');
+            // $table->foreign('factura_id')->references('id')->on('facturacion');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facturas_files');
+    }
+};
