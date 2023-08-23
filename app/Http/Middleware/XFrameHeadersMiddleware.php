d88c192542cb594a7134fa865d902cf4af91d0f5
john <vojohn95@gmail.com>
2023-08-23 16:57:27 -0600

    middleware modified

--- app/Http/Middleware/XFrameHeadersMiddleware.php
+++ app/Http/Middleware/XFrameHeadersMiddleware.php
@@ -46,10 +46,7 @@ class XFrameHeadersMiddleware
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Frame-Options', 'DENY');
-        //$response->headers->set('X-Frame-Options', 'SAMEORIGIN', false);
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-        //$response->headers->set('Access-Control-Allow-Origin', '*');
-        //$response->headers->set('Content-Security-Policy', "style-src 'self'"); // Clearly, you will be more elaborate here.
         return $response;
     }
 
