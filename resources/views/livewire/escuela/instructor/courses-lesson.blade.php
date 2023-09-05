e0e5d163998d9c962322d02117ca5be61d637fd5
vicvicto <vhraindiana@hotmail.com>
2023-09-05 10:21:40 -0600

    Edit datos al abrir formulario

--- resources/views/livewire/escuela/instructor/courses-lesson.blade.php
+++ resources/views/livewire/escuela/instructor/courses-lesson.blade.php
@@ -1,76 +1,80 @@
-
-    <div class="px-4 py-3">
-        @forelse ($section->lessons as $item)
-            <div class="card shadow-none">
-                <div class="card-header" style="color:blue; border: 1px solid #D8D8D8;">
-                    <i style="font-size:10pt; cursor: pointer;" class="d-inline text-black-500 fas fa-play-circle abirCollapse"
-                    data-toggle="collapse" data-target="#miCollapse{{$item->id}}"  aria-expanded="false"></i>
-                    <h5 class="d-inline">
-                        {{ $item->name }}
-                    </h5>
-                    <div class="d-inline">
-                        <a wire:click="edit({{ $item }})" style="cursor: pointer">
-                            <i style="font-size:10pt" class="ml-1 text-blue-500 cursor-pointer fas fa-edit"
-                                title="Editar" id="abir{{$item->id}}"></i>
-                        </a>
-                        <a wire:click="destroy({{ $item }})" style="cursor: pointer">
-                            <i style="font-size:10pt; color:red;" class="ml-2 fas fa-trash" title="Eliminar"></i>
-                        </a>
-                    </div>
+<div class="px-4 py-3">
+    @forelse ($section->lessons as $item)
+        <div class="card shadow-none">
+            <div class="card-header" style="color:blue; border: 1px solid #D8D8D8;">
+                <a wire:click="edit({{ $item }})" style="cursor: pointer">
+                    <i style="font-size:10pt; cursor: pointer;"
+                        class="d-inline text-black-500 fas fa-play-circle abirCollapse" data-toggle="collapse"
+                        data-target="#miCollapse{{ $item->id }}" aria-expanded="false"></i>
+                </a>
+                <h5 class="d-inline">
+                    {{ $item->name }}
+                </h5>
+                <div class="d-inline">
+                    {{-- <a wire:click="edit({{ $item }})" style="cursor: pointer">
+                        <i style="font-size:10pt" class="ml-1 text-blue-500 cursor-pointer fas fa-edit" title="Editar"
+                            id="abir{{ $item->id }}"></i>
+                    </a> --}}
+                    <a wire:click="destroy({{ $item }})" style="cursor: pointer">
+                        <i style="font-size:10pt; color:red;" class="ml-2 fas fa-trash" title="Eliminar"></i>
+                    </a>
                 </div>
-                <div class="card-body collapse hide" style="border: 1px solid #D8D8D8;" id="miCollapse{{$item->id}}">
-                    <h5>{{$item}}</h5>
-                    <form wire:submit.prevent="update" class="px-3 py-2">
-                            <div class="grid mt-2 mb-2 grid-col-1 md:grid-cols-6 md:gap-2">
-                                <label for="edit-lesson-name-{{ $section->id }}">Nombre</label>
-                                <div class="md:col-span-5">
-                                    <input wire:model="lesson.name" id="edit-lesson-name-{{ $section->id }}" type="text"
-                                        class=" w-full form-control @if ($errors->has('lesson.name')) invalid @endif">
-                                    @error('lesson.name')
-                                        <b class="block mt-1 text-xs text-red-500">{{ $message }}</b>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="grid items-center mb-2 grid-col-1 md:grid-cols-6 md:gap-3">
-                                <label for="edit-lesson-platform-{{ $section->id }}">Plataforma</label>
-                                <div class="md:col-span-5">
-                                    <select wire:model="lesson.platform_id" id="edit-lesson-platform-{{ $section->id }}"
-                                        type="text" class="w-full form-control @if ($errors->has('lesson.platform')) invalid @endif">
-                                        @foreach ($platforms as $platform)
-                                            <option value="{{ $platform->id }}">{{ $platform->name }}</option>
-                                        @endforeach
-                                    </select>
-                                    @error('lesson.platform')
-                                        <b class="block mt-1 text-xs text-red-500">{{ $message }}</b>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="grid items-center mb-2 grid-col-1 md:grid-cols-6 md:gap-3">
-                                <label for="edit-lesson-url-{{ $section->id }}">URL</label>
-                                <div class="md:col-span-5">
-                                    <input wire:model="lesson.url" id="edit-lesson-url-{{ $section->id }}" type="text"
-                                        class="form-control w-full @if ($errors->has('lesson.url')) invalid @endif">
-                                    @error('lesson.url')
-                                        <b class="block mt-1 text-xs text-red-500">{{ $message }}</b>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="d-flex justify-content-end mt-4">
-                                {{-- <button style="background-color:#333" type="submit"  >
+            </div>
+            <div class="card-body collapse hide" style="border: 1px solid #D8D8D8;" id="miCollapse{{ $item->id }}"
+                wire:ignore>
+                <form wire:submit.prevent="update" class="px-3 py-2">
+                    <div class="grid mt-2 mb-2 grid-col-1 md:grid-cols-6 md:gap-2">
+                        <label for="edit-lesson-name-{{ $section->id }}">Nombre</label>
+                        <div class="md:col-span-5">
+                            <input wire:model="lesson.name" id="edit-lesson-name-{{ $section->id }}" type="text"
+                                class=" w-full form-control @if ($errors->has('lesson.name')) invalid @endif">
+                            @error('lesson.name')
+                                <b class="block mt-1 text-xs text-red-500">{{ $message }}</b>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="grid items-center mb-2 grid-col-1 md:grid-cols-6 md:gap-3">
+                        <label for="edit-lesson-platform-{{ $section->id }}">Plataforma</label>
+                        <div class="md:col-span-5">
+                            <select wire:model="lesson.platform_id" id="edit-lesson-platform-{{ $section->id }}"
+                                type="text"
+                                class="w-full form-control @if ($errors->has('lesson.platform')) invalid @endif">
+                                @foreach ($platforms as $platform)
+                                    <option value="{{ $platform->id }}">{{ $platform->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('lesson.platform')
+                                <b class="block mt-1 text-xs text-red-500">{{ $message }}</b>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="grid items-center mb-2 grid-col-1 md:grid-cols-6 md:gap-3">
+                        <label for="edit-lesson-url-{{ $section->id }}">URL</label>
+                        <div class="md:col-span-5">
+                            <input wire:model="lesson.url" id="edit-lesson-url-{{ $section->id }}" type="text"
+                                class="form-control w-full @if ($errors->has('lesson.url')) invalid @endif">
+                            @error('lesson.url')
+                                <b class="block mt-1 text-xs text-red-500">{{ $message }}</b>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end mt-4">
+                        {{-- <button style="background-color:#333" type="submit"  >
                                   Actualizar
                                 </button> --}}
-                                <button type="submit" class="btn btn-outline-primary" style="min-width:140px;">Actualizar</button>
-                            </div>
-                    </form>
-                </div>
+                        <button type="submit" class="btn btn-outline-primary"
+                            style="min-width:140px;">Actualizar</button>
+                    </div>
+                </form>
             </div>
-            @empty
-            <div class="text-center">
-                Aun no hay lecciones en esta sección
-            </div>
-            @endforelse
+        </div>
+    @empty
+        <div class="text-center">
+            Aun no hay lecciones en esta sección
+        </div>
+    @endforelse
 
-        @include('livewire.escuela.instructor.add-new-lesson')
+    @include('livewire.escuela.instructor.add-new-lesson')
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             let abrirCollapseButtons = document.querySelectorAll('i.abirCollapse');
@@ -88,9 +92,9 @@
             });
         });
     </script>
-    </div>
+</div>
 
- {{-- <div id="miCollapse{{$item->id}}"  class="collapse hide">
+{{-- <div id="miCollapse{{$item->id}}"  class="collapse hide">
 </div> --}}
 
 {{-- <div class="card shadow-none">
@@ -122,7 +126,7 @@
                             </div>
                         </div> --}}
 
-            {{-- @if ($lesson->id == $item->id)
+{{-- @if ($lesson->id == $item->id)
                 <div class="card-body" style="border: 1px solid #D8D8D8;">
                     <form wire:submit.prevent="update" class="px-3 py-2">
                         <div class="grid mt-2 mb-2 grid-col-1 md:grid-cols-6 md:gap-2">
