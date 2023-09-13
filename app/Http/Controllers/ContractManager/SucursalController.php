ab57a4abd91608aa346d4378a796eca6ae21aca2
saul.ramirez <saul.ramirez@silent4business.com>
2023-09-13 09:44:45 -0600

    carpeta de sucursales

--- app/Http/Controllers/ContractManager/SucursalController.php
+++ app/Http/Controllers/ContractManager/SucursalController.php
@@ -79,7 +79,7 @@ class SucursalController extends Controller
 
         if ($file != null) {
             $nombre = uniqid() . '.' . $file->getClientOriginalExtension();
-            // $file->move(base_path('public/razon_social'), $nombre);
+            $file->move(base_path('public/razon_social'), $nombre);
             $sucursales->mylogo = $nombre;
             $sucursales->save();
         }
@@ -139,7 +139,7 @@ class SucursalController extends Controller
 
         if ($file != null) {
             $nombre = uniqid() . '.' . $file->getClientOriginalExtension();
-            // $img = $file->move(base_path('public/razon_social'), $nombre);
+            $file->move(base_path('public/razon_social'), $nombre);
 
             $sucursal->update([
                 'id' => $request->id,
