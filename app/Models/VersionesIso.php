f453a26bdfdab792bd91c25b1b61e24374252f09
beni <benito.lopez.tecno@gmail.com>
2023-11-02 15:45:14 -0600

    versions duo

--- app/Models/VersionesIso.php
+++ app/Models/VersionesIso.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Traits\ClearsResponseCache;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Cache;
 use OwenIt\Auditing\Contracts\Auditable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
