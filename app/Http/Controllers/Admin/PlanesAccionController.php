a8561546971203ac799884888fa56ef42f5e3f09
John Vo <vojohn95@gmail.com>
2023-11-09 22:08:41 -0600

    fx

--- app/Http/Controllers/Admin/PlanesAccionController.php
+++ app/Http/Controllers/Admin/PlanesAccionController.php
@@ -23,7 +23,7 @@ class PlanesAccionController extends Controller
     public function index(Request $request)
     {
         $planImplementacions = Cache::remember('PlanImplementacion:plan_implementacion_all_where_false', 3600 * 8, function () {
-            return PlanImplementacion::select('id', 'tasks', 'parent', 'norma', 'modulo_origen', 'objetivo', 'elaboro_id', 'es_plan_trabajo_base')->where('es_plan_trabajo_base', false)->with('elaborador')->get();
+            return PlanImplementacion::where('es_plan_trabajo_base', false)->with('elaborador')->get();
         });
 
         // $iso9001 = Cache::remember('PlanImplementacion:plan_implementacion_9001_all', 3600 * 8, function () {
