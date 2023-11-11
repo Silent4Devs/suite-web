0d4cf64ee67107ea64d5abdc520b354d54aa6f6a
John Vo <vojohn95@gmail.com>
2023-11-10 20:06:56 -0600

    fx

--- resources/views/livewire/plan-de-accion/plan-accion-index-component.blade.php
+++ resources/views/livewire/plan-de-accion/plan-accion-index-component.blade.php
@@ -59,14 +59,13 @@
                             <td>{{ $plan->modulo_origen }}</td>
                             <td>{{ $plan->objetivo }}</td>
                             <td>
-                                @isset($plan->elaboro_id)
+                                @if ($plan->elaboro_id)
                                     <span class="badge badge-primary">Elaborado por:
-                                        {{ $plan->elaborador->name }}</span>
-                                @endisset
-
-                                @empty($plan->elaboro_id)
+                                        {{ $plan->elaborador->name ?? 'No disponible' }}</span>
+                                @else
                                     <span class="badge badge-primary">Elaborado por el sistema</span>
-                                @endempty
+                                @endif
+
                             </td>
                             <td>
                                 @if (isset($plan->tasks) && count($plan->tasks) > 0)
