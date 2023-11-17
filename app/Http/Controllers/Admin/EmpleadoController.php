816421dcdb0d5f77a7f96dd889f608a28334539a
vojohn95 <vojohn95@gmail.com>
2023-11-17 16:51:53 -0600

    fx

--- app/Http/Controllers/Admin/EmpleadoController.php
+++ app/Http/Controllers/Admin/EmpleadoController.php
@@ -63,7 +63,7 @@ class EmpleadoController extends Controller
     {
         abort_if(Gate::denies('bd_empleados_acceder'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $empleados = Empleado::select('id', 'n_empleado', 'name', 'foto', 'email', 'telefono', 'area_id', 'puesto', 'supervisor_id', 'antiguedad', 'estatus', 'sede_id', 'cumpleaños')->get()
+        $empleados = Empleado::select('id', 'n_empleado', 'name', 'foto', 'genero', 'email', 'telefono', 'area_id', 'puesto', 'supervisor_id', 'antiguedad', 'estatus', 'sede_id', 'cumpleaños')->get()
         ->map(function ($empleado) {
             $empleado['avatar_ruta'] = $empleado->avatar_ruta; // Access the computed attribute
             return $empleado;
