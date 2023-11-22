a5aa6fc9df3c690dbf62b9e878e62f5626d25ead
saul.ramirez <saul.ramirez@silent4business.com>
2023-11-22 15:01:32 -0600

    jen

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -12,7 +12,7 @@
     word-wrap: break-word
     }
     </style>
-    <h5 class="col-12 titulo_general_funcion">Ahuiui </h5>
+    <h5 class="col-12 titulo_general_funcion">Ahuiui ya salio </h5>
     <div class="mt-5 card">
         <div class="d-flex justify-content-between" style="justify-content: flex-end !important;">
             <div class="p-2">
