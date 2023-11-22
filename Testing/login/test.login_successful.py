0fff8a9ac4553e1ab63f6f1084345ad656c99963
imzzaidd <angieillumi@gmail.com>
2023-11-22 09:37:39 -0600

    feat: Se crean archivos para el login test

--- Testing/login/test.login_successful.py
+++ Testing/login/test.login_successful.py
@@ -2,15 +2,12 @@ from selenium import webdriver
 import os
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
 import time
-tiempo_modulos=4
+tiempo_modulos=5
 tiempo_carga=10
-tiempo_espera=2.5
 #driver Firefox
 driver=webdriver.Firefox()
 
@@ -20,11 +17,3 @@ driver.get('https://192.168.9.78/')
 #Maximize Window
 driver.maximize_window()
 time.sleep(5)
-
-#Login with good credentials
-usr=driver.find_element(By.XPATH,"//input[contains(@name,'email')]").send_keys("zaid.garcia@becarios.silent4business.com")
-time.sleep(tiempo_modulos)
-pw=driver.find_element(By.XPATH,"//input[contains(@name,'password')]").send_keys("$QB&kT3&R4")
-time.sleep(tiempo_modulos)
-btn=driver.find_element(By.XPATH,"//button[@type='submit'][contains(.,'Enviar')]")
-btn.click()
