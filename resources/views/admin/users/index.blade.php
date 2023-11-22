0154b2b806fb38d0e084a705e65f55095fa8f370
saul.ramirez <saul.ramirez@silent4business.com>
2023-11-22 14:00:35 -0600

    jen

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -12,7 +12,7 @@
     word-wrap: break-word
     }
     </style>
-    <h5 class="col-12 titulo_general_funcion">Prueba 2 jenis saul</h5>
+    <h5 class="col-12 titulo_general_funcion">Prueba 2 jenis sa</h5>
     <div class="mt-5 card">
         <div class="d-flex justify-content-between" style="justify-content: flex-end !important;">
             <div class="p-2">
