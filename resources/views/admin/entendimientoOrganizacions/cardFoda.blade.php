ab6bf487a2be47daae7ecdf4ee675b4e3d780d37
BenitoLM <benito.lopez.tecno@gmail.com>
2023-11-28 19:28:44 -0600

    merge new brnach

--- resources/views/admin/entendimientoOrganizacions/cardFoda.blade.php
+++ resources/views/admin/entendimientoOrganizacions/cardFoda.blade.php
@@ -5,21 +5,36 @@
 @section('content')
     <h5 class="col-12 titulo_general_funcion">Análisis FODA</h5>
 
-    <div class="caja-cards">
-        @foreach ($query as $foda)
-            <div class="card card-foda">
-                <div class="card-header">
-                    <strong> 16-10-2022 </strong>
-                </div>
-                <div class="card-body">
-                    <h3>
-                        FODA Corporativo 2023 V3
-                    </h3>
-                    <p>
-                        <small>Layla Delgadillo Aguilar</small>
-                    </p>
-                </div>
+    <div class="card-filtros-fodas">
+        <div class="row">
+            <div class="col-md-6">
+                <label for="">Buscar</label>
+                <input type="text" class="form-control">
             </div>
+            <div class="col-md-6">
+                <label for="">Fecha</label>
+                <input type="date" class="form-control">
+            </div>
+        </div>
+    </div>
+
+    <div class="caja-cards mt-5">
+        @foreach ($query as $foda)
+            <a href="{{ asset('admin/entendimiento-organizacions') }}/{{ $foda->id }}">
+                <div class="card card-foda">
+                    <div class="card-header">
+                        <strong> {{ Carbon\Carbon::parse($foda->fecha)->format('d/m/Y') }}</strong>
+                    </div>
+                    <div class="card-body">
+                        <h3>
+                            {{ $foda->analisis }}
+                        </h3>
+                        <p>
+                            <small>{{ $foda->elaboro_id ? $foda->empleado->name : 'No asignado' }}</small>
+                        </p>
+                    </div>
+                </div>
+            </a>
         @endforeach
     </div>
 @endsection
