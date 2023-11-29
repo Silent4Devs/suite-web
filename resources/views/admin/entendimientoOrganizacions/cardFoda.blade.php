a3e92f6b4f297dd6ede24e802d199d34baf39495
vicvicto <vhraindiana@hotmail.com>
2023-11-29 12:48:29 -0600

    Se agrego la funcion de buscador en las cards

--- resources/views/admin/entendimientoOrganizacions/cardFoda.blade.php
+++ resources/views/admin/entendimientoOrganizacions/cardFoda.blade.php
@@ -9,12 +9,14 @@
         <div class="row">
             <div class="col-md-6">
                 <label for="">Buscar</label>
-                <input type="text" class="form-control">
+                <input id="input-search" type="text" class="form-control" onkeyup="buscadorGlobal()">
             </div>
+
             <div class="col-md-6">
-                <label for="">Fecha</label>
-                <input type="date" class="form-control">
+                <label for="">Buscar por Fecha</label>
+                <input id="input-search-fechas" type="date" class="form-control" onchange="buscadorGlobal()">
             </div>
+
         </div>
     </div>
 
@@ -38,3 +40,44 @@
         @endforeach
     </div>
 @endsection
+<script>
+    function buscadorGlobal() {
+        var inputText, inputDate, section, div, strong, i;
+        inputText = document.getElementById("input-search");
+        inputDate = document.getElementById("input-search-fechas");
+        var filterText = inputText.value.toUpperCase();
+        var filterDate = inputDate.value;
+
+        section = document.getElementsByClassName("caja-cards")[0];
+        div = section.getElementsByClassName("card-foda");
+
+        for (i = 0; i < div.length; i++) {
+            strong = div[i].getElementsByTagName("strong")[0];
+            if (strong) {
+                var cardDateText = strong.innerHTML.trim(); // Date from the card
+                var cardText = div[i].getElementsByTagName("h3")[0].innerText.toUpperCase(); // Text from the card
+
+                // Format card date to match input date format (yyyy-mm-dd)
+                var cardDateFormatted = formatDate(cardDateText);
+
+                if (
+                    (filterText === '' || cardText.includes(filterText)) &&
+                    (filterDate === '' || filterDate === cardDateFormatted)
+                ) {
+                    div[i].style.display = "";
+                } else {
+                    div[i].style.display = "none";
+                }
+            }
+        }
+    }
+
+    // Helper function to format date from dd/mm/yyyy to yyyy-mm-dd
+    function formatDate(dateString) {
+        var parts = dateString.split('/');
+        if (parts.length === 3) {
+            return parts[2] + '-' + parts[1].padStart(2, '0') + '-' + parts[0].padStart(2, '0');
+        }
+        return null; // Return null for invalid dates
+    }
+</script>
