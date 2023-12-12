e09a4dd3d4cfc35a298f35567ed714eadae99788
Saul183 <152949586+Saul183@users.noreply.github.com>
2023-12-12 17:50:11 -0600

    experiencia de usuario

---
+++ resources/views/admin/comiteseguridads/table.blade.php
@@ -0,0 +1,18 @@
+<table class="datatable datatable-Comiteseguridad" id="datatable-Comiteseguridad">
+    <thead>
+        <tr>
+            <th style="min-width: 200px; max-width:200px;">
+                Nombre
+            </th>
+            <th style="min-width: 200px; max-width:200px;">
+                Miembros
+            </th>
+            <th style="min-width: 200px; max-width:200px;">
+                Descripción
+            </th>
+            <th style="min-width: 200px; max-width:200px;">
+
+            </th>
+        </tr>
+    </thead>
+</table>
--- resources/views/livewire/show-miembros-comite-seguridad.blade.php
+++ resources/views/livewire/show-miembros-comite-seguridad.blade.php
@@ -4,55 +4,17 @@
         @livewire('create-miembros-comite-seguridad', ['id_comite' => $id_comite])
     </div>
 
-    <table class="table">
-        <thead class="head-light">
-            <tr>
-                <th scope="col-6">Colaborador</th>
-                <th scope="col-6">Posición</th>
-                <th scope="col-6">Responsabilidades</th>
-                <th scope="col-6">Entrada en vigor</th>
-                <th scope="col-6">Opciones</th>
+    @include('partials.flashMessages')
+    <div class="datatable-fix datatable-rds">
+        <h3 class="title-table-rds">Miembros  del  Comites</h3>
+        @include('admin.comiteseguridads.table_miembros')
+    </div>
 
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($datas as $data)
-                <tr>
-                    <th style="min-width:130px;"><img class="img_empleado"
-                            src="{{ asset('storage/empleados/imagenes') }}/{{ $data->asignacion ? $data->asignacion->avatar : 'user.png' }}"
-                            title="{{ $data->asignacion->name }}"></th>
-                    <th style="min-width:130px;">{{ $data->nombrerol }}</th>
-                    <td style="min-width:100px;">{!!$data->responsabilidades!!}</td>
-                    <td style="min-width:100px;">{{ $data->fechavigor }}</td>
-                    {{-- <td style="min-width:40px; position:relative">
-                        @if (!is_null($data->normas))
-                            <ul>
-                                @forelse ($data->normas as $norma)
-                                    <li> {{ $norma->norma }}</li>
-                                @empty
-                                    Sin normas
-                                @endforelse
-                            </ul>
-                        @endif
-                    </td> --}}
-                    <td style="min-width:40px;">
-                        <i class="fas fa-edit" wire:click.prevent="$emit('editarParteInteresada',{{ $data->id }})">
-                        </i>
-                        {{-- <i class="fas fa-project-diagram"
-                            wire:click.prevent="$emit('agregarNormas',{{ $data->id }})"> </i> --}}
-                        <i class="fas fa-trash-alt text-danger"
-                            wire:click.prevent="$emit('eliminarParteInteresada',{{ $data->id }})"> </i>
-                    </td>
-                    {{-- <td> @livewire('edit-partes-interesadas',['id_requisito'=>$data->id])</td> --}}
-                </tr>
-            @endforeach
-
-        </tbody>
-    </table>
 
 
 </div>
 <script>
+
     document.addEventListener('DOMContentLoaded', () => {
         Livewire.on('cerrar-modal', (event) => {
             $('#exampleModal').modal('hide');
@@ -72,7 +34,6 @@
 
         })
         Livewire.on('editarParteInteresada', () => {
-            console.log('hola');
 
 
         });
@@ -102,4 +63,6 @@
 
         })
     })
+
+
 </script>
