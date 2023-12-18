2eb42792c51ca0f47c3d0e3ea070c2e53eb62c32
imzzaidd <angieillumi@gmail.com>
2023-12-18 08:33:00 -0600

    fix: Se ha optimizado el script para el login

--- Testing/login/test.login_failed.py
+++ Testing/login/test.login_failed.py
@@ -1,35 +1,41 @@
 from selenium import webdriver
-import os
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 import time
-tiempo_modulos=4
-tiempo_carga=10
-tiempo_espera=2.5
-#driver Firefox
-driver=webdriver.Firefox()
 
-#Open URL
-driver.get('https://192.168.9.78/')
+tiempo_modulos = 4
+tiempo_carga = 10
+tiempo_espera = 2.5
 
-#Maximize Window
-driver.maximize_window()
-time.sleep(5)
+# Pide al usuario que ingrese sus credenciales
+usuario = input("Ingresa tu nombre de usuario: ")
+contrasena = input("Ingresa tu contraseña: ")
 
-#Login with errornous credentials
-usr=driver.find_element(By.XPATH,"//input[contains(@name,'email')]").send_keys("zaid.garcia@becarios.silent4business.com")
-time.sleep(tiempo_modulos)
-pw=driver.find_element(By.XPATH,"//input[contains(@name,'password')]").send_keys("12345")
-time.sleep(tiempo_modulos)
-btn=driver.find_element(By.XPATH,"//button[@type='submit'][contains(.,'Enviar')]")
-btn.click()
-# Esperar hasta 10 segundos para encontrar un elemento que indique un inicio de sesión exitoso
+# Crear una instancia del controlador de Firefox
+driver = webdriver.Firefox()
+
 try:
+    # Abrir la URL
+    driver.get('https://192.168.9.78/')
+
+    # Maximizar la ventana del navegador
+    driver.maximize_window()
+    time.sleep(5)
+
+    # Ingresar credenciales
+    usr = driver.find_element(By.XPATH, "//input[contains(@name,'email')]").send_keys(usuario)
+    time.sleep(tiempo_modulos)
+    pw = driver.find_element(By.XPATH, "//input[contains(@name,'password')]").send_keys(contrasena)
+    time.sleep(tiempo_modulos)
+
+    # Hacer clic en el botón de envío
+    btn = driver.find_element(By.XPATH, "//button[@type='submit'][contains(.,'Enviar')]")
+    btn.click()
+
+    # Esperar hasta 10 segundos para encontrar un elemento que indique un inicio de sesión exitoso
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//font[@class='letra_blanca'][contains(.,'Mi perfil')]"))
     )
--- Testing/login/test.login_recuperarcontraseña.py
+++ Testing/login/test.login_recuperarcontraseña.py
@@ -1,33 +1,48 @@
 from selenium import webdriver
-import os
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 import time
-tiempo_modulos=4
-tiempo_carga=10
-tiempo_espera=2.5
-#driver Firefox
-driver=webdriver.Firefox()
 
-#Open URL
-driver.get('https://192.168.9.78/')
+tiempo_modulos = 4
+tiempo_carga = 10
+tiempo_espera = 2.5
 
-#Maximize Window
-driver.maximize_window()
-time.sleep(5)
+# Pide al usuario que ingrese sus credenciales
+usuario = input("Ingresa tu nombre de usuario: ")
+contrasena = input("Ingresa tu contraseña: ")
 
-#Login with errornous credentials
-usr=driver.find_element(By.XPATH,"//input[contains(@name,'email')]").send_keys("zaid.garcia@becarios.silent4business.com")
-time.sleep(tiempo_modulos)
-pw=driver.find_element(By.XPATH,"//input[contains(@name,'password')]").send_keys("12345")
-time.sleep(tiempo_modulos)
-btn=driver.find_element(By.XPATH,"//button[@type='submit'][contains(.,'Enviar')]")
-btn.click()
+# Crear una instancia del controlador de Firefox
+driver = webdriver.Firefox()
+
+try:
+    # Abrir la URL
+    driver.get('https://192.168.9.78/')
+
+    # Maximizar la ventana del navegador
+    driver.maximize_window()
+    time.sleep(5)
+
+    # Ingresar credenciales
+    usr = driver.find_element(By.XPATH, "//input[contains(@name,'email')]").send_keys(usuario)
+    time.sleep(tiempo_modulos)
+    pw = driver.find_element(By.XPATH, "//input[contains(@name,'password')]").send_keys(contrasena)
+    time.sleep(tiempo_modulos)
+
+    # Hacer clic en el botón de envío
+    btn = driver.find_element(By.XPATH, "//button[@type='submit'][contains(.,'Enviar')]")
+    btn.click()
+
+    # Esperar hasta 10 segundos para encontrar un elemento que indique un inicio de sesión exitoso
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//font[@class='letra_blanca'][contains(.,'Mi perfil')]"))
+    )
+    print("Inicio de sesión exitoso")
+except TimeoutException:
+    print("Inicio de sesión fallido")
+
 
 # Esperamos de nuevo la pantalla de inicio de sesión
 time.sleep(tiempo_espera)
@@ -37,7 +52,9 @@ fgpw.click()
 
 # Esperamos la interfaz de recuperación de contraseña
 time.sleep(tiempo_espera)
-mail=driver.find_element(By.XPATH,"//input[contains(@type,'email')]").send_keys("zaid.garcia@becarios.silent4business.com")
+# Pide al usuario que ingrese sus credenciales
+usuario = input("Ingresa tu nombre de usuario: ")
+mail = driver.find_element(By.XPATH, "//input[contains(@type,'email')]").send_keys(usuario)
 time.sleep(tiempo_modulos)
 btnrecuperar=driver.find_element(By.XPATH,"//button[@type='submit'][contains(.,'Recuperar contraseña')]")
 btnrecuperar.click()
