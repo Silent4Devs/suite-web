411a3479d29ee06a49bab91bcff54c56e8f16836
Saul183 <152949586+Saul183@users.noreply.github.com>
2023-12-27 14:08:17 -0600

    experiencia

--- app/Http/Controllers/Admin/Escuela/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/Escuela/Admin/CategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Escuela\Category;
 use Illuminate\Http\Request;
 use RealRashid\SweetAlert\Facades\Alert;
+use Yajra\DataTables\Facades\DataTables;
 
 class CategoryController extends Controller
 {
@@ -14,9 +15,43 @@ class CategoryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $categories = Category::all();
+        if ($request->ajax()) {
+            $query = Category::get();
+            $table = Datatables::of($query);
+
+            $table->addColumn('placeholder', '&nbsp;');
+            $table->addColumn('actions', '&nbsp;');
+            $table->addIndexColumn();
+            $table->editColumn('actions', function ($row) {
+                $viewGate = 'capacitaciones_ver';
+                $editGate = 'capacitaciones_editar';
+                $deleteGate = 'capacitaciones_eliminar';
+                $crudRoutePart = 'recursos';
+
+                return view('partials.datatablesActions', compact(
+                    'viewGate',
+                    'editGate',
+                    'deleteGate',
+                    'crudRoutePart',
+                    'row'
+                ));
+            });
+
+            $table->editColumn('id', function ($row) {
+                return $row->id ? $row->id : '';
+            });
+            $table->editColumn('name', function ($row) {
+                return $row->name ? $row->name : '';
+            });
+
+            $table->rawColumns(['actions', 'placeholder']);
+
+            return $table->make(true);
+        }
+
+        $categories = Category::get();
 
         return view('admin.escuela.admin.categories.index', compact('categories'));
     }
@@ -79,7 +114,7 @@ class CategoryController extends Controller
     public function update(Request $request, Category $category)
     {
         $request->validate([
-            'name' => 'required|unique:categories,name,'.$category->id,
+            'name' => 'required|unique:categories,name,' . $category->id,
         ]);
 
         $category->update($request->all());
--- resources/views/admin/escuela/admin/categories/edit.blade.php
+++ resources/views/admin/escuela/admin/categories/edit.blade.php
@@ -13,20 +13,14 @@
         <div class="card-body">
             <h1 class="font-weight-bold mb-4" style="padding-bottom: 10px; border-color: #3086AF !important; font-size: 20px; border-bottom-style:solid;border-width: 1px;">Categoría</h1>
             {!! Form::model($category, ['route' => ['admin.categories.update', $category], 'method' => 'put']) !!}
-                <div class="form-group">
-                    <span style="color: #3086AF;">Nombre</span><span style="color: #AF3041;">*</span>
-                    <div class="row align-items-start">
-                        <div class="col-9">
-                            {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre de la categoría']) !!}
-                        </div>
-                        <div class="col-3">
-                            {!! Form::submit('ACTUALIZAR CATEGORIA', ['class' => 'btn btn-link', 'style' => 'color: #006DDB; font-size: 15px; margin-left: 30px; font-weight: bold;']) !!}
-                        </div>
-                    </div>
-
-                    @error('name')
-                        <span class="text-danger">{{$message}}</span>
-                    @enderror
+                <div class="form-group anima-focus">
+                            {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => '']) !!}
+                            {!! Form::label('name', 'Nombre*', ['class' => 'asterisco']) !!}
+                            @error('name')
+                            <span class="text-danger">{{$message}}</span>
+                            @enderror
+                            <br>
+                            {!! Form::submit('ACTUALIZAR CATEGORIA', ['class' => 'btn btn-primary']) !!}
                 </div>
         {!! Form::close() !!}
     </div>
