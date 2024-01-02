53a7b6a2adfc66c523f3cd9b5934c7f91a7dfbfa
vicvicto <vhraindiana@hotmail.com>
2024-01-02 10:05:08 -0600

    Arreglo de los modales en el index de la lista de distribucion

--- resources/views/admin/listadistribucion/table.blade.php
+++ resources/views/admin/listadistribucion/table.blade.php
@@ -15,76 +15,122 @@
             </th>
         </tr>
     </thead>
-</table>
-
-@foreach ($participantes as $key => $row)
-    <div class="modal fade" id="exampleModal{{ $key }}" tabindex="-1" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <!-- Modal content structure -->
-                <div class="modal-body">
-                    <h5>Lista de Aprobadores</h5>
-
-                    @php
-                        $levels = []; // Initialize an empty array to store levels temporarily
-                    @endphp
-
-                    @foreach ($row->participantes as $participante)
+    <tbody>
+        @foreach ($query as $modulo)
+            <tr>
+                <td>{{ $modulo->modulo }}</td>
+                <td>{{ $modulo->submodulo }}</td>
+                <td>
+                    <div class="row">
                         @php
-                            $nivel = $participante->nivel;
-                            $levels[$nivel][] = $participante; // Group participantes by their nivel
-                        @endphp
-                    @endforeach
-
-                    @foreach ($levels as $nivel => $participantesByLevel)
-                        @php
-                            // Sort participantes by numero_orden within each nivel
-                            usort($participantesByLevel, function ($a, $b) {
-                                return $a->numero_orden <=> $b->numero_orden;
-                            });
-
+                            $participantCount = $modulo->participantes->count();
                         @endphp
 
-                        @if ($nivel == 0)
-                            <div class="row mb-3">
-                                <div class="col-6">
-                                    <h6>Super Aprobadores</h6>
-                                </div>
-                                <div class="col-6">
-                                    <div class="row">
-                                        @foreach ($participantesByLevel as $participante)
-                                            <div class="col-2">
-                                                <img src="{{ asset('storage/empleados/imagenes') }}/{{ $participante->empleado->avatar }}"
-                                                    class="img_empleado" title="{{ $participante->empleado->name }}">
-                                                <!-- Add other empleado details here -->
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                </div>
+                        @foreach ($modulo->participantes->take(3) as $index => $participante)
+                            <div class="col-3">
+                                <img src="{{ asset('storage/empleados/imagenes') }}/{{ $participante->empleado->avatar }}"
+                                    class="img_empleado" title="{{ $participante->empleado->name }}">
                             </div>
-                        @else
-                            <div class="row mb-3">
-                                <div class="col-6">
-                                    <h6>Nivel {{ $nivel }}</h6>
-                                </div>
-                                <div class="col-6">
-                                    <div class="row">
-                                        @foreach ($participantesByLevel as $participante)
-                                            <div class="col-2">
-                                                <img src="{{ asset('storage/empleados/imagenes') }}/{{ $participante->empleado->avatar }}"
-                                                    class="img_empleado" title="{{ $participante->empleado->name }}">
-                                                <!-- Add other empleado details here -->
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                </div>
+                        @endforeach
+
+                        @if ($participantCount > 3)
+                            <div class="col-3">
+                                <button type="button" class="btn btn-round ml-2"
+                                    style="border-radius: 50%;  background-color: #fff8dc;" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal{{ $modulo->id }}">+
+                                    {{ $participantCount - 3 }}</button>
                             </div>
                         @endif
-                    @endforeach
+                    </div>
+
+                </td>
+                <td>
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fa-solid fa-ellipsis-vertical" style="color: #000000;"></i>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a href="/admin/lista-distribucion/{{ $modulo->id }}/edit" class="btn btn-sm"
+                                    title="Editar"><i class="fa fa-edit"></i>&nbsp;
+                                    Editar</a></li>
+                            <li><a href="/admin/lista-distribucion/{{ $modulo->id }}/show" class="btn btn-sm"
+                                    title="Visualizar"><i class="fa fa-eye"></i>&nbsp;Ver</a></li>
+                        </ul>
+                    </div>
+                </td>
+            </tr>
+            <div class="modal fade" id="exampleModal{{ $modulo->id }}" tabindex="-1"
+                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <!-- Modal content structure -->
+                        <div class="modal-body">
+                            <h5>Lista de Aprobadores</h5>
+
+                            @php
+                                $levels = []; // Initialize an empty array to store levels temporarily
+                            @endphp
+
+                            @foreach ($modulo->participantes as $participante)
+                                @php
+                                    $nivel = $participante->nivel;
+                                    $levels[$nivel][] = $participante; // Group participantes by their nivel
+                                @endphp
+                            @endforeach
+
+                            @foreach ($levels as $nivel => $participantesByLevel)
+                                @php
+                                    // Sort participantes by numero_orden within each nivel
+                                    usort($participantesByLevel, function ($a, $b) {
+                                        return $a->numero_orden <=> $b->numero_orden;
+                                    });
+
+                                @endphp
+
+                                @if ($nivel == 0)
+                                    <div class="row mb-3">
+                                        <div class="col-6">
+                                            <h6>Super Aprobadores</h6>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="row">
+                                                @foreach ($participantesByLevel as $participante)
+                                                    <div class="col-2">
+                                                        <img src="{{ asset('storage/empleados/imagenes') }}/{{ $participante->empleado->avatar }}"
+                                                            class="img_empleado"
+                                                            title="{{ $participante->empleado->name }}">
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                @else
+                                    <div class="row mb-3">
+                                        <div class="col-6">
+                                            <h6>Nivel {{ $nivel }}</h6>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="row">
+                                                @foreach ($participantesByLevel as $participante)
+                                                    <div class="col-2">
+                                                        <img src="{{ asset('storage/empleados/imagenes') }}/{{ $participante->empleado->avatar }}"
+                                                            class="img_empleado"
+                                                            title="{{ $participante->empleado->name }}">
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-@endforeach
+        @endforeach
+    </tbody>
+</table>
 
+{{-- @foreach ($query as $key => $modulo)
+@endforeach --}}
