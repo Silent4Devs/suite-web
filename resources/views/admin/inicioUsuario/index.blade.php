120fdb8d25bb2f204b720c5a550b634393c67ca4
beni <benito.lopez.tecno@gmail.com>
2024-01-04 14:45:19 -0600

    inicio usuario

--- resources/views/admin/inicioUsuario/index.blade.php
+++ resources/views/admin/inicioUsuario/index.blade.php
@@ -3,12 +3,13 @@
     <link rel="stylesheet" href="{{ asset('css/inicio_usuario.css') }}">
 @endsection
 @section('content')
+    @include('partials.menu-slider')
     <div class="d-flex" style="gap: 30px;">
         <div class="w-100">
             <div class="header-card-iu">
                 <div class="d-flex justify-content-between align-items-start">
                     <div class="d-flex align-items-end">
-                        <h4 class="title-name-user">Carlos Fernando Alberto Crúz Andrade</h4>
+                        <h4 class="title-name-user">{{ $usuario->empleado->name }}</h4>
                         <small class="ml-3">
                             <i class="fa-solid fa-location-dot"></i>
                             Torre Murano
@@ -20,14 +21,15 @@
                     </div>
                 </div>
                 <div>
-                    Nº de empelado <span>0000</span>
+                    Nº de empelado <span>{{ $usuario->empleado->n_registro }}</span>
                 </div>
             </div>
             <div class="card overflow-hidden">
                 <div class="d-flex">
                     <div class=" info-blue-user">
                         <div class="img-person" style="width: 205px; height: 205px;">
-                            <img src="{{ asset('') }}" alt="">
+                            <img src="{{ asset('storage/empleados/imagenes/' . '/' . $usuario->empleado->avatar) }}"
+                                alt="">
                         </div>
                         <div class="mt-4">
                             <a href="">Ver perfil profesional</a> <br>
@@ -36,11 +38,11 @@
                         </div>
                         <div class="mt-4">
                             <strong>Email</strong><br>
-                            nombre@dominio.com
+                            {{ $usuario->empleado->email }}
                         </div>
                         <div class="mt-4">
                             <strong>Teléfono</strong><br>
-                            55 5555 5555
+                            {{ $usuario->empleado->telefono }}
                         </div>
                     </div>
                     <div class="card-body">
@@ -50,23 +52,24 @@
                         <div class=" caja-info-user-main">
                             <div>
                                 <span>Genero</span><br>
-                                H
+                                {{ $usuario->empleado->genero }}
                             </div>
                             <div>
                                 <span>Perfil</span><br>
-                                Gerente
+                                {{ $usuario->empleado->puesto }}
                             </div>
                             <div>
                                 <span>fecha de ingreso</span><br>
-                                21-12-22
+                                {{ $usuario->empleado->fecha_ingreso }}
                             </div>
                             <div>
                                 <span>Jefe inmediato</span><br>
-                                Juan Francisco Curiel Morales
+                                {{ $usuario->empleado->jefe_inmediato }}
                             </div>
                             <div>
                                 <span>Cumpleaños</span><br>
-                                21-12-22
+                                {{ $usuario->empleado->actual_birdthday }}
+
                             </div>
                         </div>
                     </div>
@@ -76,21 +79,21 @@
 
         <div class="w-100" style="max-width: 600px;">
             <div class="header-card-iu d-flex caja-btn-user" style="padding: 20px 0px;">
-                <button class="btn" style="background-color: #B8DFE1;">
+                <button class="btn" style="background-color: #B8DFE1;" onclick="miCard('#user-equipo')">
                     <i class="material-symbols-outlined">contacts</i>
                     Mi&nbsp;equipo
                 </button>
-                <button class="btn" style="background-color: #D3ECEC;">
+                <button class="btn" style="background-color: #D3ECEC;" onclick="miCard('#user-activos')">
                     <i class="material-symbols-outlined">devices</i>
                     Mis&nbsp;activos
                 </button>
-                <button class="btn" style="background-color: #CCE2E2;">
+                <button class="btn" style="background-color: #CCE2E2;" onclick="miCard('#user-competencias')">
                     <i class="material-symbols-outlined">star</i>
                     Mis&nbsp;Competencias
                 </button>
             </div>
             <div class="card card-body">
-                <div id="user-equipo" class="">
+                <div id="user-equipo" class="mis-cards active">
                     <h3 class="title-user-card">Mi equipo</h3>
                     <hr class="my-4">
                     <div class="caja-equipo">
@@ -114,6 +117,14 @@
                         </div>
                     </div>
                 </div>
+                <div id="user-activos" class="mis-cards">
+                    <h3 class="title-user-card">Mis activos</h3>
+                    <hr class="my-4">
+                </div>
+                <div id="user-competencias" class="mis-cards">
+                    <h3 class="title-user-card">Mis competencias</h3>
+                    <hr class="my-4">
+                </div>
             </div>
         </div>
     </div>
@@ -124,4 +135,10 @@
 @endsection
 
 @section('scripts')
+    <script>
+        function miCard(id) {
+            $('.mis-cards').removeClass('active');
+            $(id).addClass('active');
+        }
+    </script>
 @endsection
