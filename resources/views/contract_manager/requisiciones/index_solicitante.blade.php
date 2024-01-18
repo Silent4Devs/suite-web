f4540ff7403e946a0595cc70cb5b2d6772fe0c37
Saul183 <152949586+Saul183@users.noreply.github.com>
2024-01-18 15:43:29 -0600

    deploy

--- resources/views/contract_manager/requisiciones/index_solicitante.blade.php
+++ resources/views/contract_manager/requisiciones/index_solicitante.blade.php
@@ -13,7 +13,7 @@
             word-wrap: break-word
         }
     </style>
-    <h5 class="col-12 titulo_general_funcion">Requisiciónes</h5>
+    <h5 class="col-12 titulo_general_funcion">Requisiciónes Despligue</h5>
     <div class="mt-5 card">
         <form class="text-right" action="{{ route('contract_manager.requisiciones.indexAprobadores') }}" method="GET">
             @method('GET')
