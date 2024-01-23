51467b3c01a31c0fb056792e12e3bd044af8c6f8
vojohn95 <vojohn95@gmail.com>
2024-01-23 17:45:32 -0600

    fx

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -63,7 +63,7 @@
     @livewireStyles
 </head>
 
-<body style="zoom: 95%">
+<body style="zoom: 100%">
     <div id="loading">
         <img id="loading-image" src="https://i.pinimg.com/originals/07/24/88/0724884440e8ddd0896ff557b75a222a.gif"
             alt="Loading...">
