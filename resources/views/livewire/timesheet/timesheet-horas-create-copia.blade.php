d4aa056a60fc4f52acbac64b0d0c7ac1083db15e
Saul183 <152949586+Saul183@users.noreply.github.com>
2024-01-24 17:26:31 -0600

    mejoras-optimizaciones

--- resources/views/livewire/timesheet/timesheet-horas-create-copia.blade.php
+++ resources/views/livewire/timesheet/timesheet-horas-create-copia.blade.php
@@ -483,7 +483,7 @@
                                     '{{ route('admin.timesheet-inicio') }}';
                             });
                         } else {
-                            toastr.error('Error al enviar');
+                            toastr.error('!Error al enviar valide  que la semana  laboral no este vacia y/o que al menos una fila este llena!');
                         }
                     },
                     error: function(request, status, error) {
