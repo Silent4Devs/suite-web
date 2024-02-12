e6dca1d22e135db4b28d02d95bf27aed0e539d81
vicvicto <vhraindiana@hotmail.com>
2024-02-12 09:04:16 -0600

    Modelos y migraciones de objetivos evaluaciones 360

---
+++ app/Http/Controllers/CatalogoRangosObjetivosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CatalogoRangosObjetivos;
+use Illuminate\Http\Request;
+
+class CatalogoRangosObjetivosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CatalogoRangosObjetivos $catalogoRangosObjetivos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CatalogoRangosObjetivos $catalogoRangosObjetivos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CatalogoRangosObjetivos $catalogoRangosObjetivos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CatalogoRangosObjetivos $catalogoRangosObjetivos)
+    {
+        //
+    }
+}
