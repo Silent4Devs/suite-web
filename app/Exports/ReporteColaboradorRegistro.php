7503706b76dd10aa978cc828a323dea3255d9c71
Saul183 <152949586+Saul183@users.noreply.github.com>
2024-02-20 17:05:50 -0600

    filtro correcto

--- app/Exports/ReporteColaboradorRegistro.php
+++ app/Exports/ReporteColaboradorRegistro.php
@@ -53,6 +53,10 @@ class ReporteColaboradorRegistro implements FromCollection, WithHeadings
             )
             ->where('timesheet.estatus', '!=', 'papelera')
             ->where('timesheet.estatus', '!=', 'rechazado')
+            ->where('timesheet.estatus', '!=', 'Rechazada')
+            ->where('timesheet_proyectos.estatus', '!=', 'papelera')
+            ->where('timesheet_proyectos.estatus', '!=', 'rechazado')
+            ->where('timesheet_proyectos.estatus', '!=', 'Rechazada')
             ->where(function ($query) {
 
                 if ($this->fecha_inicio || $this->fecha_fin) {
@@ -67,8 +71,7 @@ class ReporteColaboradorRegistro implements FromCollection, WithHeadings
                 if ($this->area_id != 0) {
                     $query->where('empleados.area_id', $this->area_id);
                 }
-            })->where('timesheet_proyectos.estatus', '!=', 'papelera')
-            ->where('timesheet_proyectos.estatus', '!=', 'rechazado')
+            })
             ->groupBy(
                 'timesheet.id',
                 'fecha_dia',
