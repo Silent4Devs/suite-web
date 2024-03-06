a38608b3ceb279965184f84424a30f6749007b8d
John Vo <vojohn95@gmail.com>
2024-03-06 14:07:38 -0600

    fx

--- _ide_helper.php
+++ _ide_helper.php
@@ -5,7 +5,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 10.45.1.
+ * Generated for Laravel 10.47.0.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -9331,179 +9331,84 @@ namespace Illuminate\Support\Facades {
                         return $instance->setConnectionName($name);
         }
                     /**
-         * 
+         * Migrate the delayed jobs that are ready to the regular queue.
          *
-         * @throws AMQPProtocolChannelException
+         * @param string $from
+         * @param string $to
+         * @param int $limit
+         * @return array 
          * @static 
-         */        public static function laterRaw($delay, $payload, $queue = null, $attempts = 0)
+         */        public static function migrateExpiredJobs($from, $to)
         {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->laterRaw($delay, $payload, $queue, $attempts);
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
+                        return $instance->migrateExpiredJobs($from, $to);
         }
                     /**
-         * 
+         * Delete a reserved job from the queue.
          *
-         * @throws AMQPProtocolChannelException
+         * @param string $queue
+         * @param \Illuminate\Queue\Jobs\RedisJob $job
+         * @return void 
          * @static 
-         */        public static function bulkRaw($payload, $queue = null, $options = [])
+         */        public static function deleteReserved($queue, $job)
         {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->bulkRaw($payload, $queue, $options);
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
+                        $instance->deleteReserved($queue, $job);
         }
                     /**
-         * 
+         * Delete a reserved job from the reserved queue and release it.
          *
-         * @throws RuntimeException
+         * @param string $queue
+         * @param \Illuminate\Queue\Jobs\RedisJob $job
+         * @param int $delay
+         * @return void 
          * @static 
-         */        public static function getConnection()
+         */        public static function deleteAndRelease($queue, $job, $delay)
         {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->getConnection();
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
+                        $instance->deleteAndRelease($queue, $job, $delay);
         }
                     /**
-         * 
+         * Delete all of the jobs from the queue.
          *
+         * @param string $queue
+         * @return int 
          * @static 
-         */        public static function setConnection($connection)
+         */        public static function clear($queue)
         {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->setConnection($connection);
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
+                        return $instance->clear($queue);
         }
                     /**
-         * Job class to use.
+         * Get the queue or return the default.
          *
-         * @throws Throwable
+         * @param string|null $queue
+         * @return string 
          * @static 
-         */        public static function getJobClass()
+         */        public static function getQueue($queue)
         {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->getJobClass();
-        }
-                    /**
-         * Gets a queue/destination, by default the queue option set on the connection.
-         *
-         * @static 
-         */        public static function getQueue($queue = null)
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
                         return $instance->getQueue($queue);
         }
                     /**
-         * Checks if the given exchange already present/defined in RabbitMQ.
-         * 
-         * Returns false when the exchange is missing.
+         * Get the connection for the queue.
          *
-         * @throws AMQPProtocolChannelException
+         * @return \Illuminate\Redis\Connections\Connection 
          * @static 
-         */        public static function isExchangeExists($exchange)
+         */        public static function getConnection()
         {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->isExchangeExists($exchange);
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
+                        return $instance->getConnection();
         }
                     /**
-         * Declare an exchange in rabbitMQ, when not already declared.
+         * Get the underlying Redis instance.
          *
+         * @return \Illuminate\Contracts\Redis\Factory 
          * @static 
-         */        public static function declareExchange($name, $type = 'direct', $durable = true, $autoDelete = false, $arguments = [])
+         */        public static function getRedis()
         {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->declareExchange($name, $type, $durable, $autoDelete, $arguments);
-        }
-                    /**
-         * Delete an exchange from rabbitMQ, only when present in RabbitMQ.
-         *
-         * @throws AMQPProtocolChannelException
-         * @static 
-         */        public static function deleteExchange($name, $unused = false)
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->deleteExchange($name, $unused);
-        }
-                    /**
-         * Checks if the given queue already present/defined in RabbitMQ.
-         * 
-         * Returns false when the queue is missing.
-         *
-         * @throws AMQPProtocolChannelException
-         * @static 
-         */        public static function isQueueExists($name = null)
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->isQueueExists($name);
-        }
-                    /**
-         * Declare a queue in rabbitMQ, when not already declared.
-         *
-         * @static 
-         */        public static function declareQueue($name, $durable = true, $autoDelete = false, $arguments = [])
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->declareQueue($name, $durable, $autoDelete, $arguments);
-        }
-                    /**
-         * Delete a queue from rabbitMQ, only when present in RabbitMQ.
-         *
-         * @throws AMQPProtocolChannelException
-         * @static 
-         */        public static function deleteQueue($name, $if_unused = false, $if_empty = false)
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->deleteQueue($name, $if_unused, $if_empty);
-        }
-                    /**
-         * Bind a queue to an exchange.
-         *
-         * @static 
-         */        public static function bindQueue($queue, $exchange, $routingKey = '')
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->bindQueue($queue, $exchange, $routingKey);
-        }
-                    /**
-         * Purge the queue of messages.
-         *
-         * @static 
-         */        public static function purge($queue = null)
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->purge($queue);
-        }
-                    /**
-         * Acknowledge the message.
-         *
-         * @static 
-         */        public static function ack($job)
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->ack($job);
-        }
-                    /**
-         * Reject the message.
-         *
-         * @static 
-         */        public static function reject($job, $requeue = false)
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->reject($job, $requeue);
-        }
-                    /**
-         * Close the connection to RabbitMQ.
-         *
-         * @throws Exception
-         * @static 
-         */        public static function close()
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->close();
-        }
-                    /**
-         * 
-         *
-         * @static 
-         */        public static function getChannel($forceNew = false)
-        {
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
-                        return $instance->getChannel($forceNew);
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
+                        return $instance->getRedis();
         }
                     /**
          * Get the maximum number of attempts for an object-based queue handler.
@@ -9513,7 +9418,7 @@ namespace Illuminate\Support\Facades {
          * @static 
          */        public static function getJobTries($job)
         {            //Method inherited from \Illuminate\Queue\Queue         
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
                         return $instance->getJobTries($job);
         }
                     /**
@@ -9524,7 +9429,7 @@ namespace Illuminate\Support\Facades {
          * @static 
          */        public static function getJobBackoff($job)
         {            //Method inherited from \Illuminate\Queue\Queue         
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
                         return $instance->getJobBackoff($job);
         }
                     /**
@@ -9535,7 +9440,7 @@ namespace Illuminate\Support\Facades {
          * @static 
          */        public static function getJobExpiration($job)
         {            //Method inherited from \Illuminate\Queue\Queue         
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
                         return $instance->getJobExpiration($job);
         }
                     /**
@@ -9546,7 +9451,7 @@ namespace Illuminate\Support\Facades {
          * @static 
          */        public static function createPayloadUsing($callback)
         {            //Method inherited from \Illuminate\Queue\Queue         
-                        \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue::createPayloadUsing($callback);
+                        \Illuminate\Queue\RedisQueue::createPayloadUsing($callback);
         }
                     /**
          * Get the container instance being used by the connection.
@@ -9555,7 +9460,7 @@ namespace Illuminate\Support\Facades {
          * @static 
          */        public static function getContainer()
         {            //Method inherited from \Illuminate\Queue\Queue         
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
                         return $instance->getContainer();
         }
                     /**
@@ -9566,7 +9471,7 @@ namespace Illuminate\Support\Facades {
          * @static 
          */        public static function setContainer($container)
         {            //Method inherited from \Illuminate\Queue\Queue         
-                        /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+                        /** @var \Illuminate\Queue\RedisQueue $instance */
                         $instance->setContainer($container);
         }
             }
@@ -22748,6 +22653,31 @@ namespace  {
                                 return $instance->joinSub($query, $as, $first, $operator, $second, $type, $where);
             }
                             /**
+             * Add a lateral join clause to the query.
+             *
+             * @param \Closure|\Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder|string $query
+             * @param string $as
+             * @param string $type
+             * @return \Illuminate\Database\Query\Builder 
+             * @static 
+             */            public static function joinLateral($query, $as, $type = 'inner')
+            {
+                                /** @var \Illuminate\Database\Query\Builder $instance */
+                                return $instance->joinLateral($query, $as, $type);
+            }
+                            /**
+             * Add a lateral left join to the query.
+             *
+             * @param \Closure|\Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder|string $query
+             * @param string $as
+             * @return \Illuminate\Database\Query\Builder 
+             * @static 
+             */            public static function leftJoinLateral($query, $as)
+            {
+                                /** @var \Illuminate\Database\Query\Builder $instance */
+                                return $instance->leftJoinLateral($query, $as);
+            }
+                            /**
              * Add a left join to the query.
              *
              * @param \Illuminate\Contracts\Database\Query\Expression|string $table
@@ -23607,6 +23537,60 @@ namespace  {
             {
                                 /** @var \Illuminate\Database\Query\Builder $instance */
                                 return $instance->orWhereFullText($columns, $value, $options);
+            }
+                            /**
+             * Add a "where" clause to the query for multiple columns with "and" conditions between them.
+             *
+             * @param string[] $columns
+             * @param mixed $operator
+             * @param mixed $value
+             * @param string $boolean
+             * @return \Illuminate\Database\Query\Builder 
+             * @static 
+             */            public static function whereAll($columns, $operator = null, $value = null, $boolean = 'and')
+            {
+                                /** @var \Illuminate\Database\Query\Builder $instance */
+                                return $instance->whereAll($columns, $operator, $value, $boolean);
+            }
+                            /**
+             * Add an "or where" clause to the query for multiple columns with "and" conditions between them.
+             *
+             * @param string[] $columns
+             * @param string $operator
+             * @param mixed $value
+             * @return \Illuminate\Database\Query\Builder 
+             * @static 
+             */            public static function orWhereAll($columns, $operator = null, $value = null)
+            {
+                                /** @var \Illuminate\Database\Query\Builder $instance */
+                                return $instance->orWhereAll($columns, $operator, $value);
+            }
+                            /**
+             * Add an "where" clause to the query for multiple columns with "or" conditions between them.
+             *
+             * @param string[] $columns
+             * @param string $operator
+             * @param mixed $value
+             * @param string $boolean
+             * @return \Illuminate\Database\Query\Builder 
+             * @static 
+             */            public static function whereAny($columns, $operator = null, $value = null, $boolean = 'and')
+            {
+                                /** @var \Illuminate\Database\Query\Builder $instance */
+                                return $instance->whereAny($columns, $operator, $value, $boolean);
+            }
+                            /**
+             * Add an "or where" clause to the query for multiple columns with "or" conditions between them.
+             *
+             * @param string[] $columns
+             * @param string $operator
+             * @param mixed $value
+             * @return \Illuminate\Database\Query\Builder 
+             * @static 
+             */            public static function orWhereAny($columns, $operator = null, $value = null)
+            {
+                                /** @var \Illuminate\Database\Query\Builder $instance */
+                                return $instance->orWhereAny($columns, $operator, $value);
             }
                             /**
              * Add a "group by" clause to the query.
