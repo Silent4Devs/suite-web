da2d6e14764466bc816abf559816027d35f26e10
imzzaidd <angieillumi@gmail.com>
2024-03-12 12:57:49 -0600

    feat: se crea archivos para contratos por aparte de gestion contractual

--- Testing/contratos/tests/test_contratos.py
+++ Testing/contratos/tests/test_contratos.py
@@ -1,16 +1,10 @@
 import pytest
-from pages.gestion_contractual_page import GestionContractual
+from selenium import webdriver
+from pages.contratos_page import GestionContractual
 
-@pytest.mark.usefixtures("browser")
-def test_gestion_contractual(browser):
-    #LOGIN
-    username = "admin@admin.com"
-    password = "#S3cur3P4$$w0Rd!"
-    login_page = GestionContractual(browser)
-    login_page.login(username, password)
-    #MENÚ HAMBURGUESA
-    menu = GestionContractual(browser)
-    menu.open_menu()
-    #GESTIÓN CONTRACTUAL
-    gestion_contractual_module = GestionContractual(browser)
-    gestion_contractual_module.go_to_gestion_contractual()
+
+def test_contratos(browser):
+    gestion_contractual = GestionContractual(browser)
+    gestion_contractual.login("admin@admin.com","#S3cur3P4$$w0Rd!")
+    gestion_contractual.open_menu()
+    gestion_contractual.go_to_gestion_contractual()
