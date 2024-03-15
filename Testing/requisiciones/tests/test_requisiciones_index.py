d085d276926ffabcdfa32fa58574e6bb97101ed0
imzzaidd <angieillumi@gmail.com>
2024-03-15 08:39:16 -0600

    feat: index view

---
+++ Testing/requisiciones/tests/test_requisiciones_index.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from pages.requisiciones_index_page import Requisiciones_index
+
+
+def test_contratos(browser):
+    #LOGIN
+    requisiciones_index_page= Requisiciones_index(browser)
+    requisiciones_index_page.login("admin@admin.com","#S3cur3P4$$w0Rd!")
+    #MENÚ HAMBURGUESA
+    requisiciones_index_page.open_menu()
+    #GESTION CONTRACTUAL
+    requisiciones_index_page.go_to_gestion_contractual()
+    #REQUISICIONES
+    requisiciones_index_page.requisiciones_module()
+
