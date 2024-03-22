bd3d6dcfbd3ff8754eb45b36844a65d222785bed
vojohn95 <vojohn95@gmail.com>
2024-03-22 11:46:45 -0600

    fx

--- app/Http/Controllers/ContractManager/RequisicionesController.php
+++ app/Http/Controllers/ContractManager/RequisicionesController.php
@@ -301,10 +301,10 @@ class RequisicionesController extends Controller
         $requisicion = KatbolRequsicion::where('id', $id)->first();
 
         $user = User::getCurrentUser();
-        $supervisor = User::find($requisicion->id_user)->empleado->supervisor;
+        $supervisor = User::find($requisicion->id_user)->empleado->supervisor->name;
         $supervisor_email = User::select('id', 'name', 'email')->where('name', $supervisor)->first()->email;
         $comprador = KatbolComprador::with('user')->where('id', $requisicion->comprador_id)->first();
-        dd($supervisor, $supervisor_email);
+
         if ($requisicion->firma_solicitante === null) {
             $tipo_firma = 'firma_solicitante';
         } elseif ($requisicion->firma_jefe === null) {
