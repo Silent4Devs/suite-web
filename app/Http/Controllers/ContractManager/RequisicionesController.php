f57f9d8c5ad5a3a26ed5af82d3241c3afcabded2
vojohn95 <vojohn95@gmail.com>
2024-03-22 10:36:05 -0600

    fx

--- app/Http/Controllers/ContractManager/RequisicionesController.php
+++ app/Http/Controllers/ContractManager/RequisicionesController.php
@@ -311,6 +311,7 @@ class RequisicionesController extends Controller
             if (removeUnicodeCharacters($supervisor_email) === removeUnicodeCharacters($user->email)) {
                 $tipo_firma = 'firma_jefe';
             } else {
+                dd($supervisor_email, $user->email, $requisicion->firma_jefe);
                 return view('contract_manager.requisiciones.error')->with('mensaje', 'No tiene permisos para firmar<br> En espera del jefe directo: <br> <strong>'.$supervisor.'</strong>');
             }
         } elseif ($requisicion->firma_finanzas === null) {
