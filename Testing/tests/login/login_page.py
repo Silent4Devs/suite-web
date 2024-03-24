0ff44b63652259c44b866a398c771c00f561ccb8
imzzaidd <angieillumi@gmail.com>
2024-03-23 21:08:39 -0600

    fx: new structure for TestOps

---
+++ Testing/tests/login/login_page.py
@@ -0,0 +1,40 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+
+    def login(self, username, password):
+        self.driver.get('https://192.168.9.78/')
+        self.driver.maximize_window()
+        print("------ LOGIN - TABANTAJ -----")
+        time.sleep(5)
+        username_input = WebDriverWait(self.driver, 3).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='email']"))
+        )
+        username_input.clear()
+        username_input.send_keys(username)
+        print("Usario ingresado")
+
+        password_input = WebDriverWait(self.driver, 3).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='password']"))
+        )
+        password_input.clear()
+        password_input.send_keys(password)
+        print("Contraseña ingresada")
+
+        submit_button = WebDriverWait(self.driver, 3).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[@type='submit'][contains(text(),'Enviar')]"))
+        )
+        submit_button.click()
+        print("Enviando credenciales de acceso")
+
+        WebDriverWait(self.driver, 2).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "img[alt='Logo Tabantaj']"))
+        )
+        print("Login correcto")
+
