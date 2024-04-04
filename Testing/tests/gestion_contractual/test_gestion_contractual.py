7bf41e8bdced2d30f16f4d734354ac8dffd36edf
imzzaidd <angieillumi@gmail.com>
2024-04-04 11:15:48 -0600

    refactor:gestion contractual index page

--- Testing/tests/gestion_contractual/test_gestion_contractual.py
+++ Testing/tests/gestion_contractual/test_gestion_contractual.py
@@ -1,13 +1,36 @@
 import pytest
-from pages.gestion_contractual_page import GestionContractual
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from pages.gestion_contractual.index.gestion_contractual_page import GestionContractual
+from selenium import webdriver
+from config import username, password
+
+@pytest.fixture(scope="session")
+def browser():
+    options = FirefoxOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-browser-side-navigation')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--log-level=3')
+
+    #driver = webdriver.Chrome(options=options)
+    driver = webdriver.Firefox(options=options)
+    yield driver
+    driver.quit()
 
 @pytest.mark.usefixtures("browser")
 def test_gestion_contractual(browser):
     #LOGIN
-    username = "admin@admin.com"
-    password = "#S3cur3P4$$w0Rd!"
     login_page = GestionContractual(browser)
-    login_page.login(username, password)
+    login_page.login()
     #MENÚ HAMBURGUESA
     menu = GestionContractual(browser)
     menu.open_menu()
