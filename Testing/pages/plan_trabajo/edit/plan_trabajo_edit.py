e49b0fd19b63254ea0824f85037d011bb562a893
imzzaidd <angieillumi@gmail.com>
2024-04-04 15:53:15 -0600

    refactor

--- Testing/pages/plan_trabajo/edit/plan_trabajo_edit.py
+++ Testing/pages/plan_trabajo/edit/plan_trabajo_edit.py
@@ -6,41 +6,27 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from config import username, password
 
 class PlanTrabajo_edit:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(self.driver, 20)
 
-    def login(self, username, password):
-        self.driver.get('https://192.168.9.78/')
-        self.driver.maximize_window()
-        print("------ LOGIN - TABANTAJ -----")
-        time.sleep(5)
-        username_input = WebDriverWait(self.driver, 3).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='email']"))
-        )
-        username_input.clear()
-        username_input.send_keys(username)
-        print("Usario ingresado")
-
-        password_input = WebDriverWait(self.driver, 3).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='password']"))
-        )
-        password_input.clear()
-        password_input.send_keys(password)
-        print("Contraseña ingresada")
-
-        submit_button = WebDriverWait(self.driver, 3).until(
-            EC.element_to_be_clickable((By.XPATH, "//button[@type='submit'][contains(text(),'Enviar')]"))
-        )
-        submit_button.click()
-        print("Enviando credenciales de acceso")
-
-        WebDriverWait(self.driver, 2).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "img[alt='Logo Tabantaj']"))
-        )
-        print("Login correcto")
-        print("URL actual: ", self.driver.current_url)
+    def login(self):
+        try:
+            self.driver.get('https://192.168.9.78/')
+            self.driver.maximize_window()
+            print("Iniciando sesión en el sistema...")
+            time.sleep(4)
+            self._fill_input_field("input[name='email']", username)
+            self._fill_input_field("input[name='password']", password)
+            self._click_element("//button[@type='submit'][contains(text(),'Enviar')]")
+            print("¡Sesión iniciada con éxito!")
+            self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "img[alt='Logo Tabantaj']")))
+            print("Login correcto.")
+        except Exception as e:
+            print("Error durante el inicio de sesión:", e)
 
     def open_menu(self):
         menu_btn = WebDriverWait(self.driver, 3).until(
@@ -77,10 +63,27 @@ class PlanTrabajo_edit:
         nombre_input.clear()
         nombre_input.send_keys(nombre_edit)
 
-    def select_norma_edit(self):
-        norma_edit = WebDriverWait(self.driver, 5).until(
-            EC.visibility_of_element_located((By.XPATH, "//select[contains(@id,'norma')]"))
-        )
-        select = Select(norma_edit)
-        select.select_by_index(2)
-        print("Norma editada")
+
+    def seleccionar_fecha_inicio(self, fecha):
+        print("Seleccionando fecha...")
+        self._wait_and_fill("//input[@id='inicio']", fecha)
+        print("Fecha seleccionada.")
+    def seleccionar_fecha_fin(self, fecha):
+        print("Seleccionando fecha...")
+        self._wait_and_fill("//input[@id='fin']", fecha)
+        print("Fecha seleccionada.")
+    def descripcion(self, descripcion):
+        print("Ingresando descripción...")
+        self._fill_input_field1("//textarea[contains(@class,'form-control')]", descripcion)
+        print("Descripción ingresada.")
+    def guardar_btn(self):
+        self._click_element("//button[contains(.,'Guardar')]")
+
+    def _fill_input_field(self, locator, value):
+        input_field = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, locator)))
+        input_field.clear()
+        input_field.send_keys(value)
+
+    def _click_element(self, xpath):
+        element = self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        element.click()
