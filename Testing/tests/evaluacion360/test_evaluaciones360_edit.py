9afa5823b0f9300e2c09def5d7e9e893d0816cec
imzzaidd <angieillumi@gmail.com>
2024-04-08 10:25:47 -0600

    fx: refactor strcuture ev360 & gestion talento

---
+++ Testing/tests/evaluacion360/test_evaluaciones360_edit.py
@@ -0,0 +1,38 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from pages.evaluacion360.edit.evaluaciones360_edit_page import Evaluaciones360Edit
+from config import username, password
+
+@pytest.fixture(scope="session")
+def browser():
+    options = FirefoxOptions()
+    # options = ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-browser-side-navigation')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--log-level=3')
+
+    #driver = webdriver.Chrome(options=options)
+    driver = webdriver.Firefox(options=options)
+    yield driver
+    driver.quit()
+
+def test_evaluaciones360_manualmente(browser):
+    evaluaciones360_create = Evaluaciones360Edit(browser)
+    evaluaciones360_create.login()
+    index_cap_humano = "https://192.168.9.78/admin/capital-humano"
+    evaluaciones360_create.cap_humano_index(index_cap_humano)
+
+
+
+
+
