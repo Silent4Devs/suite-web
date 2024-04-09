7d9376f6967754f4a01cc09f46ee0eb46d841a0f
vicvicto <vhraindiana@hotmail.com>
2024-04-09 16:34:18 -0600

    Sprint 2 dashboard ev trimestrales v1

--- database/migrations/2024_03_13_135715_create_evaluadores_evaluacion_objetivos_desempenos_table.php
+++ database/migrations/2024_03_13_135715_create_evaluadores_evaluacion_objetivos_desempenos_table.php
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('evaluadores_evaluacion_desempenos');
+        Schema::dropIfExists('evaluadores_evaluacion_objetivos_desempenos');
     }
 };
