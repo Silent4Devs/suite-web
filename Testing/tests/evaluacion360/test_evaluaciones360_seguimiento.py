3313d0bf08ad136db791f42cff5cf8d706c28a8c
imzzaidd <angieillumi@gmail.com>
2024-04-10 09:55:42 -0600

    fx

--- Testing/tests/evaluacion360/test_evaluaciones360_seguimiento.py
+++ Testing/tests/evaluacion360/test_evaluaciones360_seguimiento.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from pages.evaluacion360.seguimiento.evaluaciones360_seguimiento_page import Evaluaciones360Seguimiento
 from config import username, password
-'''
+
 @pytest.fixture(scope="session")
 def browser():
     options = FirefoxOptions()
@@ -25,7 +25,7 @@ def browser():
     driver = webdriver.Firefox(options=options)
     yield driver
     driver.quit()
-'''
+
 def test_evaluaciones360_seguimiento(browser):
     evaluaciones360_seguimiento = Evaluaciones360Seguimiento(browser)
     username_input = "input[name='email']"
