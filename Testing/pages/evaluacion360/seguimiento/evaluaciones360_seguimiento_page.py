49ade4eed02b5814ca03ee0eaa33df5eea580175
imzzaidd <angieillumi@gmail.com>
2024-04-10 09:43:33 -0600

    feat: icons and search bar - ok

--- Testing/pages/evaluacion360/seguimiento/evaluaciones360_seguimiento_page.py
+++ Testing/pages/evaluacion360/seguimiento/evaluaciones360_seguimiento_page.py
@@ -32,6 +32,49 @@ class Evaluaciones360Seguimiento:
         except Exception as e:
             print("Error al cargar Seguimiento de Evaluaciones 360:", e)
 
+    def mostrar_select(self,select):
+        try:
+            self._select_option_by_text(select, "100")
+            print("Evaluación 360 seleccionada.")
+
+        except Exception as e:
+            print("Error al seleccionar la Evaluación 360:", e)
+    def csv_download(self,csv):
+        try:
+            self._click_element(csv)
+            print("Descarga de CSV iniciada.")
+
+        except Exception as e:
+            print("Error al descargar CSV:", e)
+    def excel_download(self,excel):
+        try:
+            self._click_element(excel)
+            print("Descarga de Excel iniciada.")
+
+        except Exception as e:
+            print("Error al descargar Excel:", e)
+
+    def pdf_download(self, pdf):
+        try:
+            self._click_element(pdf)
+            print("Descarga de PDF iniciada.")
+        except Exception as e:
+            print("Error al descargar PDF:", e)
+
+    def imprimir(self,imprimir):
+        try:
+            self._click_element(imprimir)
+            print("Impresión iniciada.")
+        except Exception as e:
+            print("Error al imprimir:", e)
+
+    def searchbar(self,search_bar,credential):
+        try:
+            self._fill_input_field_xpath(search_bar,credential)
+            print("Búsqueda realizada.")
+        except Exception as e:
+            print("Error al realizar la búsqueda:", e)
+
     def _select_option_by_text(self, locator, text):
         select = Select(self.driver.find_element(By.XPATH, locator))
         select.select_by_visible_text(text)
