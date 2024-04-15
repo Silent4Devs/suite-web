c2d849c226a75f026dc90008d7277288be028fca
vojohn95 <vojohn95@gmail.com>
2024-04-15 15:45:49 -0600

    udopated

--- app/Http/Controllers/QueueCorreo.php
+++ app/Http/Controllers/QueueCorreo.php
@@ -259,7 +259,7 @@ class QueueCorreo extends Controller
             }
         }
 
-        dd('Proceso finalizado');
+        dd('Proceso finalizado', 'total: ' . count($datos));
     }
 
     /**
