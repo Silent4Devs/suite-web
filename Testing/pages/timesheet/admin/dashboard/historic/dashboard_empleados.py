8a9f61577bca8c60c8e8cfa4d9c90a7dda3c18d0
imzzaidd <angieillumi@gmail.com>
2024-04-23 17:30:30 -0600

    feat: empleados view - ok

---
+++ Testing/pages/timesheet/admin/dashboard/historic/dashboard_empleados.py
@@ -0,0 +1,62 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+import pdb
+from config import username, password
+
+class DashboardEmpleadosTimesheet:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 20)
+
+    def login(self,logo,btn_enviar,username_input,password_input):
+        try:
+            self.driver.get('https://192.168.9.78/')
+            self.driver.maximize_window()
+            print("Iniciando sesión en el sistema...")
+            time.sleep(4)
+            self._fill_input_field_css(username_input, username)
+            self._fill_input_field_css(password_input, password)
+            self._click_element(btn_enviar)
+            print("¡Sesión iniciada con éxito!")
+            self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, logo)))
+        except Exception as e:
+            print("Error durante el inicio de sesión:", e)
+
+    def timesheet_dashboard_index(self, dashboard_route):
+        try:
+            self.driver.get(dashboard_route)
+            print("Index de timesheet cargado.")
+        except Exception as e:
+            print("Error al cargar el index de timesheet:", e)
+
+    def empleados_section(self,empleados):
+        try:
+            self._click_element(empleados)
+            print("Sección de empleados cargada.")
+        except Exception as e:
+            print("Error al cargar la sección de empleados:", e)
+
+    def _select_option_by_text(self, locator, text):
+        select = Select(self.driver.find_element(By.XPATH, locator))
+        select.select_by_visible_text(text)
+
+    def _fill_input_field_xpath(self, locator, value):
+        input_field = self.wait.until(EC.visibility_of_element_located((By.XPATH, locator)))
+        input_field.clear()
+        input_field.send_keys(value)
+
+    def _fill_input_field_css(self, locator, value):
+        input_field = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, locator)))
+        input_field.clear()
+        input_field.send_keys(value)
+
+    def _select_option_by_text(self, locator, text):
+        select = Select(self.driver.find_element(By.XPATH, locator))
+        select.select_by_visible_text(text)
+
+    def _click_element(self, xpath):
+        element = self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        element.click()
--- Testing/tests/timesheet/test_dashboardempleados.py
+++ Testing/tests/timesheet/test_dashboardempleados.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from pages.timesheet.admin.dashboard.historic.dashboard_v1 import TimesheetDashboard
+from pages.timesheet.admin.dashboard.historic.dashboard_empleados import DashboardEmpleadosTimesheet
 from config import username, password
 '''
 @pytest.fixture(scope="session")
@@ -27,25 +27,15 @@ def browser():
     driver.quit()
 '''
 def test_timesheet_dashboard(browser):
-    timesheet_dashboard = TimesheetDashboard(browser)
+    timesheet_dashboardempleados = DashboardEmpleadosTimesheet(browser)
     username_input = "input[name='email']"
     password_input = "input[name='password']"
     btn_enviar = "//button[@type='submit'][contains(text(),'Enviar')]"
     logo="img[alt='Logo Tabantaj']"
-    timesheet_dashboard.login(logo,btn_enviar,username_input,password_input)
+    timesheet_dashboardempleados.login(logo,btn_enviar,username_input,password_input)
 
     dashboard_route = "https://192.168.9.78/admin/timesheet/dashboard"
-    timesheet_dashboard.timesheet_dashboard_index(dashboard_route)
+    timesheet_dashboardempleados.timesheet_dashboard_index(dashboard_route)
 
-    select1 = "//select[@class='form-control' and @id='areas-graf-areas-time']"
-    registro1 = "QA"
-    timesheet_dashboard.registros_timesheet_area_select_graphic(select1,registro1)
-
-    select2="//select[contains(@id,'areas-graf-registros-general')]"
-    registro2 = "QA"
-    timesheet_dashboard.registro_timesheet_select_graphic(select2,registro2)
-
-    select3 = "//select[contains(@id,'graf-registros-area')]"
-    registro3 = "QA"
-    timesheet_dashboard.registro_horas_area_select_graphic(select3,registro3)
-
+    empleados = "//a[@class='nav-link'][contains(.,'Empleados')]"
+    timesheet_dashboardempleados.empleados_section(empleados)
