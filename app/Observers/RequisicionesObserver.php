c1a0e57bd2048e80e3c7fc382495cf45ae6baa75
vojohn95 <vojohn95@gmail.com>
2024-05-02 11:05:53 -0600

    fx

--- app/Observers/RequisicionesObserver.php
+++ app/Observers/RequisicionesObserver.php
@@ -17,7 +17,6 @@ class RequisicionesObserver
         event(new RequisicionesEvent($requisiciones, 'create', 'requisiciones', 'Requisiciones'));
     }
 
-
     /**
      * Handle the requisiciones "deleted" event.
      *
