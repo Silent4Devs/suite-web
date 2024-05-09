125667cd131af92811433317c8b88606f5d365fe
Saul183 <152949586+Saul183@users.noreply.github.com>
2024-05-09 16:52:21 -0600

    erro

--- app/Notifications/DocumentoNotification.php
+++ app/Notifications/DocumentoNotification.php
@@ -69,8 +69,6 @@ class DocumentoNotification extends Notification
     {
         return [
             'id' => $this->documento->id,
-            'updated_at' => $this->documento->updated_at,
-            'deleted_at' => $this->documento->deleted_at,
             'time' => Carbon::now(),
             'type' => $this->tipo_consulta,
             'tabla' => $this->tabla,
