c4798d5cbdd2ecb3deae47b95655dc573ab24fc8
vojohn95 <vojohn95@gmail.com>
2024-05-09 14:33:11 -0600

    fx

--- resources/views/items-notificaciones/item-auditoria-anual.blade.php
+++ resources/views/items-notificaciones/item-auditoria-anual.blade.php
@@ -2,14 +2,16 @@
     <div style="{{ $place == 'notificaciones-page' ? 'flex-basis: calc(80% - 5px)' : 'flex-basis:100%' }}">
         <a class="dropdown-item text-secondary"
             href="{{ route('admin.auditoria-anuals.show', $last_unread_notification->data['id']) }}">
-            @switch(" ".$last_unread_notification->data['type']) {{-- Se concatena un espacio porque el autoformateado lo agrega en el case --}}
-                @case(" create")
+            @switch(" ".$last_unread_notification->data['type'])
+                {{-- Se concatena un espacio porque el autoformateado lo agrega en el case --}}
+                @case(' create')
                     <div class="d-flex align-items-center justify-content-start">
                         <i class="pr-2 fas fa-tasks text-success"></i>
                         <p class="p-0 m-0">Nueva {{ $last_unread_notification->data['slug'] }} creado</p>
                     </div>
                 @break
-                @case(" update")
+
+                @case(' update')
                     <div class="d-flex align-items-center justify-content-start">
                         <i class="pr-2 fas fa-tasks text-info"></i>
                         <p class="p-0 m-0">
@@ -19,7 +21,8 @@
                         </p>
                     </div>
                 @break
-                @case(" delete")
+
+                @case(' delete')
                     <div class="d-flex align-items-center justify-content-start">
                         <i class="pr-2 fas fa-tasks text-danger"></i>
                         <p class="p-0 m-0">
@@ -29,6 +32,7 @@
                         </p>
                     </div>
                 @break
+
                 @default
             @endswitch
         </a>
