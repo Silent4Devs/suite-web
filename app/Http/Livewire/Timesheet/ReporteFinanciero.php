8607113e3d1a113dc7ae3641f8b767374230eda3
MartiDavid23 <marti.david@silent4business.com>
2024-05-09 18:07:01 -0600

    se agrega filtro para horas aceptadas

--- app/Http/Livewire/Timesheet/ReporteFinanciero.php
+++ app/Http/Livewire/Timesheet/ReporteFinanciero.php
@@ -24,53 +24,53 @@ class ReporteFinanciero extends Component
 
     public function render()
     {
-        $this->proyectos_select = TimesheetProyecto::getAllWithData();
-        $this->proyectos_select = $this->proyectos_select->sortByDesc('is_num');
+        $this->proyectos_select = TimesheetProyecto::getAllWithData()->sortByDesc('is_num');
         $organizacion_actual = $this->obtenerOrganizacion();
         $logo_actual = $organizacion_actual->logo;
         $empresa_actual = $organizacion_actual->empresa;
         $this->proyectos = null;
+
         if ($this->selectedProjectId) {
-            $selectedProjectId = $this->selectedProjectId;
-            $proyectoId = $selectedProjectId;
+            $proyectoId = $this->selectedProjectId;
 
             $ids_emp = TimesheetProyectoEmpleado::where('proyecto_id', $proyectoId)->get(['empleado_id', 'area_id', 'costo_hora']);
             $empleadosIds = $ids_emp->pluck('empleado_id');
 
             $horas = TimesheetHoras::where('proyecto_id', $proyectoId)
+                ->with('timesheet')
+                ->whereHas('timesheet', function ($query) {
+                    $query->where('estatus', 'aprobado');
+                })
                 ->whereIn('empleado_id', $empleadosIds)
                 ->get(['empleado_id', 'horas_lunes', 'horas_martes', 'horas_miercoles', 'horas_jueves', 'horas_viernes', 'horas_sabado', 'horas_domingo']);
 
             $empleados = [];
-            foreach ($ids_emp as $emp_p) {
-                $total_horas = 0;
 
-                foreach ($horas as $hora) {
-                    if ($hora->empleado_id === $emp_p->empleado_id) {
-                        $total_horas += array_sum(array_filter([
-                            $hora->horas_lunes,
-                            $hora->horas_martes,
-                            $hora->horas_miercoles,
-                            $hora->horas_jueves,
-                            $hora->horas_viernes,
-                            $hora->horas_sabado,
-                            $hora->horas_domingo
-                        ], 'is_numeric'));
-                    }
-                }
+            foreach ($ids_emp as $emp_p) {
+                $total_horas = $horas->where('empleado_id', $emp_p->empleado_id)->sum(function ($hora) {
+                    return array_sum(array_filter([
+                        $hora->horas_lunes,
+                        $hora->horas_martes,
+                        $hora->horas_miercoles,
+                        $hora->horas_jueves,
+                        $hora->horas_viernes,
+                        $hora->horas_sabado,
+                        $hora->horas_domingo
+                    ], 'is_numeric'));
+                });
 
                 $costo_por_hora_usuario = $emp_p->costo_hora ?? 0;
-                $costo_horas = $costo_por_hora_usuario * $total_horas;
+                if (!$costo_por_hora_usuario) {
+                    $salario_base = $emp_p->empleado->salario_base_mensual ?? $emp_p->empleado->salario_diario ?? 0;
+                    $costo_por_hora_usuario = ($salario_base / 20) / 7;
+                }
 
                 $empleado = Empleado::select('id', 'name')->find($emp_p->empleado_id);
                 $area = Area::select('id', 'area')->find($emp_p->area_id);
-                $proyecto = TimesheetProyecto::select('id', 'proyecto', 'estatus', 'cliente_id','identificador')->find($proyectoId);
+                $proyecto = TimesheetProyecto::select('id', 'proyecto', 'estatus', 'cliente_id', 'identificador')->find($proyectoId);
                 $cliente = TimesheetCliente::select('id', 'nombre')->find($proyecto->cliente_id);
 
                 $horasTotalesProyecto = $this->getHorasTotales($proyectoId);
-
-                $costoTotal = $horasTotalesProyecto['horasCosto'];
-                $horaTotal = $horasTotalesProyecto['totalhoras'];
 
                 $empleados[$empleado->id] = [
                     'id' => $empleado->id,
@@ -81,68 +81,57 @@ class ReporteFinanciero extends Component
                     'proyecto' => $proyecto->proyecto,
                     'estatus' => $proyecto->estatus,
                     'horasEmpleado' => $total_horas,
-                    'costoHoraEmpleado' => $costo_horas,
+                    'costoHoraEmpleado' => $costo_por_hora_usuario * $total_horas,
                     'cliente' => $cliente->nombre,
-                    'horaTotal' => $horaTotal,
-                    'costoTotal' => $costoTotal,
+                    'horaTotal' => $horasTotalesProyecto['totalhoras'],
+                    'costoTotal' => $horasTotalesProyecto['horasCosto'],
                     'identificador' => $proyecto->identificador,
-
                 ];
             }
-            //dd($selectedProjectId);
 
             $this->proyectos = $empleados;
         }
+
         $this->emit('scriptTabla');
         return view('livewire.timesheet.reporte-financiero', compact('logo_actual', 'empresa_actual'));
     }
 
+
     public function getHorasTotales($id)
     {
         $ids_emp = TimesheetProyectoEmpleado::where('proyecto_id', $id)->get();
-        $horasTotales = 0;
-        $horasCosto = 0;
-        $totalhoras = 0;
+
+        $horas = TimesheetHoras::where('proyecto_id', $id)
+            ->with('timesheet')
+            ->whereHas('timesheet', function ($query) {
+                $query->where('estatus', 'aprobado');
+            })
+            ->whereIn('empleado_id', $ids_emp->pluck('empleado_id'))
+            ->get();
+
+        $horasTotales = $horasCosto = $totalhoras = 0;
+
         foreach ($ids_emp as $emp_p) {
+            $horasEmpleado = $horas->where('empleado_id', $emp_p->empleado_id);
+            $total_horas = $horasEmpleado->sum('horas_lunes')
+                + $horasEmpleado->sum('horas_martes')
+                + $horasEmpleado->sum('horas_miercoles')
+                + $horasEmpleado->sum('horas_jueves')
+                + $horasEmpleado->sum('horas_viernes')
+                + $horasEmpleado->sum('horas_sabado')
+                + $horasEmpleado->sum('horas_domingo');
 
-            $empItem = Empleado::select('id', 'name')->where('id', $emp_p->empleado_id)->first();
-            $horas = TimesheetHoras::where('proyecto_id', $id)
-                ->where('empleado_id', $empItem->id)
-                ->get();
-            // Si hay horas para este empleado, sumar las horas de los diferentes días
-
-            $total_horas = 0;
-
-            foreach ($horas as $hora) {
-                $total_horas += is_numeric($hora->horas_lunes) ? $hora->horas_lunes : 0;
-                $total_horas += is_numeric($hora->horas_martes) ? $hora->horas_martes : 0;
-                $total_horas += is_numeric($hora->horas_miercoles) ? $hora->horas_miercoles : 0;
-                $total_horas += is_numeric($hora->horas_jueves) ? $hora->horas_jueves : 0;
-                $total_horas += is_numeric($hora->horas_viernes) ? $hora->horas_viernes : 0;
-                $total_horas += is_numeric($hora->horas_sabado) ? $hora->horas_sabado : 0;
-                $total_horas += is_numeric($hora->horas_domingo) ? $hora->horas_domingo : 0;
-            }
-            $horasTotales = $total_horas;
-            $totalhoras += $horasTotales;
+            $horasTotales += $total_horas;
             $costo_por_hora_usuario = $emp_p->costo_hora ?? 0;
-
             if (!$costo_por_hora_usuario) {
-                if (isset($emp_p->empleado->salario_base_mensual)) {
-                    $costo_por_hora_usuario = ($emp_p->empleado->salario_base_mensual / 20) / 7;
-                } else {
-                    if (isset($emp_p->empleado->salario_diario)) {
-                        $costo_por_hora_usuario = $emp_p->empleado->salario_diario / 7;
-                    } else {
-                        $costo_por_hora_usuario = 0;
-                    }
-                }
+                $salario_base = $emp_p->empleado->salario_base_mensual ?? $emp_p->empleado->salario_diario ?? 0;
+                $costo_por_hora_usuario = ($salario_base / 20) / 7;
             }
-            $horasCosto += $costo_por_hora_usuario * $horasTotales;
+            $horasCosto += $costo_por_hora_usuario * $total_horas;
         }
-
         return [
             'horasCosto' => $horasCosto,
-            'totalhoras' => $totalhoras
+            'totalhoras' => $horasTotales
         ];
     }
 }
