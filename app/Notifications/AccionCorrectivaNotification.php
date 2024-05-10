7b8bc8f5d2c10ef2dd50ead00bf5443a2899b83e
Saul183 <152949586+Saul183@users.noreply.github.com>
2024-05-10 11:20:07 -0600

    foto

--- app/Notifications/AccionCorrectivaNotification.php
+++ app/Notifications/AccionCorrectivaNotification.php
@@ -75,6 +75,7 @@ class AccionCorrectivaNotification extends Notification
             'tabla' => $this->tabla,
             'slug' => $this->slug,
             'name' => Auth::user()->name,
+            'avatar_ruta' => Auth::user()->empleado->avatar_ruta,
         ];
     }
 }
