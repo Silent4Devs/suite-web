0cdd29c107edf52e9b32288e504c19a28fd5e9e9
vicvicto <vhraindiana@hotmail.com>
2024-05-22 12:16:14 -0600

    Lista Dist lideres

---
+++ database/seeders/ListaDistribucionSuplentesLideresSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Area;
+use App\Models\ListaDistribucion;
+use App\Models\ParticipantesListaDistribucion;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ListaDistribucionSuplentesLideresSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $areas = Area::get();
+        $cuentaArea = $areas->count();
+
+        $modulo = ListaDistribucion::create([
+            'modulo' => 'Gestion Talento',
+            'submodulo' => 'Lideres',
+            'modelo' => 'Empleado',
+            'niveles' => $cuentaArea,
+        ]);
+
+        foreach ($areas as $key => $area) {
+
+            $lider = ParticipantesListaDistribucion::create([
+                'modulo_id' => $modulo->id,
+                'nivel' => $key + 1,
+                'numero_orden' => 1,
+                'empleado_id' => $area->lider->id,
+            ]);
+        }
+    }
+}
