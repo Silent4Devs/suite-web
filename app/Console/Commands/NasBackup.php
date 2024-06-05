93b0e4668a407a137e9788a332499a122319e879
johnvo <vojohn95@gmail.com>
2024-06-05 14:19:03 -0600

    primer commit

---
+++ app/Console/Commands/NasBackup.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class NasBackup extends Command
+{
+    protected $nombre;
+
+    protected $correodestinatario;
+
+    protected $cumplehoy;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'transfer:nas';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Felicitar empleados';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $cumplehoy = Carbon::today();
+        $cumplehoy->toDateString();
+        // dd($cumplehoy);
+        $cumpleañeros = Empleado::alta()
+            ->whereMonth('cumpleaños', '=', $cumplehoy->format('m'))
+            ->whereDay('cumpleaños', '=', $cumplehoy->format('d'))
+            ->get();
+
+        $imgtab = public_path("img\icono_tabantaj.png");
+        $imgpastel = public_path('img\pastel.png');
+
+        if ($cumpleañeros != null) {
+            foreach ($cumpleañeros as $cumpleañero) {
+                $filtro = CorreoCumpleanos::where('empleado_id', $cumpleañero->id)
+                    ->whereDate('fecha_envio', '=', $cumplehoy);
+                if ($filtro->exists() == false) {
+                    // dd("Si aparece");
+                    $empcump = CorreoCumpleanos::firstOrCreate([
+                        'empleado_id' => $cumpleañero->id,
+                        'fecha_envio' => $cumplehoy,
+                        'enviado' => false,
+                    ]);
+                    // dd("Si crea el registro");
+                    $nombre = $cumpleañero->name;
+                    $correodestinatario = $cumpleañero->email;
+
+                    $email = new FelicitacionesMail($nombre, $correodestinatario, $imgpastel, $imgtab);
+                    Mail::to(removeUnicodeCharacters($correodestinatario))->queue($email);
+                    // dd('Si manda el correo');
+                    $empcump->update([
+                        'enviado' => true,
+                    ]);
+                } else {
+                    //No hace nada
+                }
+            }
+        }
+    }
+}
