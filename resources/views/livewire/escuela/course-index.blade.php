1f386e43d733e386c07900359e1173c5d0c6998c
BenitoLM <benito.lopez.tecno@gmail.com>
2024-06-05 17:25:33 -0600

    merger

--- resources/views/livewire/escuela/course-index.blade.php
+++ resources/views/livewire/escuela/course-index.blade.php
@@ -46,8 +46,7 @@
                         <p>Creado:</p>
                         <div class="d-flex align-items-center gap-1">
                             <div class="img-person">
-                                <img src="{{ asset('storage/empleados/imagenes/' . isset($c->teacher->empleado) ? $c->teacher->empleado->avatar : '') }}"
-                                    alt="{{ $c->teacher->name }}">
+                                <img src="{{ $c->teacher->empleado->avatar_ruta }}" alt="{{ $c->teacher->name }}">
                             </div>
                             {{ $c->teacher->name }}
                         </div>
@@ -55,8 +54,7 @@
                             <p>Instructor: </p>
                             <div class="d-flex align-items-center gap-1">
                                 <div class="img-person">
-                                    <img src="{{ asset('storage/empleados/imagenes/' . isset($instructor->empleado->foto) ? $instructor->empleado->foto : '') }}"
-                                        alt="{{ $instructor->name }}">
+                                    <img src="{{ $instructor->empleado->avatar_ruta }}" alt="{{ $instructor->name }}">
                                 </div>
                                 {{ $instructor->name }}
                             </div>
