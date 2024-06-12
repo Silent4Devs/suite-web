9b0559ebb60aeef0dd8393fd102d1f7cee837975
Beni Mejia <71240244+BenitoLopez12@users.noreply.github.com>
2024-06-12 15:41:15 -0600

    merger

--- resources/views/admin/pasarelaPago/inicio-servicios.blade.php
+++ resources/views/admin/pasarelaPago/inicio-servicios.blade.php
@@ -1,7 +1,72 @@
 @extends('layouts.admin')
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/pasarelaPago/pasarelaPago.css') }}">
+@endsection
+@section('styles')
+    <style>
+    </style>
+@endsection
 @section('content')
-    <h5 class="titulo_general_funcion">Inicio</h5>
-    content pasarela de pago
+    <div class="menu-pasarela">
+        <ul>
+            <li> <strong> Aplicaciones </strong> </li>
+            <li>
+                <i class="material-symbols-outlined">apps</i>
+                Todas las aplicaciones
+            </li>
+            <li>
+                <i class="material-symbols-outlined">install_desktop</i>
+                Actualizaciones
+            </li>
+        </ul>
+
+        <ul class="mt-5">
+            <li> <strong> Planes </strong> </li>
+            <li>
+                <i class="material-symbols-outlined">credit_card</i>
+                Planes y Precios
+            </li>
+        </ul>
+
+        <ul class="mt-5">
+            <li> <strong> Aplicaciones </strong> </li>
+            <li>
+                <i class="material-symbols-outlined">school</i>
+                Capacitación
+            </li>
+            <li>
+                <i class="material-symbols-outlined">language</i>
+                Gestión Normativa
+            </li>
+            <li>
+                <i class="material-symbols-outlined">quick_reference</i>
+                Planes de trabajo
+            </li>
+            <li>
+                <i class="material-symbols-outlined">folder_managed</i>
+                Gestor Documental
+            </li>
+            <li>
+                <i class="material-symbols-outlined">install_desktop</i>
+                Gestión de Talento
+            </li>
+            <li>
+                <i class="material-symbols-outlined">quick_reference</i>
+                Gestión Contractual
+            </li>
+            <li>
+                <i class="material-symbols-outlined">gpp_maybe</i>
+                Gestión de Riesgos
+            </li>
+            <li>
+                <i class="material-symbols-outlined">groups</i>
+                Visitantes
+            </li>
+        </ul>
+    </div>
+    <div>
+
+    </div>
 @endsection
 @section('scripts')
 @endsection
