82ff4e6dd4ac6be27a8b252b4203b80d51e1ef1d
vojohn95 <vojohn95@gmail.com>
2024-06-13 18:18:41 -0600

    fx

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,25 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import laravel from 'laravel-vite-plugin';
+import laravel from "laravel-vite-plugin";
 export default defineConfig({
   plugins: [
     laravel({
       input: [
-            "public/css/app.css",
-            "public/css/style.css",
-            "public/css/admin.css",
-            "public/css/rds.css",
+        "public/css/app.css",
+        "public/css/style.css",
+        "public/css/admin.css",
+        "public/css/rds.css",
 
-            "resources/js/app.js",
-            'resources/js/app.js',
-            'public/js/pages/index/template-analisis-riesgos.jsx',
-            'public/js/pages/index/FormulasAnalisisRiesgos.jsx',
-            'public/js/pages/index/SettingsAnalisisRiesgos.jsx',
-            'public/js/pages/index/TemplateViewPrevAnalisisRiesgos.jsx',
-        ],
+        "resources/js/app.js",
+        "resources/js/app.js",
+        "public/js/pages/index/template-analisis-riesgos.jsx",
+        "public/js/pages/index/FormulasAnalisisRiesgos.jsx",
+        "public/js/pages/index/SettingsAnalisisRiesgos.jsx",
+        "public/js/pages/index/TemplateViewPrevAnalisisRiesgos.jsx"
+      ],
       refresh: true
     })
   ]
-
 });
