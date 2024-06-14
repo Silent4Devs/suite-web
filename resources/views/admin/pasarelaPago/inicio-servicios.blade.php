3f3f76932c7ee8a5f53e3c09cbc2b8fe61045650
Beni Mejia <71240244+BenitoLopez12@users.noreply.github.com>
2024-06-14 01:22:13 -0600

    servicios

--- resources/views/admin/pasarelaPago/inicio-servicios.blade.php
+++ resources/views/admin/pasarelaPago/inicio-servicios.blade.php
@@ -64,8 +64,139 @@
             </li>
         </ul>
     </div>
-    <div>
 
+    <div class="content-pasarela">
+        <div class="d-flex portada-pasarela">
+            <img src="{{ asset('img/pasarelaPago/portada-inicio.png') }}" alt="">
+            <div class="text-white portada-pasarela-info p-5">
+                <h3>Software Integral de Gestión Empresarial</h3>
+                <p class="p-3">
+                    Planifica, y analiza las operaciones contables de la compañía.
+                </p>
+                <p class="mt-5">
+                    <strong>
+                        Elige tus aplicaciones
+                    </strong>
+                </p>
+            </div>
+        </div>
+
+        <div class="instaladas mt-5">
+            <h4>Instaladas</h4>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card card-body">
+                        <div class="d-flex align-items-center gap-2">
+                            <i class="material-symbols-outlined icon-background">school</i>
+                            <span>Capacitación</span>
+                        </div>
+                        <a href="" class="btn">Abrir</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card card-body">
+                        <div class="d-flex align-items-center gap-2">
+                            <i class="material-symbols-outlined icon-background">school</i>
+                            <span>Capacitación</span>
+                        </div>
+                        <a href="" class="btn">Abrir</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-6">
+                    <div class="card card-body">
+                        <div class="d-flex align-items-center gap-2">
+                            <i class="material-symbols-outlined icon-background">school</i>
+                            <span>Capacitación</span>
+                        </div>
+                        <a href="" class="btn">Abrir</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card card-body">
+                        <div class="d-flex align-items-center gap-2">
+                            <i class="material-symbols-outlined icon-background">school</i>
+                            <span>Capacitación</span>
+                        </div>
+                        <a href="" class="btn">Abrir</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="tu-plan">
+            <h4>Disponible en tu plan</h4>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card card-body">
+                        <div>
+                            <i class="material-symbols-outlined icon-background">school</i>
+                            Plan de trabajo
+                        </div>
+                        <p class="text-center">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                        <hr>
+                        <div class="text-center">
+                            <a class="btn btn-primary" href="">Agregar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card card-body">
+                        <div>
+                            <i class="material-symbols-outlined icon-background">school</i>
+                            Plan de trabajo
+                        </div>
+                        <p class="text-center">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                        <hr>
+                        <div class="text-center">
+                            <a class="btn btn-primary" href="">Agregar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card card-body">
+                        <div>
+                            <i class="material-symbols-outlined icon-background">school</i>
+                            Plan de trabajo
+                        </div>
+                        <p class="text-center">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                        <hr>
+                        <div class="text-center">
+                            <a class="btn btn-primary" href="">Agregar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card card-body">
+                        <div>
+                            <i class="material-symbols-outlined icon-background">school</i>
+                            Plan de trabajo
+                        </div>
+                        <p class="text-center">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.
+                        </p>
+                        <hr>
+                        <div class="text-center">
+                            <a class="btn btn-primary" href="">Agregar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 @section('scripts')
