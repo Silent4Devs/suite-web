3175835139ce7d25d08ce62aab4dd2ff2a205e55
vojohn95 <vojohn95@gmail.com>
2024-07-06 01:10:32 -0600

    fx

--- app/Http/Controllers/Admin/DeskController.php
+++ app/Http/Controllers/Admin/DeskController.php
@@ -27,6 +27,7 @@ use App\Models\FirmaCentroAtencion;
 use App\Models\FirmaModule;
 use App\Models\IncidentesSeguridad;
 use App\Models\Mejoras;
+use App\Models\Organizacion;
 use App\Models\Proceso;
 use App\Models\Quejas;
 use App\Models\QuejasCliente;
@@ -39,7 +40,6 @@ use App\Models\TimesheetProyecto;
 use App\Models\User;
 use App\Traits\ObtenerOrganizacion;
 use Auth;
-use App\Models\Organizacion;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -284,7 +284,7 @@ class DeskController extends Controller
 
         $empleadoIds = $request->participantes ?? [];
 
-        if (empty($empleadoIds) || !is_array($empleadoIds)) {
+        if (empty($empleadoIds) || ! is_array($empleadoIds)) {
             return back()->with('error', 'No se seleccionaron participantes para la aprobacion.');
         }
 
@@ -324,13 +324,13 @@ class DeskController extends Controller
 
         $documento = $incidentesSeguridad->evidencia;
 
-        if ($request->file('evidencia') != null or !empty($request->file('evidencia'))) {
+        if ($request->file('evidencia') != null or ! empty($request->file('evidencia'))) {
             foreach ($request->file('evidencia') as $file) {
                 $extension = pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
 
-                $name_documento = basename(pathinfo($file->getClientOriginalName(), PATHINFO_BASENAME), '.' . $extension);
+                $name_documento = basename(pathinfo($file->getClientOriginalName(), PATHINFO_BASENAME), '.'.$extension);
 
-                $new_name_documento = 'Seguridad_file_' . $incidentesSeguridad->id . '_' . $name_documento . '.' . $extension;
+                $new_name_documento = 'Seguridad_file_'.$incidentesSeguridad->id.'_'.$name_documento.'.'.$extension;
 
                 $route = 'public/evidencias_seguridad';
 
@@ -532,7 +532,7 @@ class DeskController extends Controller
 
         $empleadoIds = $request->participantes;
 
-        if (empty($empleadoIds) || !is_array($empleadoIds)) {
+        if (empty($empleadoIds) || ! is_array($empleadoIds)) {
             return back()->with('error', 'No se seleccionaron participantes para la aprobacion.');
         }
 
@@ -718,7 +718,7 @@ class DeskController extends Controller
 
         $empleadoIds = $request->participantes;
 
-        if (empty($empleadoIds) || !is_array($empleadoIds)) {
+        if (empty($empleadoIds) || ! is_array($empleadoIds)) {
             return back()->with('error', 'No se seleccionaron participantes para la aprobacion.');
         }
 
@@ -904,7 +904,7 @@ class DeskController extends Controller
 
         $empleadoIds = $request->participantes;
 
-        if (empty($empleadoIds) || !is_array($empleadoIds)) {
+        if (empty($empleadoIds) || ! is_array($empleadoIds)) {
             return back()->with('error', 'No se seleccionaron participantes para la aprobacion.');
         }
 
@@ -916,7 +916,7 @@ class DeskController extends Controller
         $organizacion = Organizacion::first();
         // Enviar correos electrónicos
         foreach ($empleados as $empleado) {
-            Mail::to(trim($this->removeUnicodeCharacters($empleado->email)))->send(new EmpleadoEmail($empleado, $status, $denuncias->id,  $organizacion));
+            Mail::to(trim($this->removeUnicodeCharacters($empleado->email)))->send(new EmpleadoEmail($empleado, $status, $denuncias->id, $organizacion));
         }
 
         $denuncias->update([
@@ -1094,7 +1094,7 @@ class DeskController extends Controller
 
         $empleadoIds = $request->participantes;
 
-        if (empty($empleadoIds) || !is_array($empleadoIds)) {
+        if (empty($empleadoIds) || ! is_array($empleadoIds)) {
             return back()->with('error', 'No se seleccionaron participantes para la aprobacion.');
         }
 
@@ -1269,7 +1269,7 @@ class DeskController extends Controller
 
         $empleadoIds = $request->participantes;
 
-        if (empty($empleadoIds) || !is_array($empleadoIds)) {
+        if (empty($empleadoIds) || ! is_array($empleadoIds)) {
             return back()->with('error', 'No se seleccionaron participantes para la aprobacion.');
         }
 
@@ -1281,7 +1281,7 @@ class DeskController extends Controller
         $organizacion = Organizacion::first();
         // Enviar correos electrónicos
         foreach ($empleados as $empleado) {
-            Mail::to(trim($this->removeUnicodeCharacters($empleado->email)))->send(new EmpleadoEmail($empleado, $status, $sugerencias->id,  $organizacion));
+            Mail::to(trim($this->removeUnicodeCharacters($empleado->email)))->send(new EmpleadoEmail($empleado, $status, $sugerencias->id, $organizacion));
         }
 
         $sugerencias->update([
@@ -1423,13 +1423,13 @@ class DeskController extends Controller
 
         $image = null;
 
-        if ($request->file('evidencia') != null or !empty($request->file('evidencia'))) {
+        if ($request->file('evidencia') != null or ! empty($request->file('evidencia'))) {
             foreach ($request->file('evidencia') as $file) {
                 $extension = pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
 
-                $name_image = basename(pathinfo($file->getClientOriginalName(), PATHINFO_BASENAME), '.' . $extension);
+                $name_image = basename(pathinfo($file->getClientOriginalName(), PATHINFO_BASENAME), '.'.$extension);
 
-                $new_name_image = 'Queja_file_' . $quejasClientes->id . '_' . $name_image . '.' . $extension;
+                $new_name_image = 'Queja_file_'.$quejasClientes->id.'_'.$name_image.'.'.$extension;
 
                 $route = 'public/evidencias_quejas_clientes';
 
@@ -1557,13 +1557,13 @@ class DeskController extends Controller
 
         $documento = null;
 
-        if ($request->file('evidencia') != null or !empty($request->file('evidencia'))) {
+        if ($request->file('evidencia') != null or ! empty($request->file('evidencia'))) {
             foreach ($request->file('evidencia') as $file) {
                 $extension = pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
 
-                $name_documento = basename(pathinfo($file->getClientOriginalName(), PATHINFO_BASENAME), '.' . $extension);
+                $name_documento = basename(pathinfo($file->getClientOriginalName(), PATHINFO_BASENAME), '.'.$extension);
 
-                $new_name_documento = 'Queja_file_' . $quejasClientes->id . '_' . $name_documento . '.' . $extension;
+                $new_name_documento = 'Queja_file_'.$quejasClientes->id.'_'.$name_documento.'.'.$extension;
 
                 $route = 'public/evidencias_quejas_clientes';
 
@@ -1580,13 +1580,13 @@ class DeskController extends Controller
 
         $image = null;
 
-        if ($request->file('cierre') != null or !empty($request->file('cierre'))) {
+        if ($request->file('cierre') != null or ! empty($request->file('cierre'))) {
             foreach ($request->file('cierre') as $file) {
                 $extension = pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
 
-                $name_image = basename(pathinfo($file->getClientOriginalName(), PATHINFO_BASENAME), '.' . $extension);
+                $name_image = basename(pathinfo($file->getClientOriginalName(), PATHINFO_BASENAME), '.'.$extension);
 
-                $new_name_image = 'Queja_file_' . $quejasClientes->id . '_' . $name_image . '.' . $extension;
+                $new_name_image = 'Queja_file_'.$quejasClientes->id.'_'.$name_image.'.'.$extension;
 
                 $route = 'public/evidencias_quejas_clientes_cerrado';
 
@@ -1616,7 +1616,7 @@ class DeskController extends Controller
 
         if ($notificar_atencion_queja_no_aprobada) {
             if ($cerrar_ticket == false) {
-                if (!$quejasClientes->email_env_resolucion_rechazada) {
+                if (! $quejasClientes->email_env_resolucion_rechazada) {
                     if ($quejasClientes->registro != null && $quejasClientes->responsableAtencion != null) {
                         $quejasClientes->update([
                             'email_env_resolucion_rechazada' => true,
@@ -1629,7 +1629,7 @@ class DeskController extends Controller
 
         if ($notificar_atencion_queja_no_aprobada) {
             if ($cerrar_ticket) {
-                if (!$quejasClientes->email_env_resolucion_aprobada) {
+                if (! $quejasClientes->email_env_resolucion_aprobada) {
                     if ($quejasClientes->registro != null && $quejasClientes->responsableAtencion != null) {
                         $quejasClientes->update([
                             'email_env_resolucion_aprobada' => true,
@@ -1640,8 +1640,8 @@ class DeskController extends Controller
             }
         }
 
-        if (!$email_realizara_accion_inmediata) {
-            if (!is_null($quejasClientes->acciones_tomara_responsable)) {
+        if (! $email_realizara_accion_inmediata) {
+            if (! is_null($quejasClientes->acciones_tomara_responsable)) {
                 if ($quejasClientes->registro != null && $quejasClientes->responsableAtencion != null) {
                     $quejasClientes->update([
                         'email_realizara_accion_inmediata' => true,
@@ -1652,7 +1652,7 @@ class DeskController extends Controller
         }
 
         if ($notificar_registro_queja) {
-            if (!$quejasClientes->correo_enviado_registro) {
+            if (! $quejasClientes->correo_enviado_registro) {
                 if ($quejasClientes->registro != null && $quejasClientes->responsableAtencion != null) {
                     $quejasClientes->update([
                         'correo_enviado_registro' => true,
@@ -1673,7 +1673,7 @@ class DeskController extends Controller
                 $query->where('acciones_correctivas_aprobacionables_id', $quejasClientes->id);
             })->exists();
 
-            if (!$existeAC) {
+            if (! $existeAC) {
                 $accion_correctiva = AccionCorrectiva::create([
                     'tema' => $request->titulo,
                     'causaorigen' => 'Queja de un cliente',
@@ -1700,7 +1700,7 @@ class DeskController extends Controller
                 $quejasClientes->accionCorrectivaAprobacional()->sync($accion_correctiva->id);
             }
 
-            if (!$quejasClientes->correoEnviado) {
+            if (! $quejasClientes->correoEnviado) {
                 $quejasClientes->update([
                     'correoEnviado' => true,
                 ]);
@@ -2009,7 +2009,7 @@ class DeskController extends Controller
 
             return response()->json(['isValid' => true]);
         } elseif ($request->tipo_validacion == 'queja-atencion') {
-            if (!is_null($quejasClientes->responsable_atencion_queja_id)) {
+            if (! is_null($quejasClientes->responsable_atencion_queja_id)) {
                 if ($quejasClientes->responsable_atencion_queja_id != User::getCurrentUser()->empleado->id) {
                     $this->validateRequestRegistroQuejaCliente($request);
                     $this->validateRequestAnalisisQuejaCliente($request);
--- app/Mail/EmpleadoEmail.php
+++ app/Mail/EmpleadoEmail.php
@@ -12,8 +12,11 @@ class EmpleadoEmail extends Mailable
     use Queueable, SerializesModels;
 
     public $empleado;
+
     public $id;
+
     public $status;
+
     public $organizacion;
 
     /**
@@ -34,18 +37,18 @@ class EmpleadoEmail extends Mailable
         try {
             $img_route = $url;
             $logo_base = file_get_contents($img_route);
-            $img = 'data:image/png;base64,' . base64_encode($logo_base);
+            $img = 'data:image/png;base64,'.base64_encode($logo_base);
 
             return $img;
         } catch (\Exception $e) {
             try {
                 $img_route = $url;
                 $logo_base = Storage::get($img_route);
-                $img = 'data:image/png;base64,' . base64_encode($logo_base);
+                $img = 'data:image/png;base64,'.base64_encode($logo_base);
 
                 return $img;
             } catch (\Throwable $th) {
-                $img = 'data:image/png;base64,' . '';
+                $img = 'data:image/png;base64,'.'';
 
                 return $img;
             }
