d5eafc32ddd37bb3ea32b926a4821e86e056912b
Saul183 <152949586+Saul183@users.noreply.github.com>
2024-07-09 18:06:58 -0600

    firmas

--- resources/views/admin/desk/riesgos/edit.blade.php
+++ resources/views/admin/desk/riesgos/edit.blade.php
@@ -127,14 +127,18 @@
                                 <b>Datos generales:</b>
                             </div>
 
-                            @if ($riesgos->estatus === 'cerrado' || $riesgos->estatus === 'cancelado')
                             <div style="position: relative; left: 2rem;">
                                 <label>
-                                    <input type="checkbox" id="toggle-info" {{ !empty($aprobadoresArray) ? 'checked' : '' }}>
+                                    <input type="checkbox" id="toggle-info" {{ !empty($aprobadoresArray) ? 'checked' : '' }} {{ $riesgos->estatus === 'cerrado' || $riesgos->estatus === 'cancelado' ? '' : 'disabled' }}>
                                     Activar flujo de firma(s)
                                 </label>
+                                <br>
+                                @if($riesgos->estatus !== 'cerrado' && $riesgos->estatus !== 'cancelado')
+                                <span style="color: red;">No puedes guardar aprobadores hasta que cumplas con un estatus permitido.</span>
+                                @endif
                             </div>
 
+                            @if($riesgos->estatus === 'cerrado' || $riesgos->estatus === 'cancelado')
                             <div class="mt-2 form-group col-md-12">
                                 <div class="info-bar" id="info-bar" style="display: {{ !empty($aprobadoresArray) ? 'block' : 'none' }};">
                                     <p>Seleccione cuántos participantes de aprobación tendrá tu lista.</p>
@@ -157,7 +161,8 @@
                             </div>
                             @endif
 
-                            <div class="mt-2 form-group col-2">
+
+                            <div class="mt-2 form-group col-12">
                                 <label class="form-label"><i class="fas fa-ticket-alt iconos-crear"></i>Folio</label>
                                 <div class="form-control">{{ $riesgos->folio }}</div>
                             </div>
