d23b705adfbe999cb2a8472455564dd9fd5941fa
Beni Mejia <71240244+BenitoLopez12@users.noreply.github.com>
2024-07-10 11:34:11 -0600

    permisos doble carpeta

--- _ide_helper.php
+++ _ide_helper.php
@@ -5,7 +5,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 10.48.15.
+ * Generated for Laravel 10.48.16.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
