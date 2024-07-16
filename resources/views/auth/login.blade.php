6098c1d4cced9bcff46d7d64a1f949bcbe076f67
BenitoLM <benito.lopez.tecno@gmail.com>
2024-07-16 17:00:05 -0600

    loging

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,8 +19,22 @@
             }
         @endphp
 
-        <input id="email" name="email" type="text" class="{{ $errors->has('email') ? ' is-invalid ' : '' }}" required
-            autocomplete="email" placeholder="{{ trans('global.login_email') }}" value="{{ old('email', null) }}">
+        <div class="box-logo-org">
+            <img src="{{ asset('img/logotipo-tabantaj.png') }}" alt="Logo de la Organizacion">
+        </div>
+
+        <div class="text-iniciar">
+            Iniciar Sesión
+        </div>
+
+        <div class="input-item">
+            <label for="email" class="icon icon-box">
+                <img src="{{ asset('img/auth/icon-person.svg') }}" alt="Icon person">
+            </label>
+            <input id="email" name="email" type="text" class="{{ $errors->has('email') ? ' is-invalid ' : '' }}"
+                required autocomplete="email" placeholder="{{ trans('global.login_email') }}"
+                value="{{ old('email', null) }}">
+        </div>
 
         @if ($errors->has('email'))
             <div class="invalid-feedback">
@@ -28,22 +42,30 @@
             </div>
         @endif
 
-
-        <input id="password" name="password" type="password" class="{{ $errors->has('password') ? ' is-invalid' : '' }}"
-            required placeholder="{{ trans('global.login_password') }}">
+        <div class="input-item">
+            <label for="password" class="icon icon-box">
+                <img src="{{ asset('img/auth/icon-lock.svg') }}" alt="Icon person">
+            </label>
+            <input id="password" name="password" type="password"
+                class="{{ $errors->has('password') ? ' is-invalid' : '' }}" required
+                placeholder="{{ trans('global.login_password') }}">
+        </div>
 
         @if ($errors->has('password'))
             <div class="invalid-feedback">{{ $errors->first('password') }}</div>
         @endif
 
-        <div class="text-center">
-            <button type="submit" class="btn_enviar">Enviar</button>
+        <div class="button-item">
+            <button type="submit" class="btn_enviar">Entrar</button>
         </div>
-        @if (Route::has('password.request'))
-            <a class="" href="{{ route('password.request') }}">¿Olvidó su contraseña?</a>
-        @endif
-        <a class="" href="#" id="btn_modal_aviso">Aviso de privacidad </a>
-        <a href="{{ route('visitantes.presentacion') }}" id="registrar_visitantes">Registro de Visitantes</a>
+
+        <div class="box-links">
+            @if (Route::has('password.request'))
+                <a class="" href="{{ route('password.request') }}">¿Olvidó su contraseña?</a>
+            @endif
+            <a class="" href="#" id="btn_modal_aviso">Aviso de privacidad </a>
+            <a href="{{ route('visitantes.presentacion') }}" id="registrar_visitantes">Registro de Visitantes</a>
+        </div>
     </form>
     @include('auth.aviso-privacidad-s4b')
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="es-MX">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/auth/TBIconTabantaj.png') }}">
 
     <link rel="stylesheet" href="{{ asset('css/auth/TBlogin.css') }}">
 
@@ -24,14 +26,25 @@
                     <img src="{{ asset('img/auth/TBLogoColorCircle.png') }}" class="logo-circle" alt="Logo circulo">
                 </div>
             </div>
+            <div class="box-title-tbj">
+                <img src="{{ asset('img/auth/TBlogoLoginLetras.png') }}" alt="Logo Tabantaj" class="tbj-title">
+                <h2 class="text-by">
+                    By <strong>Silent</strong>for<strong>Business</strong>
+                </h2>
+            </div>
+            <h1 class="text-bienvenidos">
+                <strong> Bienvenidos al </strong> Sistema Integral de Gestión Empresarial
+            </h1>
         </div>
         <div class="aside-tbj">
-            <div>
+            <div class="aside-box-content">
                 @yield('content')
             </div>
         </div>
     </main>
 
+
+    <script src="{{ asset('js/auth/app.js') }}"></script>
     @yield('scripts')
     @livewireScripts
 </body>
