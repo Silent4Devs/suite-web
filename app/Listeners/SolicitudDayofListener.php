74515a26464ec4e2d8b453be7c735915d5172574
Saul183 <152949586+Saul183@users.noreply.github.com>
2024-07-24 12:50:30 -0600

    notificaciones reportadas

--- app/Listeners/SolicitudDayofListener.php
+++ app/Listeners/SolicitudDayofListener.php
@@ -32,13 +32,7 @@ class SolicitudDayofListener implements ShouldQueue
      */
     public function handle($event)
     {
-        // User::select('users.id', 'users.name', 'users.email', 'role_user.role_id')
-        //     ->join('role_user', 'role_user.user_id', '=', 'users.id')
-        //     ->where('role_user.role_id', '=', '1')->where('users.id', '!=', auth()->id())
-        //     ->get()
-        //     ->each(function (User $user) use ($event) {
-        //         Notification::send($user, new SolicitudDayofNotification($event->solicitud_dayof, $event->tipo_consulta, $event->tabla, $event->slug));
-        //     });
+
         $user = auth()->user();
         if ($user->empleado && $user->empleado->supervisor) {
             // Obtener al supervisor por su dirección de correo electrónico
