0feaae9e7ac9fd9c0ca1a0bbb4f7f59add1c6d36
Beni Mejia <71240244+BenitoLopez12@users.noreply.github.com>
2024-08-08 10:51:59 -0600

    merger

--- resources/views/admin/analisis-riesgos/tbShowRiskAnalysis.blade.php
+++ resources/views/admin/analisis-riesgos/tbShowRiskAnalysis.blade.php
@@ -77,28 +77,11 @@
 
     <div class="card card-body">
         <div class="row">
-            <div class="col-md-4 py-5">
-                <div class="form-group">
-                    <label for="">Metrica 1</label>
-                    <input type="text" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="">Metrica 2</label>
-                    <input type="text" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="">Metrica 3</label>
-                    <input type="text" class="form-control">
-                </div>
-
-                <div class="form-group text-right">
-                    <button class="btn btn-primary">Enviar</button>
-                </div>
+            <div class="col-md-6">
+                <div id="map-position-cl" style="width: 100%; height: 300px;"></div>
             </div>
-            <div class="col-md-8">
-                <div id="map-position-cl" style="width: 100%; height: 100%;"></div>
+            <div class="col-md-6">
+                <div id="map-position-cl-v2" style="width: 100%; height: 300px;"></div>
             </div>
         </div>
     </div>
@@ -128,6 +111,113 @@
             // prettier-ignore
             const data = [
                     [0, 0, 1],
+                    [0, 1, 1],
+                    [0, 2, 2],
+                    [0, 3, 3],
+                    [1, 0, 1],
+                    [1, 1, 2],
+                    [1, 2, 3],
+                    [1, 3, 4],
+                    [2, 0, 2],
+                    [2, 1, 3],
+                    [2, 2, 4],
+                    [2, 3, 4],
+                    [3, 0, 3],
+                    [3, 1, 4],
+                    [3, 2, 4],
+                    [3, 3, 4]
+                ]
+                .map(function(item) {
+                    return [item[1], item[0], item[2] || '-'];
+                });
+            option = {
+                tooltip: {
+                    position: 'top'
+                },
+                grid: {
+                    // top: '10%'
+                },
+                xAxis: {
+                    type: 'category',
+                    data: hours,
+                    splitArea: {
+                        show: true
+                    },
+                    name: "Probabilidad",
+                    nameLocation: "center",
+                    nameGap: 20,
+                    axisLabel: {
+                        show: false,
+                    },
+                    axisTick: {
+                        show: false,
+                    },
+                },
+                yAxis: {
+                    type: 'category',
+                    name: 'Valor Y',
+                    splitArea: {
+                        show: true
+                    },
+                    name: "Impacto",
+                    nameLocation: "center",
+                    nameGap: 20,
+                    axisLabel: {
+                        show: false,
+                    },
+                    axisTick: {
+                        show: false,
+                    },
+                },
+                visualMap: {
+                    show: false,
+                    min: 1,
+                    max: 4,
+                    calculable: true,
+                    orient: 'horizontal',
+                    left: 'center',
+                    inRange: {
+                        color: ['#4BCFA2', '#F2C322', '#FF8C34', '#FC5375']
+                    }
+                    // padding:5,
+                    // top: '5%'
+                },
+                series: [{
+                    // name: 'Punch Card',
+                    type: 'heatmap',
+                    data: data,
+                    label: {
+                        show: false,
+                    },
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }]
+            };
+
+            option && myChart.setOption(option);
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            var chartDom = document.getElementById('map-position-cl-v2');
+            var myChart = echarts.init(chartDom);
+            var option;
+
+            // prettier-ignore
+            const hours = [
+                '1a', '2a', '3a', '4a'
+            ];
+            // prettier-ignore
+            const days = [
+                '1', '2', '3', '4',
+            ];
+            // prettier-ignore
+            const data = [
+                    [0, 0, 5],
                     [0, 1, 1],
                     [0, 2, 2],
                     [0, 3, 3],
