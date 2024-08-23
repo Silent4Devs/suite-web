ba8f876905c2a90c7f6a8e085da58401853964ac
vicvicto <vhraindiana@hotmail.com>
2024-08-23 12:48:33 -0600

    Correccion tabla

--- resources/views/admin/recursos-humanos/evaluacion-360/objetivos-periodo/cargar-por-area.blade.php
+++ resources/views/admin/recursos-humanos/evaluacion-360/objetivos-periodo/cargar-por-area.blade.php
@@ -3,7 +3,5 @@
     <link rel="stylesheet" href="{{ asset('css/evaluations/evaluations.css') }}{{ config('app.cssVersion') }}">
 @endsection
 @section('content')
-
-@livewire('cargar-objetivos-area', ['id_area'=> $AreaID])
-
+    @livewire('cargar-objetivos-area', ['id_area' => $AreaID])
 @endsection
