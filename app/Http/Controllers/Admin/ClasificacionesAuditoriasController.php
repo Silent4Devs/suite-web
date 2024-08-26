1596058bceebe186eff3992aaf10eeb24c3d1b62
vicvicto <vhraindiana@hotmail.com>
2024-08-26 11:17:28 -0600

    Validacion Identificador

--- app/Http/Controllers/Admin/ClasificacionesAuditoriasController.php
+++ app/Http/Controllers/Admin/ClasificacionesAuditoriasController.php
@@ -79,7 +79,7 @@ class ClasificacionesAuditoriasController extends Controller
     {
         abort_if(Gate::denies('clasificaciones_auditorias_crear'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         $validatedData = $request->validate([
-            // 'identificador' => 'unique:clasificaciones_auditorias,identificador', // Ignora el actual en la validación
+            'identificador' => 'unique:clasificaciones_auditorias,identificador', // Ignora el actual en la validación
             'nombre' => 'required|unique:clasificaciones_auditorias,nombre_clasificaciones',
         ]);
 
@@ -134,8 +134,8 @@ class ClasificacionesAuditoriasController extends Controller
         // Validación
 
         $validatedData = $request->validate([
-            // 'identificador' => 'unique:clasificaciones_auditorias,identificador,' . $id . '', // Ignora el actual en la validación
-            'nombre' => 'required|unique:clasificaciones_auditorias,nombre_clasificaciones,'.$id.'',
+            'identificador' => 'unique:clasificaciones_auditorias,identificador,' . $id . '', // Ignora el actual en la validación
+            'nombre' => 'required|unique:clasificaciones_auditorias,nombre_clasificaciones,' . $id . '',
         ]);
 
         try {
