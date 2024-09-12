a63f304086470301c500399fd4a037c253ec0309
BenitoLM <benito.lopez.tecno@gmail.com>
2024-09-11 18:32:21 -0600

    permisos review

--- app/Http/Controllers/Admin/DashboardPermisosController.php
+++ app/Http/Controllers/Admin/DashboardPermisosController.php
@@ -25,9 +25,13 @@ class DashboardPermisosController extends Controller
         $dayOffMounth = SolicitudDayOff::where('fecha_inicio', '>=', $inicioMes)->where('fecha_fin', '<=', $hoy)->get();
         $permisoMounth = SolicitudPermisoGoceSueldo::where('fecha_inicio', '>=', $inicioMes)->where('fecha_fin', '<=', $hoy)->get();
 
-        $vacaciones = SolicitudVacaciones::get();
-        $dayOff = SolicitudDayOff::get();
-        $permisos = SolicitudPermisoGoceSueldo::get();
+        $vacaciones = SolicitudVacaciones::where('aprobacion', 3)->get();
+        $dayOff = SolicitudDayOff::where('aprobacion', 3)->get();
+        $permisos = SolicitudPermisoGoceSueldo::where('aprobacion', 3)->get();
+
+        $vacacionesEvents = collect();
+        $dayOffEvents = collect();
+        $permisosEvents = collect();
 
         if (($id == 'all') or ($id == 'todos') or ($id == 'todas')) {
             $areaSeleccionada = 'all';
@@ -49,12 +53,63 @@ class DashboardPermisosController extends Controller
             });
         }
 
+        foreach ($vacaciones as $vacacion) {
+            $vacacionesEvents->push([
+                'title' => 'Vacaciones - '.$vacacion->empleado->name,
+                'inicio' => [
+                    'dia' => Carbon::parse($vacacion->fecha_inicio)->format('d'),
+                    'mes' => Carbon::parse($vacacion->fecha_inicio)->format('m'),
+                    'año' => Carbon::parse($vacacion->fecha_inicio)->format('Y'),
+                ],
+                'fin' => [
+                    'mes' => Carbon::parse($vacacion->fecha_fin)->format('m'),
+                    'dia' => Carbon::parse($vacacion->fecha_fin)->format('d'),
+                    'año' => Carbon::parse($vacacion->fecha_fin)->format('Y'),
+                ],
+                'color' => '#428BEC',
+            ]);
+        }
+
+        foreach ($dayOff as $day) {
+            $dayOffEvents->push([
+                'title' => 'DayOff - '.$day->empleado->name,
+                'inicio' => [
+                    'dia' => Carbon::parse($day->fecha_inicio)->format('d'),
+                    'mes' => Carbon::parse($day->fecha_inicio)->format('m'),
+                    'año' => Carbon::parse($day->fecha_inicio)->format('Y'),
+                ],
+                'fin' => [
+                    'mes' => Carbon::parse($day->fecha_fin)->format('m'),
+                    'dia' => Carbon::parse($day->fecha_fin)->format('d'),
+                    'año' => Carbon::parse($day->fecha_fin)->format('Y'),
+                ],
+                'color' => '#428BEC',
+            ]);
+        }
+
+        foreach ($permisos as $permiso) {
+            $permisosEvents->push([
+                'title' => 'Permiso - '.$permiso->empleado->name,
+                'inicio' => [
+                    'dia' => Carbon::parse($permiso->fecha_inicio)->format('d'),
+                    'mes' => Carbon::parse($permiso->fecha_inicio)->format('m'),
+                    'año' => Carbon::parse($permiso->fecha_inicio)->format('Y'),
+                ],
+                'fin' => [
+                    'dia' => Carbon::parse($permiso->fecha_fin)->format('d'),
+                    'mes' => Carbon::parse($permiso->fecha_fin)->format('m'),
+                    'año' => Carbon::parse($permiso->fecha_fin)->format('Y'),
+                ],
+                'color' => '#428BEC',
+            ]);
+        }
+
         $areasToSelect = Area::orderBy('area', 'Asc')->get();
 
         $organizacion_actual = $this->obtenerOrganizacion();
         $logo_actual = $organizacion_actual->logo;
         $empresa_actual = $organizacion_actual->empresa;
 
-        return view('admin.dashboardSolicitudesPermisos.dashboardOrg', compact('logo_actual', 'empresa_actual', 'currentUser', 'areasToSelect', 'areaSeleccionada', 'vacacionesMounth', 'dayOffMounth', 'permisoMounth', 'vacaciones', 'dayOff', 'permisos'));
+        return view('admin.dashboardSolicitudesPermisos.dashboardOrg', compact('logo_actual', 'empresa_actual', 'currentUser', 'areasToSelect', 'areaSeleccionada', 'vacacionesMounth', 'dayOffMounth', 'permisoMounth', 'vacaciones', 'dayOff', '', 'vacacionesEvents', 'dayOffEvents', 'permisosEvents'));
     }
 }
