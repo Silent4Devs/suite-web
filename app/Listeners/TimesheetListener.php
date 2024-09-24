5d0343bc1e5d171a79c52770beca4a8866ca077a
Saul Ramirez Galicia <saul.ramirez@silent4business.com>
2024-09-24 14:04:31 -0600

    emails

--- app/Listeners/TimesheetListener.php
+++ app/Listeners/TimesheetListener.php
@@ -50,11 +50,4 @@ class TimesheetListener implements ShouldQueue
         }
 
     }
-
-
-    function removeUnicodeCharacters($string)
-    {
-        return trim(preg_replace('/[^\x00-\x7F]/u', '', $string));
-    }
-
 }
