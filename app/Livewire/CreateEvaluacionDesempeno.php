e3eaa266ac2011faa9c8bf48a7b5b88ca4338b82
Víctor Hugo Rodríguez Albarrá <victor.rodriguez@silent4business.com>
2024-09-25 12:27:13 -0600

    Correccion en borrador

--- app/Livewire/CreateEvaluacionDesempeno.php
+++ app/Livewire/CreateEvaluacionDesempeno.php
@@ -917,18 +917,18 @@ class CreateEvaluacionDesempeno extends Component
             // Comienza o continúa el borrador de la evaluación
             $evaluacion = EvaluacionDesempeno::create([
                 'nombre' => $this->datosPaso1['nombre'] ?? $this->nombre_evaluacion,
-                'descripcion' => $this->datosPaso1['descripcion'] ?? '',
-                'activar_objetivos' => $this->datosPaso1['activar_objetivos'] ?? 0,
-                'porcentaje_objetivos' => $this->datosPaso1['porcentaje_objetivos'] ?? 0,
-                'activar_competencias' => $this->datosPaso1['activar_competencias'] ?? 0,
-                'porcentaje_competencias' => $this->datosPaso1['porcentaje_competencias'] ?? 0,
+                'descripcion' => $this->descripcion_evaluacion ?? '',
+                'activar_objetivos' => $this->activar_objetivos  ?? 0,
+                'porcentaje_objetivos' => $this->porcentaje_objetivos  ?? 0,
+                'activar_competencias' => $this->activar_competencias  ?? 0,
+                'porcentaje_competencias' => $this->porcentaje_competencias  ?? 0,
                 'tipo_periodo' => $this->periodo_evaluacion ?? null,
                 'estatus' => 0, // Estatus de borrador
                 'autor_id' => User::getCurrentUser()->empleado->id,
             ]);
 
             // Guardar el paso actual en la evaluación (por ejemplo, paso 1, 2, 3, etc.)
-            $evaluacion->update(['paso_actual' => $this->paso]);
+            // $evaluacion->update(['paso_actual' => $this->paso]);
 
             // Guardar los datos del paso actual
             switch ($this->paso) {
