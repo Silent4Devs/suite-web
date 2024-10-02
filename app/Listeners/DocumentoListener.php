42065637d6da2544a9224a57836b05ff13dbf4b0
vicvicto <vhraindiana@hotmail.com>
2024-10-02 14:22:26 -0600

    Funcionalidad

--- app/Listeners/DocumentoListener.php
+++ app/Listeners/DocumentoListener.php
@@ -33,17 +33,21 @@ class DocumentoListener implements ShouldQueue
      */
     public function handle($event)
     {
-        $elaborador = Empleado::where('id', $event->documento->elaboro_id)->first();
-        $elabor = User::where('email', trim(removeUnicodeCharacters($elaborador->email)))->first();
-        Notification::send($elabor, new DocumentoNotification($event->documento, $event->tipo_consulta, $event->tabla, $event->slug));
-        $revisor_emple = Empleado::where('id', $event->documento->reviso_id)->first();
-        $revisor = User::where('email', trim(removeUnicodeCharacters($revisor_emple->email)))->first();
-        Notification::send($revisor, new DocumentoNotification($event->documento, $event->tipo_consulta, $event->tabla, $event->slug));
-        $aprobador = Empleado::where('id', $event->documento->aprobo_id)->first();
-        $aprobo = User::where('email', trim(removeUnicodeCharacters($aprobador->email)))->first();
-        Notification::send($aprobo, new DocumentoNotification($event->documento, $event->tipo_consulta, $event->tabla, $event->slug));
-        $responsable = Empleado::where('id', $event->documento->responsable_id)->first();
-        $respo = User::where('email', trim(removeUnicodeCharacters($responsable->email)))->first();
-        Notification::send($respo, new DocumentoNotification($event->documento, $event->tipo_consulta, $event->tabla, $event->slug));
+        try {
+            $elaborador = Empleado::where('id', $event->documento->elaboro_id)->first();
+            $elabor = User::where('email', trim(removeUnicodeCharacters($elaborador->email)))->first();
+            Notification::send($elabor, new DocumentoNotification($event->documento, $event->tipo_consulta, $event->tabla, $event->slug));
+            $revisor_emple = Empleado::where('id', $event->documento->reviso_id)->first();
+            $revisor = User::where('email', trim(removeUnicodeCharacters($revisor_emple->email)))->first();
+            Notification::send($revisor, new DocumentoNotification($event->documento, $event->tipo_consulta, $event->tabla, $event->slug));
+            $aprobador = Empleado::where('id', $event->documento->aprobo_id)->first();
+            $aprobo = User::where('email', trim(removeUnicodeCharacters($aprobador->email)))->first();
+            Notification::send($aprobo, new DocumentoNotification($event->documento, $event->tipo_consulta, $event->tabla, $event->slug));
+            $responsable = Empleado::where('id', $event->documento->responsable_id)->first();
+            $respo = User::where('email', trim(removeUnicodeCharacters($responsable->email)))->first();
+            Notification::send($respo, new DocumentoNotification($event->documento, $event->tipo_consulta, $event->tabla, $event->slug));
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
 }
