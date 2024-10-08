b59618d5129adc6a7465099f15b003c2c8b629a3
vojohn95 <vojohn95@gmail.com>
2024-10-08 17:16:14 -0600

    fx

--- app/Http/Controllers/Admin/QuejasClienteController.php
+++ app/Http/Controllers/Admin/QuejasClienteController.php
@@ -23,11 +23,12 @@ use App\Models\TimesheetCliente;
 use App\Models\TimesheetProyecto;
 use App\Models\User;
 use Carbon\Carbon;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Mail;
-use Carbon\Carbon;
+
 class QuejasClienteController extends Controller
 {
     public function quejasClientes()
