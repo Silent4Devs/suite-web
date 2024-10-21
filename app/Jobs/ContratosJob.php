b06ae111d771eb0915c6d6366e67fa143a61c06e
Saul Ramirez Galicia <saul.ramirez@silent4business.com>
2024-10-21 14:29:06 -0600

    jobs en laravel

--- app/Jobs/ContratosJob.php
+++ app/Jobs/ContratosJob.php
@@ -8,8 +8,6 @@ use App\Notifications\ContratoNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Notification;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Facades\Notification;
 use Illuminate\Bus\Queueable;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\SerializesModels;
