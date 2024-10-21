612321f032d127737a59774a151fdbd6e5c83920
Saul Ramirez Galicia <saul.ramirez@silent4business.com>
2024-10-21 14:03:57 -0600

    jobs implementatios

--- app/Events/ContratoEvent.php
+++ app/Events/ContratoEvent.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Jobs\ContratosJob;
 use App\Models\ContractManager\Contrato;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
