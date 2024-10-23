e94e50a0cbf0b7281334ff54b99678e8e05a8270
Saul Ramirez Galicia <saul.ramirez@silent4business.com>
2024-10-23 13:58:07 -0600

    no function

--- app/Http/Controllers/Admin/SolicitudDayOffController.php
+++ app/Http/Controllers/Admin/SolicitudDayOffController.php
@@ -91,6 +91,12 @@ class SolicitudDayOffController extends Controller
         return view('admin.solicitudDayoff.index', compact('logo_actual', 'empresa_actual', 'dias_disponibles', 'query'));
     }
 
+    public function removeUnicodeCharacters($string)
+    {
+        return preg_replace('/[^\x00-\x7F]/u', '', $string);
+    }
+
+
     public function create()
     {
         abort_if(Gate::denies('solicitud_dayoff_crear'), Response::HTTP_FORBIDDEN, '403 Forbidden');
