d15873465d77ce7d6c2f5c517971a94e5d522421
vicvicto <vhraindiana@hotmail.com>
2024-11-05 13:52:58 -0600

    Correccion funcionamiento

--- resources/views/livewire/proporciona-informacion-aia.blade.php
+++ resources/views/livewire/proporciona-informacion-aia.blade.php
@@ -48,12 +48,12 @@
                     </td>
                     <td style="min-width:40px;">
                         <i class="fas fa-edit"
-                            wire:click.prevent="$dispatch('editarFuenteInformacionAia',{{ $data->id }})">
+                            wire:click.prevent="editarFuenteInformacionAia({{ $data->id }})">
                         </i>
                         {{-- <i class="fas fa-project-diagram"
                             wire:click.prevent="$dispatch('agregarNormas',{{ $data->id }})"> </i> --}}
                         <i class="fas fa-trash-alt text-danger"
-                            wire:click.prevent="$dispatch('eliminarFuenteInformacionAia',{{ $data->id }})"> </i>
+                            wire:click.prevent="eliminarFuenteInformacionAia({{ $data->id }})"> </i>
                     </td>
                     {{-- <td> @livewire('edit-partes-interesadas',['id_requisito'=>$data->id])</td> --}}
                 </tr>
@@ -83,6 +83,6 @@
             });
 
         })
-       
+
     })
 </script>
