ea73766499fd175e214312c01767a84ad5cbf452
MauBlancas <mauricio.blancas@becarios.silent4business.com>
2024-11-22 12:25:02 -0600

    fix: Agregando correcioes de alert,boton de regresar y dicionario para ordenes de compra

--- resources/views/contract_manager/ordenes-compra/show.blade.php
+++ resources/views/contract_manager/ordenes-compra/show.blade.php
@@ -3,6 +3,7 @@
 @section('titulo', 'Ver Orden de Compra')
 
 @include('layouts.datatables_css')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/global/tbButtons.css') }}">
 <link rel="stylesheet" href="{{ asset('css/requisitions/requisitions.css') }}{{ config('app.cssVersion') }}">
 <style>
     .col {
@@ -416,13 +417,17 @@
                     </div>
                 </div>
 
-                <div class="row print-none">
-                    <div class="col-12">
-                        <small><i style="color: #2395AA;">-NOTA : En caso de ser capacitación se necesita el visto
-                                bueno de Gestión de talento.</i></small>
-                        <button class="btn btn info" style="position: relative;   left:40%;"><a
-                                href="{{ route('contract_manager.orden-compra') }}"
-                                style="color: #EEEEEE">Regresar</a></button>
+                <div class="print-none" style="margin-left: 30px; margin-bottom:30px;">
+                    <div class="row">
+                        <div class="col-6 d-flex align-items-center pl-0">
+                            <small><i style="color: #2395AA;">-NOTA : En caso de ser capacitación se necesita el visto
+                                    bueno de Gestión de talento.</i></small>
+                        </div>
+                        <div class="col-6 d-flex justify-content-end pr-0 ">
+                            <button class="btn tb-btn-secondary" style="margin-right: 30px;"><a
+                                    href="{{ route('contract_manager.requisiciones') }}"
+                                    style="color: #EEEEEE">Regresar</a></button>
+                        </div>
                     </div>
                 </div>
             </div>
