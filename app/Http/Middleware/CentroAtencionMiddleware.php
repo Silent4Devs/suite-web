b018b5b2bd02f9ea7994f6b9a973e9b1012b2fa4
Saul Ramirez Galicia <saul.ramirez@silent4business.com>
2024-11-25 15:10:21 -0600

    mildware

---
+++ app/Http/Middleware/CentroAtencionMiddleware.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+use App\Http\Controllers\Api\tbApiPanelControlController;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CentroAtencionMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $estado = $this->consultaApi();
+
+        if ($estado) {
+            return $next($request);
+        } else {
+            abort(403);
+        }
+    }
+
+    public function consultaApi()
+    {
+        try {
+            $apiController = new tbApiPanelControlController();
+            $response = $apiController->getData();
+
+            $client = $response->original[0];
+
+            if ($client['key'] == env('CLIENT_KEY') && $client['Estatus'] == true) {
+                // Filtrar el módulo que cumpla con las condiciones deseadas
+                $modulo = array_filter($client["modulos"], function ($modulo) {
+                    return $modulo["nombre_catalogo"] == "Centro de Atención" && $modulo["estatus"] == true;
+                });
+
+                // Verificar si existe un módulo que cumpla con la condición
+                $estatus = !empty($modulo);
+                return $estatus ? true : false;
+            } else {
+                // Procesa la respuesta según sea necesario
+                return false;
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            abort(403);
+        }
+    }
+}
