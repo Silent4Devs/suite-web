d61df1a39e57687237f25a27f246f33da6298157
Saul Ramirez Galicia <saul.ramirez@silent4business.com>
2024-11-25 15:08:07 -0600

    mildware

---
+++ app/Http/Middleware/GeneralTabantajMiddleware.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Controllers\Api\tbApiPanelControlController;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class GeneralTabantajMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $estado = $this->consultaApi();
+
+        if ($estado) {
+            return $next($request);
+        } else {
+            abort(403);
+        }
+    }
+
+    public function consultaApi()
+    {
+        try {
+            $apiController = new tbApiPanelControlController();
+            $response = $apiController->getData();
+
+            $client = $response->original[0];
+
+            if ($client['key'] == env('CLIENT_KEY') && $client['Estatus'] == true) {
+                // Filtrar el módulo que cumpla con las condiciones deseadas
+                return true;
+            } else {
+                // Procesa la respuesta según sea necesario
+                return false;
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            abort(403);
+        }
+    }
+}
---
+++ app/Http/Middleware/ProyectosMiddleware.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Controllers\Api\tbApiPanelControlController;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProyectosMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $estado = $this->consultaApi();
+
+        if ($estado) {
+            return $next($request);
+        } else {
+            abort(403);
+        }
+    }
+
+    public function consultaApi()
+    {
+        try {
+            $apiController = new tbApiPanelControlController();
+            $response = $apiController->getData();
+
+            $client = $response->original[0];
+
+            if ($client['key'] == env('CLIENT_KEY') && $client['Estatus'] == true) {
+                // Definir los nombres de los módulos que son válidos
+                $modulosValidos = ["Gestión de Talento", "Gestión Financiera", 'Gestión Contractual']; // Agrega todos los nombres de módulos válidos aquí
+
+                // Filtrar los módulos que cumplan con las condiciones deseadas
+                $modulo = array_filter($client["modulos"], function ($modulo) use ($modulosValidos) {
+                    return in_array($modulo["nombre_catalogo"], $modulosValidos) && $modulo["estatus"] == true;
+                });
+
+                // Verificar si existe algún módulo que cumpla con la condición
+                $estatus = !empty($modulo);
+                return $estatus ? true : false;
+            } else {
+                // Procesa la respuesta según sea necesario
+                return false;
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            abort(403);
+        }
+    }
+}
