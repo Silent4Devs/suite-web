31abad698876eadf8b6905d7c5a17d74d0a2d8d3
vojohn95 <vojohn95@gmail.com>
2024-12-02 22:47:26 -0600

    fx

--- app/Livewire/RequisicionesCreateComponent.php
+++ app/Livewire/RequisicionesCreateComponent.php
@@ -19,7 +19,6 @@ use App\Models\ListaDistribucion;
 use App\Models\Organizacion;
 use App\Models\User;
 use App\Services\RequisicionService;
-use App\Traits\ObtenerOrganizacion;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
@@ -311,7 +310,7 @@ class RequisicionesCreateComponent extends Component
             'width' => '1000px', // Asegúrate de que el ancho esté en píxeles
             'onConfirmed' => 'redirigirFaltantes',
             'timerProgressBar' => false,
-            'text' => 'No hay registros en la selección de ' . $name . ', contacte al administrador.',
+            'text' => 'No hay registros en la selección de '.$name.', contacte al administrador.',
             'confirmButtonText' => 'Entendido.',
         ]);
     }
@@ -532,7 +531,7 @@ class RequisicionesCreateComponent extends Component
             }
 
             foreach ($dataProveedoresSugeridos as $key => $provSug) {
-                $name = 'requisicion_' . $this->requisicion_id . 'cotizacion_' . $key . '_' . uniqid() . '.' . $provSug['extArchivo'];
+                $name = 'requisicion_'.$this->requisicion_id.'cotizacion_'.$key.'_'.uniqid().'.'.$provSug['extArchivo'];
                 KatbolProveedorRequisicion::create([
                     'requisiciones_id' => $this->requisicionCreada->id,
                     'proveedor' => $provSug['proveedor'],
