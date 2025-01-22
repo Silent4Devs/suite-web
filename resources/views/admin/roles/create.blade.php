b029e90659e628109bfe0bbdfaa505350b577863
Victor Rodriguez <vhraindiana@hotmail.com>
2025-01-22 17:30:15 -0600

    Correccion vista roles

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -2,14 +2,9 @@
 @section('content')
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/select/1.3.3/css/select.dataTables.min.css">
-    <h5 class="col-12 titulo_general_funcion"> Registrar: Rol</h5>
-    <div class="mt-4 card">
-        <div class="card-body">
 
-            @livewire('create-roles-livewire')
+    @livewire('create-roles-livewire')
 
-        </div>
-    </div>
 @endsection
 
 @section('scripts')
