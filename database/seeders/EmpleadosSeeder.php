ac833484ada1e5c3900c14a3442179d40fb69d61
MARTI DAVID TENDILLA GONZALEZ <marti.tendilla@gonet.us>
2025-02-18 18:10:35 -0600

    se agrega errores

--- database/seeders/EmpleadosSeeder.php
+++ database/seeders/EmpleadosSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Area;
 use App\Models\Empleado;
 use App\Models\Puesto;
+use App\Models\Sede;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
@@ -18,8 +19,7 @@ class EmpleadosSeeder extends Seeder
     public function run()
     {
         Empleado::create([
-            'name' => 'Admin',
-            'email' => 'admin@admin.com',
+            'name' => 'Direccion General',
             'foto' => null,
             'puesto_id' => Puesto::first()->id,
             'antiguedad' => Carbon::now(),
@@ -28,8 +28,14 @@ class EmpleadosSeeder extends Seeder
             'created_at' => Carbon::now(),
             'extension' => null,
             'telefono_movil' => null,
-            'direccion' => 'Direccion General',
             'n_empleado' => 1,
+            'direccion' => 'Not provided',
+            'sede_id' => Sede::first()->id,
+            'telefono' => null,
+            'n_registro' => null,
+            'genero' => null,
+            'email' => 'example@example.com',
+            'resumen' => '',
         ]);
     }
 }
