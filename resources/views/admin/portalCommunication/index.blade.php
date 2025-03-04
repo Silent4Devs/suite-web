44d7dcfd1ad4893c89bb24b196738d79ecab73b0
Saul Ramirez Galicia <saul.ramirez@silent4business.com>
2025-03-04 14:14:52 -0600

    portal

--- resources/views/admin/portalCommunication/index.blade.php
+++ resources/views/admin/portalCommunication/index.blade.php
@@ -442,7 +442,7 @@
                                         <span class="title-item-carr-port">{{ $nuv->name }} </span> <br>
                                         <p>
                                             {{ $nuv->puestoRelacionado->puesto ?? '' }} <br>
-                                            {{ $nuv->area->area }}
+                                            {{ $nuv->area->area  ?? '' }}
                                         </p>
                                     </div>
                                     <hr>
